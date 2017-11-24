aadc76f2077ee30cad0ba7fad05788395993ab18
varconstant <32683624+libozh@users.noreply.github.com>
2017-11-24 14:49:05 -0500

    Update buffer.js

--- src/util/buffer.js
+++ src/util/buffer.js
@@ -3,11 +3,11 @@
 var semver = require('semver');
 
 // Node.js 0.12 is lacking support for Buffer.from,
-// and < 4.5.0 version Buffer.from doesn't support string as parameter 
+// and < 4.5.0 version Buffer.from doesn't support string as parameter
 
-var isSupportedVersion = function(){
+var isSupportedVersion = function() {
   return semver.satisfies(process.versions.node, '>=4.5.0');
-}
+};
 
 exports.fromString = function fromString(str, encoding) {
   encoding = encoding || 'utf8';
