c6095b9f66d1d83ff26ea315a95d0ad44a9ee9ee
Ben Ripkens <bripkens@gmail.com>
2018-04-25 09:43:32 +0200

    test(load): Change load test structure to more closely reflect customer environments

--- load-test/jmeter/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf ./result
-rm ./jmeter.log
-mkdir result
-JVM_ARGS="-Xms1024m -Xmx2048m -XX:NewSize=512m -XX:MaxNewSize=1024m"  && export JVM_ARGS && jmeter -n -t JMeter.jmx -l result/result.log -e -o result/testresult
--- load-test/src/simpleServer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// provides a simple http server without any special features
-var http = require('http');
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('OK');
-}).listen(5000);
