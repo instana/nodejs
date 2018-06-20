4d552f2dbc4c53007623d2dc3324fb022595d302
Peter Giacomo Lombardo <pglombardo@gmail.com>
2018-06-20 11:38:52 +0200

    Vanilla test and skipped transactional test case

--- test/tracing/pg_test.js
+++ test/tracing/pg_test.js
@@ -255,4 +255,62 @@ describe.only('tracing/pg', function() {
       });
     });
   });
+
+  it('must not break vanilla postgres (not tracing)', function() {
+    return expressPgControls.sendRequest({
+      method: 'GET',
+      path: '/pool-string-insert',
+      suppressTracing: true
+    })
+    .then(function(response) {
+      return utils.retry(function() {
+        return agentStubControls.getSpans()
+          .then(function(spans) {
+            var entrySpans = utils.getSpansByName(spans, 'node.http.server');
+            var pgSpans = utils.getSpansByName(spans, 'postgres');
+
+            expect(entrySpans).to.have.lengthOf(0);
+            expect(pgSpans).to.have.lengthOf(0);
+
+            expect(response.rows).to.lengthOf(1);
+            expect(response.rows[0].name).to.equal('beaker');
+            expect(response.rows[0].email).to.equal('beaker@muppets.com');
+          });
+      });
+    });
+  });
+
+  // TBD Not supported (yet)
+  it.skip('must trace transactions', function() {
+    return expressPgControls.sendRequest({
+      method: 'GET',
+      path: '/transaction',
+      body: {}
+    })
+    .then(function() {
+      return utils.retry(function() {
+        return agentStubControls.getSpans()
+          .then(function(spans) {
+            var entrySpans = utils.getSpansByName(spans, 'node.http.server');
+            var pgSpans = utils.getSpansByName(spans, 'postgres');
+
+            expect(entrySpans).to.have.lengthOf(1);
+            expect(pgSpans).to.have.lengthOf(4);
+
+            var entrySpan = entrySpans[0];
+            var pgSpan = pgSpans[0];
+
+            expect(pgSpan.f.e).to.equal(String(expressPgControls.getPid()));
+            expect(pgSpan.t).to.equal(entrySpan.t);
+            expect(pgSpan.p).to.equal(entrySpan.s);
+            expect(pgSpan.n).to.equal('postgres');
+            expect(pgSpan.f.e).to.equal(String(expressPgControls.getPid()));
+            expect(pgSpan.async).to.equal(false);
+            expect(pgSpan.error).to.equal(false);
+            expect(pgSpan.ec).to.equal(0);
+            expect(pgSpan.data.pg.stmt).to.equal('BEGIN');
+          });
+      });
+    });
+  });
 });
