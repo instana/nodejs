a68d054e275c379b288b283d52b21f3f3bcdf5be
Bastian Krol <basti1302@users.noreply.github.com>
2018-07-04 10:14:02 +0200

    fix(tracing): only instrument bluebird if the module is available

--- src/tracing/instrumentation/bluebird.js
+++ src/tracing/instrumentation/bluebird.js
@@ -13,8 +13,20 @@ exports.deactivate = function() {
 
 exports.init = function() {
   try {
-    require('cls-bluebird')(cls.ns);
-  } catch (e) {
-    logger.warn('Failed to instrument bluebird', e);
+    if (require.resolve('bluebird')) {
+      try {
+        require('cls-bluebird')(cls.ns);
+      } catch (e) {
+        logger.warn('Failed to instrument bluebird', e);
+      }
+    } else {
+      // This should actually not happen as require.resolve should either return a resolved filename or throw an
+      // exception.
+      logger.debug('Won\'t instrument bluebird as it is not installed (require.resolve returned falsy value).');
+    }
+  } catch (notResolved) {
+    // This happens if bluebird is not available, in which case we do not need
+    // to instrument anything, so we can safely ignore the error.
+    logger.debug('Won\'t instrument bluebird as it is not installed.');
   }
 };
