44c58eb621e69e3c2cf030d353837f09c38728e6
Bastian Krol <bastian.krol@instana.com>
2018-08-27 14:58:08 +0200

    remove obsolete assumption tests

--- test/util/uncaughtExceptions/apps/rethrow.js
+++ test/util/uncaughtExceptions/apps/rethrow.js
@@ -3,8 +3,8 @@
 'use strict';
 
 // Rethrowing the error (or, more generally, throwing any error) from the
-// uncaughtException handler terminates the process with an exit code != 01 The
-// orginal stack trace is preserved.
+// uncaughtException handler terminates the process with an exit code != 1.
+// The orginal stack trace is preserved.
 
 process.on('uncaughtException', function(err) {
   throw err;
