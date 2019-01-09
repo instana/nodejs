163dd6214fa2b57602df03c591e5b713e7072232
Bastian Krol <bastian.krol@instana.com>
2019-01-09 16:10:12 +0100

    test(metrics): add test for active handles and active requests
    This adds a test for the active handles metrics because the simple
    test based on setTimeout had to be disabled for the Node.js 11.x
    update.
    
    It also adds a test for the active request metric that was not covered
    before.

--- test/metrics/activeHandles_test.js
+++ test/metrics/activeHandles_test.js
@@ -1,11 +1,14 @@
+/* global Promise */
 /* eslint-env mocha */
 
 'use strict';
 
 var expect = require('chai').expect;
 var semver = require('semver');
+var net = require('net');
 
 var activeHandles = require('../../src/metrics/activeHandles');
+var utils = require('../utils');
 
 describe('metrics.activeHandles', function() {
   beforeEach(function() {
@@ -20,10 +23,12 @@ describe('metrics.activeHandles', function() {
     expect(activeHandles.currentPayload).to.equal(process._getActiveHandles().length);
   });
 
-  it('should update handle count', function() {
+  it('should update handle count for a setTimeout', function() {
     if (semver.satisfies(process.versions.node, '>=11')) {
       // skip test beginning with Node.js 11, I suspect commit https://github.com/nodejs/node/commit/ccc3bb73db
-      // (PR https://github.com/nodejs/node/pull/24264) to have broken this metric.
+      // (PR https://github.com/nodejs/node/pull/24264) to have broken this test. Seems timeouts do no longer add to
+      // active handles. See 'with net client/server' for a test case that verifies this metric also for
+      // Node.js >= 11.x.
       return;
     }
 
@@ -31,5 +36,67 @@ describe('metrics.activeHandles', function() {
     var timeoutHandle = setTimeout(function() {}, 100);
     expect(activeHandles.currentPayload).to.equal(previousCount + 1);
     clearTimeout(timeoutHandle);
+  });
+
+  describe('with net client/server', function() {
+    /* eslint-disable max-len */
+    // Inspired by
+    // https://github.com/nodejs/node/blob/01422769775a2ce7dfef8aa6dbda2d326f002e13/test/parallel/test-process-getactivehandles.js
+
+    var maxClients = 8;
+    var server;
+    var connections = [];
+    var clients = [];
+    var activeHandlesBefore;
+
+    beforeEach(function() {
+      activeHandlesBefore = activeHandles.currentPayload;
+      server = net
+        .createServer(function listener(c) {
+          connections.push(c);
+        })
+        .listen(0, makeConnection);
+    });
+
+    afterEach(function() {
+      clients.forEach(function(client) {
+        client.destroy();
+      });
+      connections.forEach(function(connection) {
+        connection.end();
+      });
+      server.close();
+    });
+
+    it('should update handle count for net client and connection', function() {
+      return utils.retry(function() {
+        return new Promise(function(resolve, reject) {
+          if (clients.length >= maxClients) {
+            // at least one handle should exist per client and one per connection
+            expect(activeHandles.currentPayload).to.be.at.least(activeHandlesBefore + 2 * maxClients);
+            resolve();
+          } else {
+            reject(new Error('Still waiting for more clients to connect.'));
+          }
+        });
+      });
+    });
+
+    function makeConnection() {
+      if (clients.length >= maxClients) {
+        return;
+      }
+      net.connect(
+        server.address().port,
+        function connected() {
+          clientConnected(this);
+          makeConnection();
+        }
+      );
+    }
+
+    function clientConnected(client) {
+      clients.push(client);
+    }
   });
 });
