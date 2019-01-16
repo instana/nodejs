2714a0037a524ea82e53fb82586b5489e258d6ca
Bastian Krol <bastian.krol@instana.com>
2019-01-16 12:34:18 +0100

    chore(ci): use grpc@^1.17.0 as default for tests
    plus: use grpc@1.10.1 for tests under Node.js 8.x only via .travis.yml
    hooks

---
+++ test/tracing/protocols/grpc/versionUnderTest.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var semver = require('semver');
+
+module.exports = exports = function determineGrpcPackageVersionUnderTest() {
+  if (process.env.GRPC_PACKAGE_VERSION) {
+    return process.env.GRPC_PACKAGE_VERSION;
+  } else if (semver.satisfies(process.versions.node, '^8')) {
+    return '=1.10.1';
+  } else {
+    return '>=1.17.0';
+  }
+};
