c17ea6331938da3c5837bb8b2f5cb17bf897857f
Bastian Krol <bastian.krol@instana.com>
2019-03-14 08:59:42 +0100

    chore(tracing): replace Math.random ID generation with crypto module
    The previous approach was actually prone to ID collisions. There was at
    least one Travis run in which the test
    tracing/tracingUtil/generate random IDs failed with:
    
    1) tracing/tracingUtil
         generate random IDs
           must generate random span IDs:
        AssertionError: found a non-unique ID at indices 13141 and 14324: 000625e2b97fffff === 000625e2b97fffff
        + expected - actual
        -000625e2b97fffff
        +an ID != 000625e2b97fffff
    
    In addition, the previous approach only produced 12-13 digits, the
    left-most characters where always zeroes. This issue would become even
    more pronounced once we switch to 32 char trace IDs.
    
    The new approach (inspired by
    https://github.com/sindresorhus/crypto-random-string) uses Node.js'
    crypto.randomBytes function instead of Math.random to generate IDs with
    a lot lower probability for collisions.
    
    It is a bit more expensive. A micro benchmark produced the following results:
    
    Previous approach (Math.random):
    - Generating 10000000 IDs with 16 characters took  9807 milliseconds.
    - Generating 10000000 IDs with 32 characters took  9804 milliseconds.
    - On average, it costs approximately 0.001 ms (1 μs) per ID.
    
    New approach (crypto.randomBytes):
    - Generating 10000000 IDs with 16 characters took 29985 milliseconds.
    - Generating 10000000 IDs with 32 characters took 30601 milliseconds.
    - On average, it costs approximately 0.003 ms (3 μs) per ID.
    
    So it is slower by factor three, but we get much better and actually
    uniqe IDs. We generate at most two IDs for each incoming request
    (entry span ID and optionally a trace ID if there is no incoming
    trace ID) and one per exit span. 3 μs is still absolutely negligible
    when compared to processing an incoming HTTP request/
    GRPc message/Kafka/RabbitMQ/... message or any exit span (DB request,
    outgoing HTTP request, ...).

--- test/tracing/tracingUtil_test.js
+++ test/tracing/tracingUtil_test.js
@@ -5,30 +5,37 @@
 var expect = require('chai').expect;
 var fail = require('assert').fail;
 
+var config = require('../config');
+
 var tracingUtil = require('../../src/tracing/tracingUtil');
 
 describe('tracing/tracingUtil', function() {
   describe('generate random IDs', function() {
-    this.timeout(60000);
+    this.timeout(config.getTestTimeout() * 10);
 
     testRandomIds('trace', tracingUtil.generateRandomTraceId, 16);
     testRandomIds('span', tracingUtil.generateRandomSpanId, 16);
 
+    // The following line checks that 128 bit (32 char)  IDs are generated okay, although we do not yet need
+    // 128 bit IDs (yet). It can be removed once trace IDs are upped to 128 bit.
+    testRandomIds('128 bit', tracingUtil.generateRandomId.bind(null, 32), 32);
+
+    var validIdRegex = /^[a-f0-9]+$/;
+
     function testRandomIds(idType, genFn, expectedLength) {
-      it('must generate random ' + idType + ' IDs', function() {
+      it('must generate unique and wellformed ' + idType + ' IDs', function() {
         var iterations = 20000;
         var generatedIds = [];
         for (var i = 0; i < iterations; i++) {
           generatedIds[i] = genFn();
           expect(generatedIds[i]).to.be.a('string');
           expect(generatedIds[i].length).to.equal(expectedLength);
+          expect(generatedIds[i]).to.match(validIdRegex);
         }
 
         // verify that the generated IDs are unique
         for (i = 0; i < iterations; i++) {
-          for (var j = 0; j < iterations; j++) {
-            // eslint-disable-next-line no-continue
-            if (i === j) continue;
+          for (var j = i + 1; j < iterations; j++) {
             // eslint-disable-next-line eqeqeq
             if (generatedIds[i] == generatedIds[j]) {
               fail(
@@ -49,6 +56,28 @@ describe('tracing/tracingUtil', function() {
             }
           }
         }
+      });
+    }
+  });
+
+  describe('benchmark ID generation', function() {
+    this.timeout(config.getTestTimeout() * 4);
+
+    // generate one million IDs each (64 bit/16 chars, 128 bit/32 chars)
+    var iterations = 1000000;
+    var maxAcceptableDuration = process.env.CI ? 50000 : 10000;
+
+    microBenchmark(16);
+    microBenchmark(32);
+
+    function microBenchmark(length) {
+      it('with hex IDs that are ' + length + ' characters long', function() {
+        var start = Date.now();
+        for (var i = 0; i < iterations; i++) {
+          tracingUtil.generateRandomSpanId(length);
+        }
+        var duration = Date.now() - start;
+        expect(duration).to.be.lte(maxAcceptableDuration);
       });
     }
   });
