e1f710ed2fbaa7859f87762b32a0857852c17df2
Bastian Krol <bastian.krol@instana.com>
2019-05-09 08:10:01 +0200

    fix(tracing): Fastify: handle array of beforeHandle functions correctly
    opts.beforeHandler can be a function or an array of functions. This
    change makes sure we also handle the array case correctly. It also
    prepares the instrumentation for Fastify 2.x, where the attribute is
    named preHandler instead of beforeHandler. Finally, it adds explicit
    test cases for routes using beforeHandler.

--- packages/collector/test/tracing/frameworks/fastify/test.js
+++ packages/collector/test/tracing/frameworks/fastify/test.js
@@ -25,24 +25,35 @@ describe('tracing/fastify', function() {
   controls.registerTestHooks();
 
   describe('path templates', function() {
-    check('/', '/');
-    check('/foo/42', '/foo/:id');
-    check('/sub', '/sub');
-    check('/sub/bar/42', '/sub/bar/:id');
+    check('/', 200, { hello: 'world' }, '/');
+    check('/foo/42', 200, { hello: 'world' }, '/foo/:id');
+    check('/before-handler/13', 200, { before: 'handler' }, '/before-handler/:id');
+    check(
+      '/before-handler-array/02',
+      500,
+      { statusCode: 500, error: 'Internal Server Error', message: 'Yikes' },
+      '/before-handler-array/:id'
+    );
+    check('/sub', 200, { hello: 'world' }, '/sub');
+    check('/sub/bar/42', 200, { hello: 'world' }, '/sub/bar/:id');
 
-    function check(actualPath, expectedTemplate) {
+    function check(actualPath, expectedStatusCode, expectedResponse, expectedTemplate) {
       it('must report path templates for actual path: ' + actualPath, function() {
         return controls
           .sendRequest({
             method: 'GET',
-            path: actualPath
+            path: actualPath,
+            simple: false,
+            resolveWithFullResponse: true
           })
-          .then(function() {
+          .then(function(response) {
+            expect(response.statusCode).to.equal(expectedStatusCode);
+            expect(response.body).to.deep.equal(expectedResponse);
             return utils.retry(function() {
               return agentControls.getSpans().then(function(spans) {
                 utils.expectOneMatching(spans, function(span) {
                   expect(span.data.http.path_tpl).to.equal(expectedTemplate);
-                  expect(span.data.http.status).to.equal(200);
+                  expect(span.data.http.status).to.equal(expectedStatusCode);
                   expect(span.data.http.url).to.equal(actualPath);
                   expect(span.k).to.equal(constants.ENTRY);
                 });
