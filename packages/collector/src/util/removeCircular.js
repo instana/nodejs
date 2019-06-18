54f3efc2c07e0d07a33c6142fe76b7aa5606420f
Sebastian Klose <sebastian.klose@jet.com>
2019-06-18 13:49:49 -0400

    fix(tracing): don't fail on span data with circular references

---
+++ packages/collector/src/util/removeCircular.js
@@ -0,0 +1,16 @@
+/* global WeakSet */
+
+'use strict';
+
+module.exports = exports = function createCircularReferencesRemover() {
+  const seen = new WeakSet();
+  return (_, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
