8b62fcc3d1a0ebcc6bb50d6a838269171c2cfc37
Bastian Krol <bastian.krol@instana.com>
2019-10-17 16:20:21 +0200

    test(lambda): remove superfluous exclude for Node.js < 8 in Lambda test
    We do not run tests in Node.js < 8 anymore for the @instana/aws-lambda
    package any longer since AWS has dropped Node.js 6 as a supported
    runtime.

--- packages/aws-lambda/test/aws_lambda/async/test.js
+++ packages/aws-lambda/test/aws_lambda/async/test.js
@@ -3,15 +3,9 @@
 'use strict';
 
 const path = require('path');
-const semver = require('semver');
 
 const commonTests = require('../common/tests_common.js');
 
 describe('aws/lambda/async', function() {
-  if (semver.lt(process.version, '8.0.0')) {
-    // eslint-disable-next-line no-console
-    console.log(`Skipping tests for async function handlers in Node.js version ${process.version}`);
-    return;
-  }
   commonTests.registerTests.bind(this)(path.join(__dirname, './lambda'));
 });
