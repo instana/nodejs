ae978311f94fc116d8a405f76ebe99d609ee946b
Bastian Krol <bastian.krol@instana.com>
2019-10-24 20:08:07 +0200

    chore(lamba): build lambda packages with and without layer on demand

--- packages/aws-lambda/lambdas/bin/create-zips.sh
+++ packages/aws-lambda/lambdas/bin/create-zips.sh
@@ -6,8 +6,13 @@ cd `dirname $BASH_SOURCE`/..
 pushd .. > /dev/null
 pwd
 rm -rf instana-aws-lambda*.tgz
-npm --loglevel=warn pack
-mv instana-aws-lambda-*.tgz instana-aws-lambda.tgz
+
+if [[ -z "${USE_LAMBDA_LAYER-}" ]]; then
+  # $USE_LAMBDA_LAYER is empty, that is, we want to use the local npm package tar file, which means we need to
+  # build it first.
+  npm --loglevel=warn pack
+  mv instana-aws-lambda-*.tgz instana-aws-lambda.tgz
+fi
 popd > /dev/null
 
 for lambda_directory in */ ; do
