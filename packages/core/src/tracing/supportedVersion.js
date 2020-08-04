1b56f51134870be395b127d37161c7cf6bc02deb
Bastian Krol <bastian.krol@instana.com>
2020-08-04 11:12:56 +0200

    chore: remove Node.js 4 and 5 from supported versions

--- packages/core/src/tracing/supportedVersion.js
+++ packages/core/src/tracing/supportedVersion.js
@@ -3,5 +3,5 @@
 var semver = require('semver');
 
 module.exports = exports = function supportedVersion(version) {
-  return semver.satisfies(version, '^4.5 || ^5.10 || ^6 || ^7 || ^8.2.1 || ^9.1.0 || ^10.4.0 || ^11 || >=12.0.0');
+  return semver.satisfies(version, '^6 || ^7 || ^8.2.1 || ^9.1.0 || ^10.4.0 || ^11 || >=12.0.0');
 };
