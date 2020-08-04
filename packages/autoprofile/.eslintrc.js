c34ff1e639e7642f4817530ce9b50e324880bcdd
Bastian Krol <bastian.krol@instana.com>
2020-08-04 11:13:48 +0200

    chore: update .eslint config files to ES 6 everywhere

--- packages/autoprofile/.eslintrc.js
+++ packages/autoprofile/.eslintrc.js
@@ -1,14 +1,6 @@
 module.exports = {
   extends: '../../.eslintrc.js',
 
-  parserOptions: {
-    ecmaVersion: 6
-  },
-
-  env: {
-    es6: true
-  },
-
   rules: {
     'class-methods-use-this': 'off'
   }
--- packages/collector/dummy-app/.eslintrc.js
+++ packages/collector/dummy-app/.eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   rules: {
     'no-console': 0
-  },
-  parserOptions: {
-    ecmaVersion: 6
   }
 };
