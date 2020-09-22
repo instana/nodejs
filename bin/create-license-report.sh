c991f6f77f7cdc5afd0f0d726f8f72c146afac79
Bastian Krol <bastian.krol@instana.com>
2020-09-22 13:21:44 +0200

    chore(deps): add script to gather dependencies and licenses
    [skip ci]

---
+++ bin/create-license-report.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# We can use this once our changes to license-ls are merged.
+licensels="npx license-ls"
+
+# For now, we use a modified version.
+licensels="npx github:basti1302/license-ls#extend-report"
+
+nodejs_collector_homedir="$( cd `dirname $BASH_SOURCE`/.. && pwd )"
+
+cd $nodejs_collector_homedir
+
+tmpdir=$(mktemp -d)
+echo "Using temporary directory: $tmpdir"
+pushd $tmpdir > /dev/null
+
+echo "Installing all user facing packages into temporary directory..."
+npm install --prod @instana/collector @instana/aws-lambda @instana/aws-fargate @instana/google-cloud-run
+
+$licensels --format csv --include name,version,licenseIdWithoutVersion,licenseVersion,copyrightHolder,copyrightYear,licenseLink > $nodejs_collector_homedir/nodejs-license-report.csv
+
+popd > /dev/null
+
+echo "Removing temporary directory: $tmpdir"
+rm -rf $tmpdir
+
