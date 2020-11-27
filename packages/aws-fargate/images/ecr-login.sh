34adbe9efef75bca2a7021bb89d4ee8f3f43553f
Bastian Krol <bastian.krol@instana.com>
2020-11-27 07:45:39 +0100

    chore(fargate): update ecr-login.sh

--- packages/aws-fargate/images/ecr-login.sh
+++ packages/aws-fargate/images/ecr-login.sh
@@ -2,4 +2,4 @@
 
 set -eo pipefail
 
-$(aws ecr get-login --region us-east-2 | sed 's/-e none //')
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 410797082306.dkr.ecr.us-east-2.amazonaws.com
