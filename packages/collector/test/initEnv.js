e0725d22f6d1305c2ddc0c8c0c3a59ec9e6ac28a
kirrg001 <katharina.irrgang@gmail.com>
2021-09-03 16:07:40 +0200

    test(refactor): moved env variables to collector package
    no issue
    
    Reasons:
    - env variables had to declared in 2 places
    - `cd packages/collector && npm run test` fails
    - simply moved them to an init file for the collector test env

---
+++ packages/collector/test/initEnv.js
@@ -0,0 +1,35 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. and contributors 2020
+ */
+
+'use strict';
+
+// NOTE: default docker compose hosts, ports and credentials
+const DEFAULT_ENV_VALUES = {
+  MONGODB: '127.0.0.1:27017',
+  ELASTICSEARCH: '127.0.0.1:9200',
+  ZOOKEEPER: '127.0.0.1:2181',
+  KAFKA: '127.0.0.1:9092',
+  REDIS: '127.0.0.1:6379',
+  MYSQL_HOST: '127.0.0.1',
+  MYSQL_PORT: '3306',
+  MYSQL_USER: 'node',
+  MYSQL_PW: 'nodepw',
+  MYSQL_DB: 'nodedb',
+  POSTGRES_USER: 'node',
+  POSTGRES_PASSWORD: 'nodepw',
+  POSTGRES_DB: 'nodedb',
+  POSTGRES_HOST: '127.0.0.1',
+  MSSQL_HOST: '127.0.0.1',
+  MSSQL_PORT: '1433',
+  MSSQL_USER: 'sa',
+  MSSQL_PW: 'stanCanHazMsSQL1'
+};
+
+// CASE: if env variable is not set from outside, fallback to defaults
+Object.keys(DEFAULT_ENV_VALUES).forEach(key => {
+  if (!process.env[key]) {
+    process.env[key] = DEFAULT_ENV_VALUES[key];
+  }
+});
