7487d8288d4f120237f75fdfc4770588ae3e14a9
Katharina Irrgang <katharina.irrgang@googlemail.com>
2021-09-06 13:03:44 +0200

    test(refactor): moved env variables to collector package
    no issue
    
    Reasons:
    - env variables had to declared in 2 places
    - `cd packages/collector && npm run test` fails
    - simply moved them to an init file for the collector test env
    
    Co-authored-by: kirrg001 <katharina.irrgang@gmail.com>

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -9,23 +9,4 @@ set -eo pipefail
 
 cd `dirname $BASH_SOURCE`/..
 
-export MONGODB="127.0.0.1:27017"
-export ELASTICSEARCH="127.0.0.1:9200"
-export ZOOKEEPER="127.0.0.1:2181"
-export KAFKA="127.0.0.1:9092"
-export REDIS="127.0.0.1:6379"
-export MYSQL_HOST="127.0.0.1"
-export MYSQL_PORT="3306"
-export MYSQL_USER="node"
-export MYSQL_PW="nodepw"
-export MYSQL_DB="nodedb"
-export POSTGRES_USER="node"
-export POSTGRES_PASSWORD="nodepw"
-export POSTGRES_DB="nodedb"
-export POSTGRES_HOST="127.0.0.1"
-export MSSQL_HOST="127.0.0.1"
-export MSSQL_PORT="1433"
-export MSSQL_USER="sa"
-export MSSQL_PW="stanCanHazMsSQL1"
-
 npm run test:debug
