b7598eb5ce1cc9ebc0310edf5e3accb5271f220f
Bastian Krol <bastian.krol@instana.com>
2021-11-15 11:15:41 +0100

    style: also lint js files in root and bin directory

---
+++ bin/.eslintrc.js
@@ -0,0 +1,21 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. and contributors 2020
+ */
+
+'use strict';
+
+module.exports = {
+  env: {
+    node: true
+  },
+
+  parserOptions: {
+    sourceType: 'script',
+    ecmaVersion: 2018
+  },
+
+  rules: {
+    'no-console': 'off'
+  }
+};
