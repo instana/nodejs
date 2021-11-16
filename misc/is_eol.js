e18acff2b13b5a4f51d7ba1bc131f902bbfe4def
Bastian Krol <bastian.krol@instana.com>
2021-11-16 11:29:58 +0100

    ci: check all commits since last release via commitlint
    This makes sure that lerna version (with the "conventionalCommits" option set in
    lerna.json) can correctly determin the▸next version number and generate
    changelogs.

--- misc/is_eol.js
+++ misc/is_eol.js
@@ -7,10 +7,10 @@
 'use strict';
 
 /**
- * Exits with exit 1 if running on Node.js >= 10.x.y, otherwise with exit code 0.
- * That is, for EOL versions (4, 6, 8, ...), this script will pass and for non-EOL versions it will fail.
+ * Exits with exit 1 if running on Node.js >= 12.x.y, otherwise with exit code 0.
+ * That is, for EOL versions (10, 8, 6, ...), this script will pass and for non-EOL versions it will fail.
  */
-if (parseInt(/v(\d+)\./.exec(process.version)[1], 10) >= 10) {
+if (parseInt(/v(\d+)\./.exec(process.version)[1], 10) >= 12) {
   process.exit(1);
 } else {
   process.exit(0);
