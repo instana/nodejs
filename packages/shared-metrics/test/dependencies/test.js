12eca339b7e8e472d7502c9f5a8e1440b34e761f
kirrg001 <katharina.irrgang@gmail.com>
2021-11-24 08:14:59 +0100

    chore: debug

--- packages/shared-metrics/test/dependencies/test.js
+++ packages/shared-metrics/test/dependencies/test.js
@@ -115,6 +115,8 @@ describe('dependencies', function () {
         agentControls.getAllMetrics(controls.getPid()).then(allMetrics => {
           const deps = findMetric(allMetrics, ['dependencies']);
           expect(deps).to.be.an('object');
+          console.log('DEBUG');
+          console.log(deps);
           expect(Object.keys(deps)).to.have.lengthOf(110);
 
           expect(deps['@instana/shared-metrics']).to.exist;
