f59ee13dec94180d09a8ea20c1c7ff4d138c9b01
kirrg001 <katharina.irrgang@gmail.com>
2021-11-24 12:59:52 +0100

    chore: lala

--- packages/shared-metrics/test/dependencies/test.js
+++ packages/shared-metrics/test/dependencies/test.js
@@ -93,7 +93,7 @@ describe('dependencies', function () {
       runCommand(`npm install --prefix ${tmpDir} ${path.join(repoRootDir, 'packages', 'autoprofile')}`, tmpDir);
       runCommand(`npm install --prefix ${tmpDir} ${path.join(repoRootDir, 'packages', 'shared-metrics')}`, tmpDir);
 
-      runCommand('ls -la packages/shared-metrics/node_modules/', repoRootDir);
+      runCommand('ls -la node_modules/@instana/shared-metrics', tmpDir);
 
       unlinkSync(path.join(tmpDir, 'package.json'));
     });
