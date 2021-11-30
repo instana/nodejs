fa7ccf0182510976c3b639feb7e674c9ab6e064c
kirrg001 <katharina.irrgang@gmail.com>
2021-11-30 18:40:21 +0100

    chore: removed node version check in collector/test/tracing/protocols/grpc/versionUnderTest.js
    refs 80206

--- packages/collector/test/tracing/protocols/grpc/versionUnderTest.js
+++ packages/collector/test/tracing/protocols/grpc/versionUnderTest.js
@@ -5,13 +5,9 @@
 
 'use strict';
 
-const semver = require('semver');
-
 module.exports = exports = function determineGrpcPackageVersionUnderTest() {
   if (process.env.GRPC_PACKAGE_VERSION) {
     return process.env.GRPC_PACKAGE_VERSION;
-  } else if (semver.satisfies(process.versions.node, '^8')) {
-    return '=1.10.1';
   } else {
     return '>=1.17.0';
   }
