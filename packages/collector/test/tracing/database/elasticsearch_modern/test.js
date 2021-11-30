57e7b6524d135d54ef097df129694c160951d29c
kirrg001 <katharina.irrgang@gmail.com>
2021-11-30 18:26:48 +0100

    chore: removed node version check in collector/test/tracing/database/elasticsearch_modern/test.js
    refs 80206

--- packages/collector/test/tracing/database/elasticsearch_modern/test.js
+++ packages/collector/test/tracing/database/elasticsearch_modern/test.js
@@ -7,7 +7,6 @@
 
 const expect = require('chai').expect;
 const path = require('path');
-const semver = require('semver');
 const { exec } = require('child_process');
 
 const constants = require('@instana/core').tracing.constants;
@@ -23,9 +22,7 @@ const globalAgent = require('../../../globalAgent');
 
 const { fail } = expect;
 
-const mochaSuiteFn = semver.gte(process.versions.node, '10.0.0') ? describe : describe.skip;
-
-mochaSuiteFn('tracing/elasticsearch (modern client)', function () {
+describe('tracing/elasticsearch (modern client)', function () {
   let originalEsVersion;
 
   this.timeout(Math.max(config.getTestTimeout() * 4, 30000));
