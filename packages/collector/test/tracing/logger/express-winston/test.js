81c685373d1df316948eabdfbd185a9c006e9bba
kirrg001 <katharina.irrgang@gmail.com>
2021-11-30 18:31:09 +0100

    chore: removed node version check in collector/test/tracing/logger/express-winston/test.js
    refs 80206

--- packages/collector/test/tracing/logger/express-winston/test.js
+++ packages/collector/test/tracing/logger/express-winston/test.js
@@ -7,7 +7,6 @@
 
 const { expect, fail } = require('chai');
 
-const semver = require('semver');
 const constants = require('@instana/core').tracing.constants;
 const supportedVersion = require('@instana/core').tracing.supportedVersion;
 const config = require('../../../../../core/test/config');
@@ -15,8 +14,7 @@ const testUtils = require('../../../../../core/test/test_util');
 const ProcessControls = require('../../../test_util/ProcessControls');
 const globalAgent = require('../../../globalAgent');
 
-const mochaSuiteFn =
-  supportedVersion(process.versions.node) && semver.gte(process.version, '10.12.0') ? describe : describe.skip;
+const mochaSuiteFn = supportedVersion(process.versions.node) ? describe : describe.skip;
 
 mochaSuiteFn('tracing/logger/express-winston', function () {
   this.timeout(config.getTestTimeout());
