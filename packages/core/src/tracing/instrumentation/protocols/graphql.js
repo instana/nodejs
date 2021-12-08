48bfbc36f4a02defe21a3ec8d104b92feaf03a2e
Willian Carvalho <willian.carvalho@instana.com>
2021-12-08 15:02:50 +0100

    feat: added asynclocalstorage implementation (#430)
    refs 77490
    
    - https://github.com/instana/nodejs/pull/430
    - https://github.com/instana/nodejs/pull/416
    
    Co-authored-by: kirrg001 <katharina.irrgang@gmail.com>
    Co-authored-by: Bastian Krol <bastian.krol@instana.com>

--- packages/core/src/tracing/instrumentation/protocols/graphql.js
+++ packages/core/src/tracing/instrumentation/protocols/graphql.js
@@ -98,7 +98,7 @@ function traceQueryOrMutation(
   operationDefinition,
   operationName
 ) {
-  const activeEntrySpan = cls.getCurrentSpan();
+  const activeEntrySpan = cls.getCurrentSpan(true);
   let span;
   if (activeEntrySpan && activeEntrySpan.k === constants.ENTRY && activeEntrySpan.n !== 'graphql.server') {
     // For now, we assume that the GraphQL operation is the only relevant operation that is happening while processing
@@ -165,7 +165,13 @@ function traceSubscriptionUpdate(
   if (!isActive) {
     return originalFunction.apply(originalThis, originalArgs);
   }
-  const parentSpan = cls.getCurrentSpan() || cls.getReducedSpan();
+
+  /**
+   * We pass `fallbackToSharedContext: true` to access the GraphQL query context,
+   * which then triggered this subscription query. We need to connect them.
+   */
+  const parentSpan = cls.getCurrentSpan(true) || cls.getReducedSpan(true);
+
   if (parentSpan && !constants.isExitSpan(parentSpan) && parentSpan.t && parentSpan.s) {
     return cls.ns.runAndReturn(() => {
       const span = cls.startSpan('graphql.client', constants.EXIT, parentSpan.t, parentSpan.s);
