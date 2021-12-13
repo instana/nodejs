6edd3039a7dd38c521e270de98867726ad24df54
Katharina Irrgang <katharina.irrgang@googlemail.com>
2021-12-13 11:07:37 +0100

    chore: removed legacy context (#431)
    refs 80206
    
    Co-authored-by: kirrg001 <katharina.irrgang@gmail.com>

--- packages/core/src/tracing/clsHooked/index.js
+++ packages/core/src/tracing/clsHooked/index.js
@@ -35,15 +35,12 @@ const semver = require('semver');
 /**
  * In order to increase node version support, this loads the version of context
  * that is appropriate for the version of on nodejs that is running.
- * Node < v8 - uses AsyncWrap and async-hooks-jl
  * Node 12.17 - 16.6 - uses native AsyncLocalStorage. See below:
  * There is a bug introduced in Node 16.7 which breaks Async LS: https://github.com/nodejs/node/issues/40693
  * Node >= v8 - uses native async-hooks
  */
 if (process && semver.satisfies(process.versions.node, '12.17 - 16.6')) {
   module.exports = require('./async_local_storage_context');
-} else if (process && semver.gte(process.versions.node, '8.0.0')) {
-  module.exports = require('./context');
 } else {
-  module.exports = require('./context-legacy');
+  module.exports = require('./context');
 }
