213ed1a19cc9f52f048e73617a0d568d8a256458
kirrg001 <katharina.irrgang@gmail.com>
2022-01-03 13:20:53 +0100

    chore: added docs

--- packages/aws-lambda/src/wrapper.js
+++ packages/aws-lambda/src/wrapper.js
@@ -174,6 +174,10 @@ function shimmedHandler(originalHandler, originalThis, originalArgs, _config) {
       });
     };
 
+    /**
+     * We offer the customer to disable the timeout detection
+     * See https://github.com/instana/nodejs/pull/443
+     */
     if (!process.env.DISABLE_LAMBDA_TIMEOUT_DETECTION || process.env.DISABLE_LAMBDA_TIMEOUT_DETECTION === 'false') {
       registerTimeoutDetection(context, entrySpan);
     }
