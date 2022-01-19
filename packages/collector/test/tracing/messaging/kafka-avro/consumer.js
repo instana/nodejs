aea93055ade4d683ecf85f26e56992149adc3c74
Willian Carvalho <willian.carvalho@instana.com>
2022-01-19 16:41:26 +0100

    chore(rdkafka): instrumentation of rdkafka and kafka avro

---
+++ packages/collector/test/tracing/messaging/kafka-avro/consumer.js
@@ -0,0 +1,82 @@
+/*
+ * (c) Copyright IBM Corp. 2022
+ */
+
+'use strict';
+
+const instana = require('../../../../')();
+const KafkaAvro = require('kafka-avro');
+
+const fetch = require('node-fetch');
+const delay = require('../../../../../core/test/test_util/delay');
+const agentPort = process.env.INSTANA_AGENT_PORT || 42699;
+const { sendToParent } = require('@instana/core/test/test_util');
+const logPrefix = `Kafka Avro Consumer (${process.pid}):\t`;
+const log = require('@instana/core/test/test_util/log').getLogger(logPrefix);
+const express = require('express');
+const port = process.env.APP_PORT || 3215;
+
+const kafkaAvro = new KafkaAvro({
+  kafkaBroker: 'localhost:9092',
+  schemaRegistry: 'http://localhost:8081'
+});
+
+let isReady = false;
+
+const topicName = 'kafka-avro-topic';
+
+(async () => {
+  await kafkaAvro.init();
+
+  isReady = true;
+
+  const consumer = await kafkaAvro.getConsumer({
+    // Keep this value low (default is 5000ms) to be sure that messages won't survive between tests.
+    'auto.commit.interval.ms': 500,
+    'group.id': 'librd-test',
+    'socket.keepalive.enable': true,
+    'enable.auto.commit': true
+  });
+
+  await new Promise((resolve, reject) => {
+    consumer.on('ready', () => {
+      resolve(consumer);
+    });
+
+    consumer.connect({}, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(consumer); // depend on Promises' single resolve contract.
+    });
+  });
+
+  // Subscribe and consume.
+  consumer.subscribe([topicName]);
+
+  consumer.on('data', async rawData => {
+    const span = instana.currentSpan();
+    span.disableAutoEnd();
+    sendToParent(rawData.value);
+    log('Got message');
+
+    await delay(200);
+    await fetch(`http://127.0.0.1:${agentPort}`);
+    span.end();
+  });
+
+  consumer.consume();
+})();
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  if (isReady) {
+    res.send('ok');
+  } else {
+    res.status(500).send('Kafka Avro Consumer is not ready yet.');
+  }
+});
+
+app.listen(port, () => log(`Kafka Avro Consumer app listening on port ${port}`));
---
+++ packages/collector/test/tracing/messaging/node-rdkafka/consumer.js
@@ -0,0 +1,123 @@
+/*
+ * (c) Copyright IBM Corp. 2022
+ */
+
+'use strict';
+
+const instana = require('../../../../')();
+const fetch = require('node-fetch');
+const delay = require('../../../../../core/test/test_util/delay');
+const agentPort = process.env.INSTANA_AGENT_PORT || 42699;
+const { sendToParent } = require('@instana/core/test/test_util');
+const Kafka = require('node-rdkafka');
+const { v4: uuid } = require('uuid');
+const logPrefix = `Node rdkafka Consumer (${process.pid}):\t`;
+const log = require('@instana/core/test/test_util/log').getLogger(logPrefix);
+const express = require('express');
+const port = process.env.APP_PORT || 3215;
+const isStream = process.env.RDKAFKA_CONSUMER_AS_STREAM === 'true';
+
+const app = express();
+
+const topic = 'rdkafka-topic';
+
+function getConsumer() {
+  /** @type {Kafka.KafkaConsumer | Kafka.ConsumerStream} */
+  let _consumer;
+
+  const consumerOptions = {
+    'metadata.broker.list': '127.0.0.1:9092',
+    'group.id': uuid()
+  };
+
+  if (isStream) {
+    _consumer = Kafka.KafkaConsumer.createReadStream(
+      consumerOptions,
+      {},
+      {
+        topics: [topic],
+        streamAsBatch: true
+      }
+    );
+
+    _consumer.on('error', err => {
+      log('Consumer as stream reader error:', err);
+    });
+
+    _consumer.on('data', async data => {
+      const span = instana.currentSpan();
+      span.disableAutoEnd();
+      // "headers" may or may not be present in data.
+      // It is an array of objects {key: value}.
+      // The producer can set a value either as string or Buffer, but the response received by the consumer is always
+      // a Uint8Array
+
+      if (Array.isArray(data)) {
+        data.forEach(sendToParent);
+      } else {
+        sendToParent(data);
+      }
+
+      log('Got message');
+
+      await delay(200);
+      await fetch(`http://127.0.0.1:${agentPort}`);
+      span.end();
+    });
+  } else {
+    _consumer = new Kafka.KafkaConsumer(consumerOptions);
+
+    _consumer.connect();
+
+    _consumer
+      .on('ready', () => {
+        log('Ready.');
+        _consumer.subscribe([topic]);
+
+        // Consume from the rdkafka-topic. This is what determines
+        // the mode we are running in. By not specifying a callback (or specifying
+        // only a callback) we get messages as soon as they are available.
+        _consumer.consume();
+
+        // Even if we use this, messages are received one by one.
+        // This basically means: wait until we have 5 messages and consume them
+        // So we can assure that the consumer as standard api (not as stream) will always spit 1 message at a time.
+        // setInterval(function () {
+        //   _consumer.consume(5);
+        // }, 100);
+      })
+      .on('data', async data => {
+        const span = instana.currentSpan();
+        span.disableAutoEnd();
+        // "headers" may or may not be present in data.
+        // it is an array of objects {key: value}.
+        // the producer can set a value either as string or Buffer, but the response received by the consumer is always
+        // a Uint8Array
+        sendToParent(data);
+        log('Got message');
+        await delay(200);
+        await fetch(`http://127.0.0.1:${agentPort}`);
+        span.end();
+      });
+
+    _consumer.on('event.error', err => {
+      log('Consumer as standard api error:', err);
+    });
+  }
+
+  return _consumer;
+}
+
+const consumer = getConsumer();
+
+app.get('/', (_req, res) => {
+  if (consumer.read || (consumer.isConnected && consumer.isConnected())) {
+    res.send('ok');
+  } else {
+    res.status(500).send('rdkafka Consumer is not ready yet.');
+  }
+});
+
+app.listen(port, () =>
+  log(`rdkafka Consumer (as ${isStream ? 'Stream' : 'Standard API'}) app listening on port ${port}`)
+);
