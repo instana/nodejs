97d2caf9e91c2d776df093cb7cb04d564c89b9f7
Bastian Krol <bastian.krol@instana.com>
2022-01-31 19:49:05 +0100

    feat(fargate): detect Node.js version, use matching @instana/aws-fargate version

--- packages/aws-fargate/images/instana-aws-fargate/setup.sh
+++ packages/aws-fargate/images/instana-aws-fargate/setup.sh
@@ -1,8 +1,8 @@
-cd /instana
-npm rebuild || echo "Warning: Rebuilding native add-ons for @instana/fargate failed. Monitoring the Fargate tasks will work nonetheless, but you will miss some Node.js metrics (GC metrics, event loop metrics, ...). See https://www.ibm.com/docs/de/obi/current?topic=agents-monitoring-aws-fargate#build-dependencies-and-native-add-ons for details."
 #######################################
 # (c) Copyright IBM Corp. 2021
 # (c) Copyright Instana Inc. and contributors 2020
 #######################################
 
+cd /instana
+npm rebuild || echo "Warning: Rebuilding native add-ons for @instana/fargate failed. Monitoring the Fargate tasks will work nonetheless, but you will miss some Node.js metrics (GC metrics, event loop metrics, ...). See https://www.ibm.com/docs/de/obi/current?topic=agents-monitoring-aws-fargate#build-dependencies-and-native-add-ons for details."
 
