0d1f9557d650aad45673c2caaa22b8906b9b29d3
Bastian Krol <bastian.krol@instana.com>
2022-01-31 19:49:05 +0100

    fix(fargate): detect Node.js version, use matching @instana/aws-fargate version

--- packages/aws-fargate/src/index.js
+++ packages/aws-fargate/src/index.js
@@ -1,32 +1,44 @@
 /*
- * (c) Copyright IBM Corp. 2021
- * (c) Copyright Instana Inc. and contributors 2020
+ * (c) Copyright IBM Corp. 2022
  */
 
 'use strict';
 
-const { isNodeJsTooOld, minimumNodeJsVersion } = require('@instana/core/src/util/nodeJsVersionCheck');
+// MAINTENANCE NOTE: All code in this file needs to be compatible with all Node.js versions >= 6.0.0.
 
-if (isNodeJsTooOld()) {
+try {
+  const nodeJsVersion = process.version;
+  if (typeof nodeJsVersion !== 'string') {
+    return;
+  }
+  const matchResult = /v(\d+)\.(\d+)\.(\d+)/.exec(nodeJsVersion);
+  if (!matchResult || matchResult.length < 4) {
+    return;
+  }
+  const majorVersion = parseInt(matchResult[1], 10);
+  if (majorVersion == null || isNaN(majorVersion)) {
+    return;
+  }
+  if (majorVersion >= 10) {
+    module.exports = exports = require('./preactivate.js');
+    return;
+  } else if (majorVersion >= 6 && majorVersion < 10) {
+    // Maintenance note: As long as we do not backport the changes to index.js and the additional preactivate.js file to
+    // the 1.x branch, this needs to point to:
+    //   /instana/legacy-1x/node_modules/@instana/aws-fargate/src/index
+    // Because in 1.x, src/index either delegates to src/activate or src/noop.
+    //
+    // If we ever include those changes into the 1.x version (which we should not), we would need to point to:
+    //   /instana/legacy-1x/node_modules/@instana/aws-fargate/src/preactivate
+    // Otherwise we would get into an endless require loop.
+    //
+    // eslint-disable-next-line instana/no-unsafe-require
+    module.exports = exports = require('/instana/legacy-1x/node_modules/@instana/aws-fargate/src/index');
+    return;
+  }
+  // Node.js versions < 6 are unsupported and will not be instrumented at all.
+} catch (e) {
+  // ignore all runtime errors
   // eslint-disable-next-line no-console
-  console.error(
-    `The package @instana/aws-fargate requires at least Node.js ${minimumNodeJsVersion} but this process is ` +
-      `running on Node.js ${process.version}. This Fargate container will not be monitored by Instana.`
-  );
-  return;
-}
-
-const { util: coreUtil } = require('@instana/core');
-const { environment: environmentUtil } = require('@instana/serverless');
-
-const isExcludedFromInstrumentation = coreUtil.excludedFromInstrumentation && coreUtil.excludedFromInstrumentation();
-
-if (!isExcludedFromInstrumentation) {
-  environmentUtil.validate();
-}
-
-if (!isExcludedFromInstrumentation && environmentUtil.isValid()) {
-  module.exports = exports = require('./activate');
-} else {
-  module.exports = exports = require('./noop');
+  console.error(e);
 }
