432ec778a4470e3967f101ede6f03345c6132646
Bastian Krol <bastian.krol@instana.com>
2022-02-01 10:06:27 +0100

    feat(google-cloud-run): detect Node.js version, use matching @instana/google-cloud-run version

---
+++ packages/google-cloud-run/src/preactivate.js
@@ -0,0 +1,32 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. and contributors 2020
+ */
+
+'use strict';
+
+const { isNodeJsTooOld, minimumNodeJsVersion } = require('@instana/core/src/util/nodeJsVersionCheck');
+
+if (isNodeJsTooOld()) {
+  // eslint-disable-next-line no-console
+  console.error(
+    `The package @instana/google-cloud-run requires at least Node.js ${minimumNodeJsVersion} but this process is ` +
+      `running on Node.js ${process.version}. This Google Cloud Run service will not be monitored by Instana.`
+  );
+  return;
+}
+
+const { util: coreUtil } = require('@instana/core');
+const { environment: environmentUtil } = require('@instana/serverless');
+
+const isExcludedFromInstrumentation = coreUtil.excludedFromInstrumentation && coreUtil.excludedFromInstrumentation();
+
+if (!isExcludedFromInstrumentation) {
+  environmentUtil.validate();
+}
+
+if (!isExcludedFromInstrumentation && environmentUtil.isValid()) {
+  module.exports = exports = require('./activate');
+} else {
+  module.exports = exports = require('./noop');
+}
