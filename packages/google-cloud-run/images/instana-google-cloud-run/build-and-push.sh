9bcf368cb326439803910a0104eb854335c9d127
Bastian Krol <bastian.krol@instana.com>
2022-03-11 19:08:34 +0100

    chore(google-cloud-run): clean up local build scripts, add documentation
    This change contains some cleanup of the build scripts used for testing
    the Instana Node.js Google Cloud Run image and adds extensive documentation how
    to use the scripts.
    
    Also, two script that were not that useful (and also somewhat
    unmaintained) have been removed (test-images/build-and-push-all.sh and
    test-images/restart.sh).
    
    [skip ci]

--- packages/google-cloud-run/images/instana-google-cloud-run/build-and-push.sh
+++ packages/google-cloud-run/images/instana-google-cloud-run/build-and-push.sh
@@ -5,9 +5,26 @@
 # (c) Copyright Instana Inc. and contributors 2020
 #######################################
 
-# This is a local script for testing the images.
-# We use serverless/ci/pipeline.yml to publish the images. 
+# This script is only used locally to build and push test versions of the container image. The production image on
+# icr.io is built and published via Concourse, see serverless/ci/pipeline.yml. However, note that the files
+# Dockerfile-npm, package.json.npm and setup.sh in this directory _are_ actually used for the production image.
+#
+# The use cases for this script are:
+# * Built a Google Cloud Run base container image from your local sources (including all local modifications), and
+# * Building a base container image from an npm image that does not have the dist tag "latest", for example a
+#   release candidate npm package tagged with "next".
+# * Testing changes in the Dockerfile or package.json used for building the base container image.
+#
+# This script will upload the image directly to a remote Docker registry after building it. By default, we use a
+# Google Cloud containerregistry (this can be configured via .env).
 
+# ##############
+# # Parameters #
+# ##############
+#
+# See ./build.sh for a detailed description of the parameters that this script accepts.
+
+# use -eox to see better output
 set -eo pipefail
 
 cd `dirname $BASH_SOURCE`
@@ -34,14 +51,21 @@ if [[ -z "${build_mode-}" ]]; then
   build_mode=local
 fi
 
-./build.sh $build_mode
+npm_tag=$2
 
-setImageTag $image_tag_prefix $build_mode
+./build.sh $build_mode $npm_tag
+
+setImageTag $image_tag_prefix $build_mode $npm_tag
 
 echo "Pushing image $image_tag to $gcr_repository"
 docker push $gcr_repository/$image_tag
 
 if [[ $build_mode = npm ]]; then
-  package_version=$(npm show @instana/google-cloud-run version)
-  docker push $gcr_repository/$image_tag:$package_version
+  if [[ -n $npm_tag ]]; then
+    package_version=$(npm show @instana/google-cloud-run@$npm_tag version)
+  else
+    package_version=$(npm show @instana/google-cloud-run version)
+  fi
+  echo npm package version: $package_version
+  docker push $gcr_repository/$image_tag_without_version:$package_version
 fi
