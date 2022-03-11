fd9f9ccd6d804ecdf500fc409d078ac80e682310
Bastian Krol <bastian.krol@instana.com>
2022-03-11 19:08:34 +0100

    chore(google-cloud-run): clean up local build scripts, add documentation
    This change contains some cleanup of the build scripts used for testing
    the Instana Node.js Google Cloud Run image and adds extensive documentation how
    to use the scripts.
    
    Also, two script that were not that useful (and also somewhat
    unmaintained) have been removed (test-images/build-and-push-all.sh and
    test-images/restart.sh).
    
    [skip ci]

--- packages/google-cloud-run/images/test-images/build-and-push.sh
+++ packages/google-cloud-run/images/test-images/build-and-push.sh
@@ -5,13 +5,25 @@
 # (c) Copyright Instana Inc. and contributors 2020
 #######################################
 
+# This script builds and pushes a test image that can be used as a Google Cloud Run service. You can either the Instanan Node.js
+# Google Cloud Run base image from one of various sources (published production image, image from your local Docker registry,
+# image from an GCR registry with pre-release images).
+
+# ##############
+# # Parameters #
+# ##############
+#
+# ./build-and-push.sh <instana-layer-mode> <node-js-version> <linux-distribution> <docker-tag>
+#
+# See ./build.sh for a more details description of the parameters that this script accepts.
+
 set -eo pipefail
 
 cd `dirname $BASH_SOURCE`
 
 source utils
 
-normalizeArgs $1 $2 $3
+normalizeArgs $1 $2 $3 $4
 
 if [[ ! -f .env ]]; then
   echo .env file is missing
@@ -28,9 +40,9 @@ if [[ -z "${container_name_prefix-}" ]]; then
   exit 1
 fi
 
-./build.sh $INSTANA_LAYER_MODE $NODEJS_VERSION $LINUX_DISTRIBUTION
+./build.sh $INSTANA_LAYER_MODE $NODEJS_VERSION $LINUX_DISTRIBUTION $DOCKER_TAG
 
-setImageTag $image_tag_prefix $NODEJS_VERSION $LINUX_DISTRIBUTION $INSTANA_LAYER_MODE
+setImageTag $image_tag_prefix $NODEJS_VERSION $LINUX_DISTRIBUTION $INSTANA_LAYER_MODE $DOCKER_TAG
 
 echo "Pushing image $image_tag to $gcr_repository"
 docker push $gcr_repository/$image_tag
--- packages/google-cloud-run/images/test-images/build.sh
+++ packages/google-cloud-run/images/test-images/build.sh
@@ -5,24 +5,47 @@
 # (c) Copyright Instana Inc. and contributors 2020
 #######################################
 
-set -eo pipefail
 
-cd `dirname $BASH_SOURCE`
+# This script builds a test image that can be used as a Google Cloud Run service. You can either use the Instana
+# Node.js Google Cloud Run base image from one of various sources (published production image, image from your local
+# Docker registry, image from a GCR registry with pre-release images). You would usually not call this script
+# directly but either use ./build-and-push.sh to directly push the built image to a registry to use it in an actual
+# Cloud Run service or use ./build-and-run.sh to run it locally in a simulated Cloud Run environment.
 
-source utils
-
+# ##############
+# # Parameters #
+# ##############
+#
 # $1: Instana Layer Mode, aka which Docker base image layer to use. One of:
-#     - released      -> public IBM container registry base image
-#     - authenticated -> containers.io base image
-#     - local         -> local Docker base image
+#     - released      -> use an official production image from the public IBM container registry (icr.io)
+#     - authenticated -> use an official production image from Instana's own registry (containers.instana.io);
+#                        these are the very same images that are available from icr.io, but containers.instana.io
+#                        requires authentication. See https://www.ibm.com/docs/en/obi/current?topic=agents-monitoring-google-cloud-run#getting-the-nodejs-layer-from-containersinstanaio
+#     - local         -> use a local Docker base image
+#     - gcr           -> use an image from the GCR registry with test base images
 # $2: Node.js version. One of:
 #     - 12
 #     - 10
 #     - 8
 # $3: Linux distribution. One of:
 #     - standard               -> (uses node:$version, that is, Debian)
-#     - alpine                 -> (uses node:$version-alpine, that is, Alpine, and installs build dependencies)
-normalizeArgs $1 $2 $3
+#     - alpine                 -> (uses node:$version-alpine, that is, Alpine)
+# $4: Docker Tag. Use a base image with a specific Docker tag instead of ":latest". One of:
+#     - latest (this is the default)
+#     - any other Docker tag that is available in the registry you are referring to via $1/Instana Layer Mode.
+#       The most common use case would be to build a base image from a pre-release npm dist tag like "next" and publish
+#       it to our pre-release Google Cloud container registry with the tag "next" by doing
+#       packages/google-cloud-run/images/instana-google-cloud-run/build.sh npm next
+#       then using that pre-release base image here by specifying "next" for $4 as
+#       well -> packages/google-cloud-run/images/test-images/build.sh gcr 12 standard next
+
+set -eo pipefail
+
+cd `dirname $BASH_SOURCE`
+
+source utils
+
+normalizeArgs $1 $2 $3 $4
 
 if [[ ! -f .env ]]; then
   echo .env file is missing
@@ -55,8 +78,8 @@ else
   echo Not building local Instana layer.
 fi
 
-setImageTag $image_tag_prefix $NODEJS_VERSION $LINUX_DISTRIBUTION $INSTANA_LAYER_MODE
-setContainerName $container_name_prefix $NODEJS_VERSION $LINUX_DISTRIBUTION $INSTANA_LAYER_MODE
+setImageTag $image_tag_prefix $NODEJS_VERSION $LINUX_DISTRIBUTION $INSTANA_LAYER_MODE $DOCKER_TAG
+setContainerName $container_name_prefix $NODEJS_VERSION $LINUX_DISTRIBUTION $INSTANA_LAYER_MODE $DOCKER_TAG
 
 echo "Stopping and removing container $container_name"
 docker rm -f $container_name || true
@@ -64,7 +87,7 @@ docker rm -f $container_name || true
 echo "Removing image $image_tag"
 docker rmi -f $image_tag
 
-echo "Building $dockerfile -> $image_tag"
+echo "Building $dockerfile -> $image_tag (INSTANA_LAYER: $INSTANA_LAYER, NODEJS_VERSION: $NODEJS_VERSION)"
 docker build \
   --progress=plain \
   --build-arg NODEJS_VERSION=$NODEJS_VERSION \
