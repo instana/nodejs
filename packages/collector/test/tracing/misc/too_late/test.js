6914436dda984c62c13e36900d967b2464ad633f
Katharina Irrgang <katharina.irrgang@googlemail.com>
2022-03-21 12:22:06 +0100

    chore: added missing packages for initializedTooLateHeuristic and optimised tests (#506)
    no issue
    
    - added unit test
       - integration test runs 26s
    - if you now add a new instrumentation, the test will fail because you need to add the package to the heuristic fn

--- packages/collector/test/tracing/misc/too_late/test.js
+++ packages/collector/test/tracing/misc/too_late/test.js
@@ -25,117 +25,78 @@ const mochaSuiteFn =
 
 mochaSuiteFn('tracing/too late', function () {
   this.timeout(config.getTestTimeout());
-
   globalAgent.setUpCleanUpHooks();
 
-  [
-    semver.gte(process.version, '10.0.0') && '@elastic/elasticsearch',
-    semver.gte(process.version, '10.0.0') && '@google-cloud/pubsub',
-    semver.gte(process.version, '10.0.0') && '@google-cloud/storage',
-    '@hapi/call',
-    'amqplib',
-    'aws-sdk',
-    'bluebird',
-    'cls-hooked',
-    'elasticsearch',
-    'express',
-    'fastify',
-    'graphql',
-    'graphql-subscriptions',
-    'grpc',
-    'ioredis',
-    'kafka-node',
-    semver.gte(process.version, '10.0.0') && 'kafkajs',
-    'koa-router',
-    'log4js',
-    'mongodb',
-    semver.gte(process.version, '10.0.0') && 'mssql',
-    'mysql',
-    'mysql2',
-    'mysql2',
-    'nats',
-    'node-nats-streaming',
-    'pg',
-    'pg-native',
-    'pino',
-    'redis',
-    'request',
-    'superagent',
-    'winston'
-  ]
-    .filter(moduleName => !!moduleName)
-    .forEach(moduleName => registerTooLateTest.bind(this)(moduleName));
+  const EXAMPLE_MODULE = 'redis';
 
-  function registerTooLateTest(moduleName) {
-    describe(`@instana/collector is initialized too late (${moduleName})`, function () {
-      const controls = new ProcessControls({
-        dirname: __dirname,
-        useGlobalAgent: true,
-        env: {
-          REQUIRE_BEFORE_COLLECTOR: moduleName
-        }
-      }).registerTestHooks();
+  describe(`@instana/collector is initialized too late (choosing ${EXAMPLE_MODULE} as an example)`, function () {
+    const controls = new ProcessControls({
+      dirname: __dirname,
+      useGlobalAgent: true,
+      env: {
+        REQUIRE_BEFORE_COLLECTOR: EXAMPLE_MODULE
+      }
+    }).registerTestHooks();
 
-      it(`should warn when module ${moduleName} has been require before @instana/collector`, () =>
-        controls
-          .sendRequest({
-            path: '/'
-          })
-          .then(() =>
-            testUtils.retry(() =>
-              Promise.all([
-                agentControls.getSpans(),
-                agentControls.getMonitoringEvents(),
-                agentControls.getAllMetrics(controls.getPid())
-              ]).then(([spans, monitoringEvents, metrics]) => {
-                // expect HTTP entry to be captured
-                testUtils.expectAtLeastOneMatching(spans, [
-                  span => expect(span.n).to.equal('node.http.server'),
-                  span => expect(span.k).to.equal(constants.ENTRY),
-                  span => expect(span.async).to.not.exist,
-                  span => expect(span.error).to.not.exist,
-                  span => expect(span.ec).to.equal(0),
-                  span => expect(span.p).to.not.exist,
-                  span => expect(span.data.http.method).to.equal('GET'),
-                  span => expect(span.data.http.url).to.equal('/')
-                ]);
+    it(`should warn when module ${EXAMPLE_MODULE} has been require before @instana/collector`, () =>
+      controls
+        .sendRequest({
+          path: '/'
+        })
+        .then(() =>
+          testUtils.retry(() =>
+            Promise.all([
+              agentControls.getSpans(),
+              agentControls.getMonitoringEvents(),
+              agentControls.getAllMetrics(controls.getPid())
+            ]).then(([spans, monitoringEvents, metrics]) => {
+              // expect HTTP entry to be captured
+              testUtils.expectAtLeastOneMatching(spans, [
+                span => expect(span.n).to.equal('node.http.server'),
+                span => expect(span.k).to.equal(constants.ENTRY),
+                span => expect(span.async).to.not.exist,
+                span => expect(span.error).to.not.exist,
+                span => expect(span.ec).to.equal(0),
+                span => expect(span.p).to.not.exist,
+                span => expect(span.data.http.method).to.equal('GET'),
+                span => expect(span.data.http.url).to.equal('/')
+              ]);
 
-                // expect HTTP exit to not be captured
-                const httpExits = testUtils.getSpansByName(spans, 'node.http.client');
-                expect(httpExits).to.have.lengthOf(0);
+              // expect HTTP exit to not be captured
+              const httpExits = testUtils.getSpansByName(spans, 'node.http.client');
+              expect(httpExits).to.have.lengthOf(0);
 
-                // expect the initialized-too-late monitoring event to have been fired
-                expect(monitoringEvents).to.deep.include(
-                  {
-                    plugin: 'com.instana.forge.infrastructure.runtime.nodejs.NodeJsRuntimePlatform',
-                    pid: controls.getPid(),
-                    code: 'nodejs_collector_initialized_too_late',
-                    duration: 660000,
-                    category: 'TRACER'
-                  },
-                  JSON.stringify(monitoringEvents)
-                );
+              // expect the initialized-too-late monitoring event to have been fired
+              expect(monitoringEvents).to.deep.include(
+                {
+                  plugin: 'com.instana.forge.infrastructure.runtime.nodejs.NodeJsRuntimePlatform',
+                  pid: controls.getPid(),
+                  code: 'nodejs_collector_initialized_too_late',
+                  duration: 660000,
+                  category: 'TRACER'
+                },
+                JSON.stringify(monitoringEvents)
+              );
 
-                // expect initTooLate to have been recorded via snapshot data too (until that mechanism is removed)
-                let initTooLateFound = false;
-                metrics.forEach(m => {
-                  if (m && m.data) {
-                    if (m.data.initTooLate === true) {
-                      initTooLateFound = true;
-                    } else if (m.data.initTooLate !== undefined) {
-                      fail(
-                        `Found invalid value (${m.data.initTooLate}, type: ${typeof m.data
-                          .initTooLate}) for initTooLate metric, should be either undefined or true.`
-                      );
-                    }
+              // expect initTooLate to have been recorded via snapshot data too (until that mechanism is removed)
+              let initTooLateFound = false;
+              metrics.forEach(m => {
+                if (m && m.data) {
+                  if (m.data.initTooLate === true) {
+                    initTooLateFound = true;
+                  } else if (m.data.initTooLate !== undefined) {
+                    fail(
+                      `Found invalid value (${m.data.initTooLate}, type: ${typeof m.data
+                        .initTooLate}) for initTooLate metric, should be either undefined or true.`
+                    );
                   }
-                });
-                expect(initTooLateFound).to.be.true;
-              })
-            )
-          ));
-    });
-  }
+                }
+              });
+              expect(initTooLateFound).to.be.true;
+            })
+          )
+        ));
+  });
 
   describe('@instana/collector is initialized properly', () => {
     const controls = new ProcessControls({
---
+++ packages/core/test/util/initializedTooLateHeuristic_test.js
@@ -0,0 +1,78 @@
+/*
+ * (c) Copyright IBM Corp. 2022
+ */
+
+'use strict';
+
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const path = require('path');
+const requireHook = require('../../src/util/requireHook');
+const initializedTooLateHeurstic = require('../../src/util/initializedTooLateHeuristic');
+
+describe('[UNIT] util.initializedTooLateHeurstic', () => {
+  const instrumentedModules = [];
+
+  before(() => {
+    sinon.stub(requireHook, 'onFileLoad').callsFake(function fake(m) {
+      instrumentedModules.push(m);
+    });
+    sinon.stub(requireHook, 'onModuleLoad').callsFake(function fake(m) {
+      instrumentedModules.push(m);
+    });
+
+    require.cache['/@contrast/agent/'] = {};
+
+    const tracing = require('../../src/');
+    tracing.init();
+  });
+
+  after(() => {
+    sinon.restore();
+    delete require.cache['/@contrast/agent/'];
+  });
+
+  beforeEach(() => {
+    initializedTooLateHeurstic.reset();
+  });
+
+  it('hasBeenInitializedTooLate is false', () => {
+    expect(initializedTooLateHeurstic()).to.be.false;
+  });
+
+  it('hasBeenInitializedTooLate is false', () => {
+    const p = '/Users/myuser/dev/instana/nodejs/node_modules/nope';
+    require.cache[p] = {};
+    expect(initializedTooLateHeurstic()).to.be.false;
+    delete require.cache[p];
+  });
+
+  it('hasBeenInitializedTooLate is true', () => {
+    instrumentedModules.forEach(moduleName => {
+      // eslint-disable-next-line no-console
+      console.log(`for module ${moduleName}`);
+
+      initializedTooLateHeurstic.reset();
+
+      let resolvedPath = moduleName;
+
+      try {
+        resolvedPath = require.resolve(moduleName);
+      } catch (err) {
+        resolvedPath = path.resolve(resolvedPath.toString().replace(/\\\/?/g, '/'));
+      }
+
+      require.cache[resolvedPath] = {};
+      const exclude = ['bluebird', 'bunyan', 'winston'];
+      const excluded = exclude.filter(n => resolvedPath.indexOf(n) !== -1);
+
+      if (excluded.length) {
+        expect(initializedTooLateHeurstic()).to.be.false;
+      } else {
+        expect(initializedTooLateHeurstic()).to.be.true;
+      }
+
+      delete require.cache[resolvedPath];
+    });
+  });
+});
