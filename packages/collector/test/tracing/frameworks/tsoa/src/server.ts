479a3f60f34c87828d2db1d515723f2aa9b076f7
kirrg001 <katharina.irrgang@gmail.com>
2022-03-23 12:57:06 +0100

    Revert "test: added tsoa framework to integration tests (#492)"
    This reverts commit fbe04d53dc5ed92ca31c716735d6f4fa81a09a53.

--- packages/collector/test/tracing/frameworks/tsoa/src/server.ts
+++
@@ -1,6 +0,0 @@
-require('../../../../../../')();
-import { app } from './app';
-
-const port = process.env.APP_PORT || 3000;
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
