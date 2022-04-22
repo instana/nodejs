5126cbf23f8ecf7db4d3c456ce0501f86f69f9e4
kirrg001 <katharina.irrgang@gmail.com>
2022-04-22 11:13:25 +0200

    test: experimental

---
+++ bin/install-optional-dev-dependencies.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#######################################
+# (c) Copyright IBM Corp. 2022
+#######################################
+
+# NPM does not offer optional dev dependencies, see https://github.com/npm/npm/issues/3870
+# When using optional dependencies, they are counted as "prod" dependency.
+# We run really quickly into audit errors for older packages.
+# This is our own implementation of having "optionalDevDependencies".
+# Requirement: grep@3
+
+set -eo pipefail
+
+cd `dirname $BASH_SOURCE`/..
+
+commands=$(jq -r '.optionalDevDependencies | to_entries[] | "npm install --save-dev \(.key)@\(.value.version)#\(.value.engine),"' ./package.json)
+IFS=$',';
+
+cp package.json package.json.tmp
+cp package-lock.json package-lock.json.tmp
+
+for cmd in $commands; do
+    engine=$(echo "$cmd" | grep -Eo "#(.*)"| cut -d# -f2)
+    cmd=$(echo "$cmd" | grep -o "^.*#" | cut -d# -f1)
+    nodeversion=$(node -v | grep -o "v[0-9][0-9]" | cut -dv -f2)
+    enginecheck="$nodeversion $engine"
+    result=$(echo $(( $enginecheck )))
+
+    if [ ! $result -eq 0 ]; then
+        echo "Executing $cmd..."
+        eval $cmd || true
+    fi
+done
+
+mv package.json.tmp package.json
+mv package-lock.json.tmp package-lock.json
