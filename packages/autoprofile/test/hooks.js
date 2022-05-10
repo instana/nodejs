c89d41f16652236efd8f332b3167b9ec32c01f5f
Bastian Krol <bastian.krol@instana.com>
2022-05-10 15:12:22 +0200

    refactor(test): use Mocha root hook plug-ins instead of root hooks
    See https://mochajs.org/#root-hook-plugins.

---
+++ packages/autoprofile/test/hooks.js
@@ -0,0 +1,37 @@
+/*
+ * (c) Copyright IBM Corp. 2022
+ */
+
+'use strict';
+
+// This file defines global (root level) before and after hooks, see https://mochajs.org/#root-hook-plugins.
+// This module needs to be pre-required via Mocha's --require parameter when running the tests in this package. The npm
+// scripts for running tests take care of that.
+//
+// In particular, these hook sets up the global profiler instance that is used in some tests.
+
+const AutoProfiler = require('../lib/auto_profiler').AutoProfiler;
+
+exports.mochaHooks = {
+  beforeEach() {
+    global.profiler = new AutoProfiler();
+
+    global.profiler.sendProfiles = function (profiles, callback) {
+      callback();
+    };
+
+    global.profiler.getExternalPid = function () {
+      return '123';
+    };
+
+    global.profiler.start({
+      debug: true,
+      disableTimers: true
+    });
+  },
+
+  afterEach() {
+    global.profiler.destroy();
+    global.profiler = undefined;
+  }
+};
