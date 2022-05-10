e47fccadafd15e6602bcf1e384d21324347465e1
Bastian Krol <bastian.krol@instana.com>
2022-05-10 15:12:22 +0200

    refactor(test): use Mocha root hook plug-ins instead of root hooks
    See https://mochajs.org/#root-hook-plugins.

---
+++ packages/shared-metrics/test/hooks.js
@@ -0,0 +1,24 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. and contributors 2020
+ */
+
+'use strict';
+
+// This file defines global (root level) before and after hooks, see https://mochajs.org/#root-hook-plugins.
+// This module needs to be pre-required via Mocha's --require parameter when running the tests in this package. The npm
+// scripts for running tests take care of that.
+//
+// The globalAgent module manages an agent stub instance that can be used globally for all tests.
+
+const { startGlobalAgent, stopGlobalAgent } = require('../../collector/test/globalAgent');
+
+exports.mochaHooks = {
+  async beforeAll() {
+    await startGlobalAgent();
+  },
+
+  async afterAll() {
+    await stopGlobalAgent();
+  }
+};
