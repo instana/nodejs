4a58c5149cf808d6ab9e5919d2a89106fabf2218
kirrg001 <katharina.irrgang@instana.com>
2022-08-10 08:48:42 +0200

    fix: package.json not being found
    refs #581
    
    - replaced deprecated usage of process.mainModule (this is not the main fix, just a replacement)
    - https://nodejs.org/api/process.html#processmainmodule
    - the main fix is to fallback reading the process.argv for the following case:
      - application uses ES modules and preloads a commonJS file using `--require`
      - in this preload file the Instana collector is required
      - package.json & existing fallback solutions did not work

---
+++ packages/shared-metrics/test/cjs-require-in-preload/test.js
@@ -0,0 +1,59 @@
+/*
+ * (c) Copyright IBM Corp. 2022
+ */
+
+'use strict';
+
+const path = require('path');
+const _ = require('lodash');
+const expect = require('chai').expect;
+
+const testUtils = require('@instana/core/test/test_util');
+const config = require('@instana/core/test/config');
+
+const ProcessControls = require('../../../collector/test/test_util/ProcessControls');
+
+describe('cjs require collector in preload phase', function () {
+  this.timeout(config.getTestTimeout());
+
+  const controls = new ProcessControls({
+    useGlobalAgent: true,
+    cwd: path.join(__dirname, 'module'),
+    appPath: path.join(__dirname, 'module', 'src', 'app'),
+    env: {
+      NODE_OPTIONS: '--require ./load-instana.js'
+    }
+  });
+
+  ProcessControls.setUpHooks(controls);
+
+  it('should be able to find package.json', async () => {
+    await controls.sendRequest({
+      method: 'GET',
+      path: '/metrics'
+    });
+
+    await testUtils.retry(() =>
+      controls.agentControls.getAllMetrics(controls.getPid()).then(metrics => {
+        const name = findMetric(metrics, ['name']);
+        expect(name).to.equal('@instana/shared-metrics');
+      })
+    );
+  });
+});
+
+/**
+ * Find a particular metric in all collected metrics.
+ *
+ * @param {Object[]} allMetrics All collected metrics
+ * @param {string[]} _path The path to the metric in question
+ */
+function findMetric(allMetrics, _path) {
+  for (let i = allMetrics.length - 1; i >= 0; i--) {
+    const value = _.get(allMetrics[i], ['data'].concat(_path));
+    if (value !== undefined) {
+      return value;
+    }
+  }
+  return undefined;
+}
