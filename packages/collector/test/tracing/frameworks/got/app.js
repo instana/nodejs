f2507c3a458e143ba0bcb2f44f392171b9569907
kirrg001 <katharina.irrgang@instana.com>
2022-11-02 09:48:49 +0100

    chore: added test coverage for got library
    refs #68389

---
+++ packages/collector/test/tracing/frameworks/got/app.js
@@ -0,0 +1,52 @@
+/*
+ * (c) Copyright IBM Corp. 2022
+ */
+
+/* eslint-disable no-console */
+
+'use strict';
+
+require('../../../..')();
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+/**
+ * NOTE: got v12 has dropped support for commonjs.
+ *       It is possible to dynamically import a ESM module,
+ *       but we simply stay on v11 to proof that got requests are auto-instrumented.
+ *
+ * (async () => {
+ *   await import('got');
+ * })();
+ */
+const got = require('got');
+const app = express();
+const logPrefix = `Got App (${process.pid}):\t`;
+
+if (process.env.WITH_STDOUT) {
+  app.use(morgan(`${logPrefix}:method :url :status`));
+}
+
+app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get('/request', async (req, res) => {
+  await got.get('https://www.instana.com');
+
+  res.json();
+});
+
+app.listen(process.env.APP_PORT, () => {
+  log(`Listening on port: ${process.env.APP_PORT}`);
+});
+
+function log() {
+  const args = Array.prototype.slice.call(arguments);
+  args[0] = logPrefix + args[0];
+  console.log.apply(console, args);
+}
