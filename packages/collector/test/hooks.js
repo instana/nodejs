ca2d24da096723b28d678fd772c78528329602bb
Bastian Krol <bastian.krol@instana.com>
2022-11-09 08:59:05 +0100

    ci: log timestamp before each test in @instana/collector on CI

--- packages/collector/test/hooks.js
+++ packages/collector/test/hooks.js
@@ -13,12 +13,37 @@
 
 const { startGlobalAgent, stopGlobalAgent } = require('./globalAgent');
 
+const isCI = require('@instana/core/test/test_util/is_ci');
+
 exports.mochaHooks = {
   async beforeAll() {
+    // eslint-disable-next-line no-console
+    console.log(`@instana/collector test suite starting at ${timestamp()}.`);
     await startGlobalAgent();
+  },
+
+  beforeEach() {
+    if (isCI()) {
+      // Troubleshooting issues on CI often involves timing-based questions like:
+      // * Why was this test application not able to connect to the database container? Did the DB container take too
+      //   long to start up?
+      // * Why has this CircleCI executor been killed during npm install before tests even started?
+      //
+      // Answering these questions is made a lot harder than necessary due to the lack of timestamps in the test output.
+      // This hook will print the current time at the start of each test, hopefully aiding troubleshooting these pesky
+      // flaky CI tests.
+
+      // eslint-disable-next-line no-console
+      console.log(`${this.currentTest.title} -- ${timestamp()}`);
+    }
   },
 
   async afterAll() {
     await stopGlobalAgent();
   }
 };
+
+function timestamp() {
+  const d = new Date();
+  return `UTC: ${d.toISOString()} (${d.getTime()})`;
+}
