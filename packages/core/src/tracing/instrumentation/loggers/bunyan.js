d0f16d136eaa5695fdf4128314a9c34a03e2a50b
Katharina Irrgang <katharina.irrgang@instana.com>
2022-12-07 10:24:56 +0100

    fix(collector): improved capturing object logging via bunyan (#664)

--- packages/core/src/tracing/instrumentation/loggers/bunyan.js
+++ packages/core/src/tracing/instrumentation/loggers/bunyan.js
@@ -54,6 +54,8 @@ function instrumentedLog(ctx, originalLog, originalArgs, markAsError) {
     span.stack = tracingUtil.getStackTrace(instrumentedLog);
     const fields = originalArgs[0];
     let message = originalArgs[1];
+    const maxStrLength = 500;
+
     if (typeof fields === 'string') {
       message = fields;
     } else if (fields && typeof fields.message === 'string' && typeof message === 'string') {
@@ -73,10 +75,43 @@ function instrumentedLog(ctx, originalLog, originalArgs, markAsError) {
       message = `${fields.err.message} -- ${message}`;
     } else if (fields && fields.err && typeof fields.err === 'object' && typeof fields.err.message === 'string') {
       message = fields.err.message;
-    } else if (typeof message !== 'string') {
+    } else if (typeof fields === 'object') {
+      // CASE: we try our best to serialize logged objects
+      //       we do not want to directly call JSON.stringify because we do not know how big the object is and
+      //       there might be possible circular dependencies in the object.
+      const maxNoOfKeys = 100;
+      const obj = {};
+
+      Object.keys(fields).every((key, index) => {
+        // CASE: stop iteration if we have enough keys
+        if (index > maxNoOfKeys) {
+          return false;
+        }
+
+        const val = fields[key];
+        if (typeof val === 'object') {
+          obj[key] = '[Object]';
+        } else {
+          obj[key] = val;
+        }
+
+        return true;
+      });
+
+      if (message && typeof message === 'string') {
+        message = `${JSON.stringify(obj)} - ${message}`;
+      } else {
+        message = JSON.stringify(obj);
+      }
+    } else {
       message =
-        'Log call without message. The Bunyan "fields" argument will not be serialized by Instana for performance reasons.';
+        'Log call without message. The Bunyan "fields" argument cannot be serialized by Instana because of unknown format.';
     }
+
+    if (message.length > maxStrLength) {
+      message = `${message.substring(0, maxStrLength - 3)}...`;
+    }
+
     span.data.log = {
       message
     };
