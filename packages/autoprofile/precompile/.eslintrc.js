613fffbbd3aceae8227159ca5dad0af6577b3e2a
Bastian Krol <bastian.krol@ibm.com>
2023-03-08 11:13:06 +0100

    chore(autoprofile): run eslint on precompile folder as well

---
+++ packages/autoprofile/precompile/.eslintrc.js
@@ -0,0 +1,13 @@
+/*
+ * (c) Copyright IBM Corp. 2023
+ */
+
+'use strict';
+
+module.exports = {
+  rules: {
+    'no-console': 'off',
+    'max-len': 'off',
+    'instana/no-unsafe-require': 'off'
+  }
+};
