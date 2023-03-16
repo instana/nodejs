70d1e418631793dd78265d077a2e440763bbcfe7
kirrg001 <katharina.irrgang@instana.com>
2023-03-16 10:50:27 +0100

    chore: optimised host check

--- packages/core/src/tracing/instrumentation/protocols/httpClient.js
+++ packages/core/src/tracing/instrumentation/protocols/httpClient.js
@@ -116,7 +116,7 @@ function shouldBeBypassed(parentSpan, options) {
     header => header.toLowerCase().indexOf('google-api-nodejs-client') > -1
   );
 
-  const hostMatchesGPC = options && options.host && options.host.indexOf('googleapis') !== -1;
+  const hostMatchesGPC = options && options.host && options.host === 'storage.googleapis.com';
   if (isGCPNodeJSHeader && hostMatchesGPC) return true;
 
   return false;
