ce0bb00615931938c9c216f2bf4d7cdc4462f6a6
kirrg001 <katharina.irrgang@instana.com>
2023-04-17 12:08:27 +0200

    test: added integration test otel & instana
    - show how otel & instana behave when they are oth used
    
    Regarding Hapi:
    
    - root
      "@hapi/hapi": "21.x.x", (own types)
    
       submodule
    	@opentelemetry/instrumentation-hapi
    		"@hapi/hapi": "20.1.5",
    		"@types/hapi__hapi": "20.0.9" (this will be installed on the root and then is not compatible with 21.x)
    
    Installing "@types/hapi__hapi": "21.0.0" on root does not help either.

---
+++ packages/collector/test/opentelemetry/app.js
@@ -0,0 +1,84 @@
+/*
+ * (c) Copyright IBM Corp. 2023
+ */
+
+/* eslint-disable no-console */
+
+'use strict';
+
+const initOtel = () => {
+  const opentelemetry = require('@opentelemetry/sdk-node');
+  const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+  const { Resource } = require('@opentelemetry/resources');
+  const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+  const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+  const { BatchSpanProcessor } = require('@opentelemetry/tracing');
+  const nodeAutoInstrumentations = getNodeAutoInstrumentations({
+    '@opentelemetry/instrumentation-fs': {
+      enabled: false
+    }
+  });
+  const traceOtlpExporter = new OTLPTraceExporter({
+    url: `http://localhost:${process.env.SPAN_RECEIVER_PORT}/v1/traces`
+  });
+
+  const spanProcessor = new BatchSpanProcessor(traceOtlpExporter, {
+    scheduledDelayMillis: 100
+  });
+
+  const sdk = new opentelemetry.NodeSDK({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: 'my-service'
+    }),
+    spanProcessor: spanProcessor,
+    instrumentations: [nodeAutoInstrumentations]
+  });
+
+  sdk.start();
+};
+
+if (process.env.INSTANA_LOAD_FIRST === 'true') {
+  require('../../src')();
+  initOtel();
+} else {
+  initOtel();
+  require('../../src')();
+}
+
+require('mysql');
+require('mysql2');
+require('ioredis');
+require('mongoose');
+require('pg');
+require('grpc');
+require('fs');
+require('amqplib');
+require('fastify');
+require('express');
+// NOTE: this is 3.x
+//       4.x throws shimmer problems
+require('mongodb');
+
+const express = require('express');
+const port = require('../test_util/app-port')();
+const app = express();
+
+const logPrefix = `Opentelemetry Usage App (${process.pid}):\t`;
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get('/trace', (req, res) => {
+  log('Received /trace request');
+  res.send();
+});
+
+app.listen(port, () => {
+  log(`Listening on port: ${port}`);
+});
+
+function log() {
+  const args = Array.prototype.slice.call(arguments);
+  args[0] = logPrefix + args[0];
+  console.log.apply(console, args);
+}
