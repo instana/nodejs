8fec4da1881e0043434fe10b897b041bf11b2c37
Arya Mohanan <arya.mohanan@ibm.com>
2023-08-14 14:36:44 +0530

    build: aws sdk v3 kinesis missing node version check added

--- packages/collector/test/tracing/cloud/aws-sdk/v3/kinesis/util.js
+++ packages/collector/test/tracing/cloud/aws-sdk/v3/kinesis/util.js
@@ -4,54 +4,63 @@
 
 'use strict';
 
-const { KinesisClient, DescribeStreamCommand, DeleteStreamCommand } = require('@aws-sdk/client-kinesis');
+const semver = require('semver');
 
-const kinesis = new KinesisClient({ region: 'us-east-2' });
-const interval = 1000;
-const MAX_WAIT_TIME = 10000;
+const minNodeJsVersion = '14.0.0';
 
-exports.checkStreamExistence = function checkStreamExistence(streamName, expectsToExist = true) {
-  let timer = 0;
-  return new Promise(resolve => {
-    const intervalId = setInterval(async () => {
-      timer += interval;
-      if (timer >= MAX_WAIT_TIME) {
-        resolve(`Timeout after ${timer} ms`);
-        clearInterval(intervalId);
-      }
-      const command = new DescribeStreamCommand({ StreamName: streamName });
+if (semver.lt(process.versions.node, minNodeJsVersion)) {
+  exports.cleanup = async function () {
+    throw new Error(`Requires Node.js >= ${minNodeJsVersion} but this is ${process.versions.node}.`);
+  };
+} else {
+  const { KinesisClient, DescribeStreamCommand, DeleteStreamCommand } = require('@aws-sdk/client-kinesis');
+  const kinesis = new KinesisClient({ region: 'us-east-2' });
+  const interval = 1000;
+  const MAX_WAIT_TIME = 10000;
 
-      const p = await kinesis.send(command);
-      p.then(data => {
-        if (
-          expectsToExist &&
-          data &&
-          data.StreamDescriptionSummary &&
-          data.StreamDescriptionSummary.StreamStatus === 'ACTIVE'
-        ) {
-          resolve(data);
+  exports.checkStreamExistence = function checkStreamExistence(streamName, expectsToExist = true) {
+    let timer = 0;
+    return new Promise(resolve => {
+      const intervalId = setInterval(async () => {
+        timer += interval;
+        if (timer >= MAX_WAIT_TIME) {
+          resolve(`Timeout after ${timer} ms`);
           clearInterval(intervalId);
         }
-      }).catch(() => {
-        if (!expectsToExist) {
-          resolve(`Stream ${streamName} does not exist as expected`);
-          clearInterval(intervalId);
-        }
-      });
-    }, interval);
-  });
-};
+        const command = new DescribeStreamCommand({ StreamName: streamName });
 
-/**
- * Attempts to delete a previously created stream before the test starts
- * @param {string} streamName
- */
-exports.cleanup = async function (streamName) {
-  try {
-    const command = new DeleteStreamCommand({ StreamName: streamName });
-    await kinesis.send(command);
-    return exports.checkStreamExistence(streamName, false);
-  } catch (err) {
-    return Promise.resolve('Stream did not exist');
-  }
-};
+        const p = await kinesis.send(command);
+        p.then(data => {
+          if (
+            expectsToExist &&
+            data &&
+            data.StreamDescriptionSummary &&
+            data.StreamDescriptionSummary.StreamStatus === 'ACTIVE'
+          ) {
+            resolve(data);
+            clearInterval(intervalId);
+          }
+        }).catch(() => {
+          if (!expectsToExist) {
+            resolve(`Stream ${streamName} does not exist as expected`);
+            clearInterval(intervalId);
+          }
+        });
+      }, interval);
+    });
+  };
+
+  /**
+   * Attempts to delete a previously created stream before the test starts
+   * @param {string} streamName
+   */
+  exports.cleanup = async function (streamName) {
+    try {
+      const command = new DeleteStreamCommand({ StreamName: streamName });
+      await kinesis.send(command);
+      return exports.checkStreamExistence(streamName, false);
+    } catch (err) {
+      return Promise.resolve('Stream did not exist');
+    }
+  };
+}
