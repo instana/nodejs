5dcb4b1a3f9be0a751203bf7ebf71a1d78b3e88c
kirrg001 <katharina.irrgang@instana.com>
2023-09-18 12:39:09 +0200

    chore: removed empty lines

--- packages/core/src/tracing/index.js
+++ packages/core/src/tracing/index.js
@@ -41,10 +41,8 @@ let processIdentityProvider = null;
 const instrumentations = [
   './instrumentation/cloud/aws-sdk/v2/index',
   './instrumentation/cloud/aws-sdk/v3/index',
-
   './instrumentation/cloud/aws-sdk/v2/sdk',
   './instrumentation/cloud/aws-sdk/v2/sqs',
-
   './instrumentation/cloud/gcp/pubsub',
   './instrumentation/cloud/gcp/storage',
   './instrumentation/control_flow/bluebird',
