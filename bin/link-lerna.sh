c104399ae39fb73ca73ebd685ff0111cb35e55c3
kirrg001 <katharina.irrgang@instana.com>
2023-10-05 15:36:07 +0200

    ci: fix

--- bin/link-lerna.sh
+++ bin/link-lerna.sh
@@ -7,5 +7,6 @@ cd `dirname $BASH_SOURCE`/..
 node_version=$(node -v)
 
 if [[ "$(node -e "console.log((process.versions.node).split('.')[0] < 14)")" == "true" ]]; then
-  ln -s ./lernav3 ./node_modules/lerna
+  ln -fs ./lernav3 ./node_modules/lerna
+  ln -fs ../lerna/cli.js node_modules/.bin/lerna
 fi
