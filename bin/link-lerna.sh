cc4406948c5ec03f701de661cbaa690d56b2843e
kirrg001 <katharina.irrgang@instana.com>
2023-10-05 14:55:09 +0200

    ci: fix sh script

--- bin/link-lerna.sh
+++ bin/link-lerna.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
+cd `dirname $BASH_SOURCE`/..
+
 node_version=$(node -v)
 
 if [[ "$(node -e "console.log((process.versions.node).split('.')[0] < 14)")" == "true" ]]; then
-  ln -s ../node_modules/lernav3 ../node_modules/lerna
+  ln -s ./node_modules/lernav3 ./node_modules/lerna
 fi
