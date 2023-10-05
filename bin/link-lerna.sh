8c8df8658bc0062980f2d5eaa949087e24f1af3c
kirrg001 <katharina.irrgang@instana.com>
2023-10-05 17:06:23 +0200

    ci: try to remove lerna first in case it can install

--- bin/link-lerna.sh
+++ bin/link-lerna.sh
@@ -7,6 +7,7 @@ cd `dirname $BASH_SOURCE`/..
 node_version=$(node -v)
 
 if [[ "$(node -e "console.log((process.versions.node).split('.')[0] < 14)")" == "true" ]]; then
+  rm -rf node_modules/lerna
   ln -fs ./lernav3 ./node_modules/lerna
   ln -fs ../lerna/cli.js node_modules/.bin/lerna
 fi
