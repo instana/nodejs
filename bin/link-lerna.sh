31287d0dbf241890865d0439dd8d752ec397d742
kirrg001 <katharina.irrgang@instana.com>
2023-10-05 22:43:43 +0200

    ci: test

--- bin/link-lerna.sh
+++ bin/link-lerna.sh
@@ -6,8 +6,8 @@ cd `dirname $BASH_SOURCE`/..
 
 node_version=$(node -v)
 
-if [[ "$(node -e "console.log((process.versions.node).split('.')[0] < 14)")" == "true" ]]; then
+if [[ "$(node -e "console.log((process.versions.node).split('.')[0] < 16)")" == "true" ]]; then
   rm -rf node_modules/lerna
-  ln -fs ./lernav3 ./node_modules/lerna
+  ln -fs ./lernav6 ./node_modules/lerna
   ln -fs ../lerna/cli.js node_modules/.bin/lerna
 fi
