6f73f863fdd297379dd50b319f8ef863dba38b32
kirrg001 <katharina.irrgang@instana.com>
2023-10-05 14:39:50 +0200

    chore: blubb

--- bin/link-lerna.sh
+++ bin/link-lerna.sh
@@ -2,6 +2,6 @@
 
 node_version=$(node -v)
 
-if [[ "$(node -e "console.log((process.versions.node).split('.')[0] < 16)")" == "true" ]]; then
-  ln -s ../node_modules/lerna node_modules/lernav3
+if [[ "$(node -e "console.log((process.versions.node).split('.')[0] < 14)")" == "true" ]]; then
+  ln -s ../node_modules/lernav3 ../node_modules/lerna
 fi
