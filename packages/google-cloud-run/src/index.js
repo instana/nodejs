21f6298e72e000069dca0379932016697b426f8c
athira s <athiras@athiras-MacBook-Pro.local>
2023-10-30 14:23:21 +0530

    fix: adds version information for all NPM packages

--- packages/google-cloud-run/src/index.js
+++ packages/google-cloud-run/src/index.js
@@ -8,9 +8,9 @@ const path = require('path');
 // MAINTENANCE NOTE: All code in this file needs to be compatible with all Node.js versions >= 6.0.0.
 
 try {
-  const nodeJsVersion = process.version;
   // eslint-disable-next-line no-console
   console.log('@instana/google-cloud-run module version:', require(path.join(__dirname, '..', 'package.json')).version);
+  const nodeJsVersion = process.version;
   if (typeof nodeJsVersion !== 'string') {
     return;
   }
