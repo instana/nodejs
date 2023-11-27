8505bcd71c1f04ff77ff4cef932452cb7bae9aaf
kirrg001 <katharina.irrgang@instana.com>
2023-11-27 11:01:45 +0100

    build(dev): @aws-sdk/client-dynamodb-v3, @aws-sdk/client-s3-v3

--- packages/collector/test/tracing/cloud/aws-sdk/v3/dynamodb/default/dynamodb2_test.js
+++ packages/collector/test/tracing/cloud/aws-sdk/v3/dynamodb/default/dynamodb2_test.js
@@ -5,7 +5,7 @@
 'use strict';
 
 const requestMethod = 'default-style';
-const version = '@aws-sdk/client-dynamodb2';
+const version = '@aws-sdk/client-dynamodb-v3';
 
 describe('tracing/cloud/aws-sdk/v3/dynamodb', function () {
   require('../test_definition').call(this, version, requestMethod);
