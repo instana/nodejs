5c9666fefe92c97fd897c421c7381ed65b86af5d
kirrg001 <katharina.irrgang@instana.com>
2023-11-30 14:53:05 +0100

    chore: fixed and simplified nodeJsVersionCheck

--- packages/core/src/util/nodeJsVersionCheck.js
+++ packages/core/src/util/nodeJsVersionCheck.js
@@ -13,16 +13,15 @@ exports.minimumNodeJsVersion = 14;
  * Checks if the value of process.version denotes a Node.js version that is not supported, that is, older than the given
  * minimum version.
  *
- * @param {number} minimumNodeJsVersion
  * @returns {boolean} true, if and only if process.version can be parsed and is older than minimumNodeJsVersion
  */
-exports.isNodeJsTooOld = function isNodeJsTooOld(minimumNodeJsVersion = exports.minimumNodeJsVersion) {
+exports.isNodeJsTooOld = function isNodeJsTooOld() {
   const currentVersion = process.version;
   if (typeof currentVersion === 'string') {
     const majorVersionStr = process.version.split('.')[0];
     if (majorVersionStr.length > 1 && majorVersionStr.charAt(0) === 'v') {
       const majorVersion = parseInt(majorVersionStr.substring(1), 10);
-      return !isNaN(majorVersion) && majorVersion < minimumNodeJsVersion;
+      return !isNaN(majorVersion) && majorVersion < exports.minimumNodeJsVersion;
     }
   }
   return false;
