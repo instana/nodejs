1f1529ad4b146518dc8ea754d4a1a6e2c8f40d30
Arya Mohanan <arya.mohanan@ibm.com>
2023-12-18 18:14:17 +0530

    feat(aws-lambda): added support for Node.js 20 AWS Lambda runtime

--- packages/aws-lambda/layer/bin/publish-layer.sh
+++ packages/aws-lambda/layer/bin/publish-layer.sh
@@ -357,7 +357,7 @@ if [[ -z $SKIP_AWS_PUBLISH_LAYER ]]; then
           --license-info $LICENSE \
           --zip-file fileb://$ZIP_NAME \
           --output json \
-          --compatible-runtimes nodejs14.x nodejs16.x nodejs18.x \
+          --compatible-runtimes nodejs14.x nodejs16.x nodejs18.x nodejs20.x \
           | jq '.Version' \
       )
 
