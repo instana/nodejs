ddf75ccd60c7115eaec135d64482d14c48a1fc4b
kirrg001 <katharina.irrgang@instana.com>
2023-12-18 09:57:11 +0100

    chore: lint

--- packages/azure-container-services/src/identity_provider.js
+++ packages/azure-container-services/src/identity_provider.js
@@ -31,6 +31,7 @@ function extractEntityId() {
   const resourceGroup = process.env.WEBSITE_RESOURCE_GROUP;
   const appName = process.env.WEBSITE_SITE_NAME;
   if (subscriptionId && resourceGroup && appName) {
+    // eslint-disable-next-line max-len
     entityId = `/subscriptions/${subscriptionId}/resourceGroups/${resourceGroup}/providers/Microsoft.Web/sites/${appName}`;
   }
   return entityId;
