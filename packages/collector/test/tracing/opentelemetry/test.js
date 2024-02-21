8586e21de9c1dfd5d44a7844ea71901258b2e6cc
Arya <90748009+aryamohanan@users.noreply.github.com>
2024-02-21 17:27:28 +0530

    build: fix timeout issue (#1045)

--- packages/collector/test/tracing/opentelemetry/test.js
+++ packages/collector/test/tracing/opentelemetry/test.js
@@ -25,12 +25,13 @@ const {
 const ProcessControls = require('../../test_util/ProcessControls');
 const globalAgent = require('../../globalAgent');
 const DELAY_TIMEOUT_IN_MS = 500;
-
+const testTimeout = Math.max(50000, config.getTestTimeout());
+const retryTime = 30000;
 const mochaSuiteFn =
   supportedVersion(process.versions.node) && semver.gte(process.versions.node, '14.0.0') ? describe : describe.skip;
 
 mochaSuiteFn('opentelemetry/instrumentations', function () {
-  this.timeout(config.getTestTimeout());
+  this.timeout(testTimeout);
 
   describe('restify', function () {
     // No support for Node v18 yet.
@@ -502,7 +503,7 @@ mochaSuiteFn('opentelemetry/instrumentations', function () {
           }
         });
 
-        await controls.startAndWaitForAgentConnection();
+        await controls.startAndWaitForAgentConnection(retryTime);
       });
 
       after(async () => {
