8e8fb57781593b9c8235b64262a9392bfc98f8ac
Arya <90748009+aryamohanan@users.noreply.github.com>
2024-03-05 13:12:41 +0530

    test: removed the unwanted test configuration (#1064)

--- packages/opentelemetry-sampler/test/.mocharc.js
+++ packages/opentelemetry-sampler/test/.mocharc.js
@@ -3,7 +3,6 @@
 const mochaOptions = {
   ignore: ['node_modules/**/*', 'test/**/node_modules/**/*']
 };
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 if (process.env.CI) {
   // Retry failed tests once on CI.
   mochaOptions.retries = 1;
--- packages/serverless/test/util/AbstractServerlessControl.js
+++ packages/serverless/test/util/AbstractServerlessControl.js
@@ -14,7 +14,9 @@ const fetch = require('node-fetch');
 
 const retry = require('@instana/core/test/test_util/retry');
 const config = require('../config');
-
+// To address the certificate authorization issue with node-fetch, process.env.NODE_TLS_REJECT_UNAUTHORIZED
+// was set to '0'. Refer to the problem discussed in https://github.com/node-fetch/node-fetch/issues/19
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 function AbstractServerlessControl(opts = {}) {
   this.opts = opts;
   this.opts.timeout = this.opts.timeout || config.getTestTimeout();
