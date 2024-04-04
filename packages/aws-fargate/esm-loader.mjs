a80c6619215879f6890b13b19a12635120713cf3
Arya Mohanan <arya.mohanan@ibm.com>
2024-04-04 21:17:41 +0530

    docs: updated the comments

--- packages/aws-fargate/esm-loader.mjs
+++ packages/aws-fargate/esm-loader.mjs
@@ -6,20 +6,20 @@
 
 /**
  * IMPORTANT NOTE: From Node.js version 18.19 and above, the ESM loaders operate off-thread.
- * Consequently, ESM instrumentation using '--experimental-loader' becomes obsolete.
- * Instead, we recommend using '--import' for loading instrumentation and relocating the 
- * Instana collector loading to './register' file.
+ * Consequently, ESM instrumentation using '--experimental-loader' becomes deprecated.
+ * Instead, we are using '--import' for loading instrumentation and relocated the Instana collector
+ * loading to './register' file.
  * Please note that '--import' flag is unavailable in earlier versions, hence we maintain both setups.
- * We will incorporate the native ESM support by using 'import-in-the-middle'.
+ * We will incorporate the native ESM support by using 'import-in-the-middle' ith register method.
  *
  * Usage:
  * ENV NODE_OPTIONS='--experimental-loader=/instana/node_modules/
  * @instana/aws-fargate/esm-loader.mjs'
  *
  * NOTE: When using ESM, the customers have the option to set up the aws-fargate
- * by configuring ENV variable within the docker file. Given that aws-fargate 
+ * by configuring ENV variable within the docker file. Given that aws-fargate
  * self-initializes, its current utilization remains unchanged.
- * However, there's potential for future integration where the init() method 
+ * However, there's potential for future integration where the init() method
  * can be invoked from this context.
  */
 
