1956bdc9d6b424d40df6f495e10dbf196d22de2e
Arya Mohanan <arya.mohanan@ibm.com>
2024-04-10 17:07:49 +0530

    chore: resolved review comments

--- packages/collector/test/tracing/sdk/multiple_installations/esm/src/register.mjs
+++
@@ -1,9 +0,0 @@
-/*
- * (c) Copyright IBM Corp. 2024
- */
-
-import { register } from 'node:module';
-
-register(import.meta.url);
-const instana = await import(process.env.INSTANA_COLLECTOR_PATH);
-instana.default();
