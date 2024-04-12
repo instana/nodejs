8b3264aa3afcadd87faeb048564ef3af7a8483d0
athirasomanath <144906220+athirasomanath@users.noreply.github.com>
2024-04-12 22:00:07 +0530

    feat: added azure blob instrumentation (#967)
    refs INSTA-782
    
    ---------
    
    Co-authored-by: athira s <athiras@athiras-MacBook-Pro.local>
    Co-authored-by: athira s <athiras@athiras-mbp.in.ibm.com>
    Co-authored-by: athira s <athiras@athiras-MBP.bbrouter>
    Co-authored-by: kirrg001 <katharina.irrgang@instana.com>

---
+++ packages/collector/test/tracing/cloud/azure/blob/util.js
@@ -0,0 +1,25 @@
+/*
+ * (c) Copyright IBM Corp. 2024
+ */
+
+'use strict';
+
+exports.minimumNodeJsVer = '18.0.0';
+
+exports.deleteContainer = async function (containerClient) {
+  try {
+    await containerClient.delete();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error deleting container:', error.message);
+  }
+};
+
+exports.createContainer = async function (containerClient) {
+  try {
+    await containerClient.create();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('Error in container creation:', e);
+  }
+};
