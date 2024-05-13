3b1b9cdae98bbd0725a1a650c4e536f189aa1873
kirrg001 <katharina.irrgang@instana.com>
2024-05-13 13:02:28 +0200

    chore: added github action workflow for test stats

---
+++ bin/gather-test-stats.js
@@ -0,0 +1,52 @@
+/*
+ * (c) Copyright IBM Corp. 2024
+ */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const packagesFolder = path.join(__dirname, '..', 'packages');
+let noOfTestFiles = 0;
+let noOfTestCases = 0;
+
+function walkSync(dir, list = []) {
+  const files = fs.readdirSync(dir);
+
+  files.forEach(file => {
+    const currentPath = path.join(dir, file);
+
+    if (fs.statSync(currentPath).isDirectory()) {
+      if (file === 'node_modules') {
+        return;
+      }
+
+      list = walkSync(currentPath, list);
+    } else if (currentPath.endsWith('test.js')) {
+      list.push(currentPath);
+    }
+  });
+
+  return list;
+}
+
+const packages = fs.readdirSync(packagesFolder);
+packages.forEach(pkg => {
+  const packageTestFolder = path.join(packagesFolder, pkg, 'test');
+  const files = walkSync(packageTestFolder);
+
+  noOfTestFiles += files.length;
+
+  files.forEach(file => {
+    const content = fs.readFileSync(file, 'utf8');
+    const match = content.match(/it\('/g);
+
+    if (match) {
+      noOfTestCases += match.length;
+    }
+  });
+});
+
+console.log(`TOTAL NUMBER OF TEST FILES: ${noOfTestFiles}`);
+console.log(`TOTAL NUMBER OF TEST CASES: ${noOfTestCases}`);
