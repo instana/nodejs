c02892152fbb6801f174ff63b95ea8152b3b1b5c
gurke <katharina.irrgang@googlemail.com>
2024-06-12 09:32:32 +0200

    Revert "chore(aws-lambda): removed timeout detection (#668)"
    This reverts commit 45e8897ea0778d96ee8f0d8c52017086a5ba3459.

---
+++ packages/aws-lambda/test/timeout/lambda.js
@@ -0,0 +1,28 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. and contributors 2020
+ */
+
+/* eslint-disable no-console */
+
+'use strict';
+
+const instana = require('../..');
+
+const delay = require('../../../core/test/test_util/delay');
+
+if (!process.env.DELAY) {
+  throw new Error('This Lambda requires the environment variable DELAY to be set.');
+}
+
+exports.handler = instana.wrap(async () => {
+  console.log(`Lambda handler started, now waiting ${process.env.DELAY} milliseconds.`);
+  await delay(process.env.DELAY);
+  console.log('Lambda handler finished');
+
+  return {
+    body: {
+      message: 'Stan says hi!'
+    }
+  };
+});
