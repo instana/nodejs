30e5e1152c9f7c636f4dd1715429f9fdcb3690dd
Arya Mohanan <arya.mohanan@ibm.com>
2024-06-26 22:23:51 +0530

    ci: skipping the chinese region from publishing the lambda layers

--- packages/aws-lambda/layer/bin/publish-layer.sh
+++ packages/aws-lambda/layer/bin/publish-layer.sh
@@ -121,7 +121,9 @@ fi
 
 # The us-gov-* regions are only available to US government agencies, U.S. government etc. The regions have not been (and
 # maybe cannot be) enabled for our AWS account. We currently do not publish Lambda layers to these regions.
-SKIPPED_REGIONS=$'us-gov-east-1\nus-gov-west-1'
+# For now, we also skip cn-north-1, since we are getting the following error:
+# Connection was closed before we received a valid response from endpoint URL: "https://lambda.cn-north-1.amazonaws.com.cn/2018-10-31/layers/.../versions".
+SKIPPED_REGIONS=$'us-gov-east-1\nus-gov-west-1\ncn-north-1'
 
 # AWS China is completely separated from the rest of AWS. You cannot enable the Chinese regions in a global AWS account.
 # Instead, we have a separate account for AWS China.
