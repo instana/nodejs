029cce6466783450bc78ba5fba2e84149ea12196
Abhilash <abhilash.sivan@ibm.com>
2024-09-19 13:24:26 +0530

    test: sdk wrap span

---
+++ packages/collector/test/tracing/sdk/rootExitSpans/app_with_entry.js
@@ -0,0 +1,40 @@
+/*
+ * (c) Copyright IBM Corp. 2024
+ */
+
+'use strict';
+
+// NOTE: c8 bug https://github.com/bcoe/c8/issues/166
+process.on('SIGTERM', () => {
+  process.disconnect();
+  process.exit(0);
+});
+
+// Initiating the app with allowRootExitSpan configuration
+// to make sure the existing functionality is working fine along with SDK wrap code
+const instana = require('../../../../src')({});
+const fetch = require('node-fetch-v2');
+const url = 'https://www.instana.com';
+
+/* eslint-disable no-console */
+function main() {
+  setTimeout(() => {
+    instana.sdk.promise
+      .startEntrySpan('test-timeout-span')
+      .then(async () => {
+        try {
+          await fetch(url);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          instana.sdk.promise.completeEntrySpan();
+        }
+      })
+      .catch(err => {
+        instana.sdk.promise.completeEntrySpan(err);
+        console.log('Error starting test-timeout-span:', err);
+      });
+  }, 100);
+}
+
+main();
---
+++ packages/collector/test/tracing/sdk/rootExitSpans/test.js
@@ -0,0 +1,99 @@
+/*
+ * (c) Copyright IBM Corp. 2024
+ */
+
+'use strict';
+
+const expect = require('chai').expect;
+const path = require('path');
+
+const supportedVersion = require('@instana/core').tracing.supportedVersion;
+const config = require('@instana/core/test/config');
+const ProcessControls = require('../../../test_util/ProcessControls');
+const globalAgent = require('../../../globalAgent');
+const { retry, delay, expectExactlyOneMatching } = require('@instana/core/test/test_util');
+const constants = require('@instana/core').tracing.constants;
+
+const mochaSuiteFn = supportedVersion(process.versions.node) ? describe.only : describe.skip;
+
+mochaSuiteFn('tracing/sdk/rootExitSpans', function () {
+  this.timeout(100000);
+
+  globalAgent.setUpCleanUpHooks();
+
+  beforeEach(async () => {
+    await globalAgent.instance.clearReceivedTraceData();
+  });
+
+  // an sdk entry span is wrapped to track the exit span
+  // TODO: revisit to update the span assertions
+  describe('APP WITH ENTRY SPAN', function () {
+    let agentControls;
+
+    before(async () => {
+      agentControls = new ProcessControls({
+        appPath: path.join(__dirname, 'app_with_entry'),
+        useGlobalAgent: true
+      });
+
+      await agentControls.start(null, null, true);
+    });
+
+    after(async () => {
+      await agentControls.stop();
+    });
+
+    it('should collect spans including sdk wrap', async () => {
+      await delay(100);
+
+      await retry(
+        async () => {
+          const spans = await globalAgent.instance.getSpans();
+          console.log(spans, spans.length);
+          // expect(spans.length).to.equal(2);
+          expectExactlyOneMatching(spans, span => {
+            console.log(span.data);
+            expect(span.p).to.exist;
+            // expect(span.k).to.equal(constants.EXIT);
+          });
+        },
+        null,
+        null,
+        2
+      );
+    });
+  });
+
+  // no sdk wrap present, exit span stands alone
+  // describe('APP WITHOUT ENTRY SPAN', function () {
+  //   let agentControls;
+
+  //   before(async () => {
+  //     agentControls = new ProcessControls({
+  //       appPath: path.join(__dirname, 'app_default'),
+  //       useGlobalAgent: true
+  //     });
+
+  //     await agentControls.start(null, null, true);
+  //   });
+
+  //   after(async () => {
+  //     await agentControls.stop();
+  //   });
+
+  //   it('should trace single exit span only', async () => {
+  //     await delay(100);
+
+  //     await retry(async () => {
+  //       const spans = await globalAgent.instance.getSpans();
+  //       expect(spans.length).to.equal(1);
+  //       expectExactlyOneMatching(spans, span => {
+  //         expect(span.t).to.exist;
+  //         expect(span.p).to.not.exist;
+  //         expect(span.s).to.exist;
+  //         expect(span.k).to.equal(constants.EXIT);
+  //       });
+  //     });
+  //   });
+  // });
+});
