9fa6667fdeeb8a7985efc09001e54e73d378ba6c
Abhilash <abhilash.sivan@ibm.com>
2024-09-27 15:08:05 +0530

    test: added test for httpclient

---
+++ packages/collector/test/tracing/protocols/http/client/allowRootExitSpanApp.js
@@ -0,0 +1,44 @@
+/*
+ * (c) Copyright IBM Corp. 2024
+ */
+
+'use strict';
+
+// NOTE: c8 bug https://github.com/bcoe/c8/issues/166
+process.on('SIGTERM', () => {
+  process.disconnect();
+  process.exit(0);
+});
+
+require('../../../../..')();
+const { delay } = require('../../../../../../core/test/test_util');
+
+const fetch = require('node-fetch-v2');
+
+const main = async () => {
+  try {
+    setTimeout(async () => {
+      await fetch('https://example.com');
+
+      await fetch('https://www.example.com');
+    }, 100);
+  } catch (err) {
+    /* eslint-disable no-console */
+    console.log(err);
+  }
+};
+
+const app = async () => {
+  let count = 0;
+
+  while (count < 2) {
+    // eslint-disable-next-line no-await-in-loop
+    await main();
+    count += 1;
+
+    // eslint-disable-next-line no-await-in-loop
+    await delay(500);
+  }
+};
+
+app();
