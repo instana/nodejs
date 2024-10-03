89c30514385b90f78cdf3ca8f51e95c8db77f533
Arya Mohanan <arya.mohanan@ibm.com>
2024-10-03 21:55:12 +0530

    fix: added log to capture instana headers from incoming request

--- packages/core/src/tracing/instrumentation/protocols/httpServer.js
+++ packages/core/src/tracing/instrumentation/protocols/httpServer.js
@@ -17,7 +17,10 @@ const {
 } = require('./captureHttpHeadersUtil');
 const shimmer = require('../../shimmer');
 const cls = require('../../cls');
-
+let logger;
+logger = require('../../../logger').getLogger('tracing/httpServer', newLogger => {
+  logger = newLogger;
+});
 let extraHttpHeadersToCapture;
 let isActive = false;
 
@@ -65,7 +68,7 @@ function shimEmit(realEmit) {
       if (res && res.on && res.addListener && res.emit) {
         cls.ns.bindEmitter(res);
       }
-
+      logInstanaHeaders(req);
       const headers = tracingHeaders.fromHttpRequest(req);
       const w3cTraceContext = headers.w3cTraceContext;
 
@@ -202,4 +205,13 @@ function shimEmit(realEmit) {
       return realEmit.apply(originalThis, originalArgs);
     });
   };
+}
+function logInstanaHeaders(req) {
+  if (req.headers) {
+    Object.keys(req.headers).forEach(header => {
+      if (header?.toLowerCase()?.startsWith('x-instana')) {
+        logger.debug(`Instana header found in the request: ${header}: ${req.headers[header]}`);
+      }
+    });
+  }
 }
