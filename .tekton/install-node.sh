50f93ae89f7b1bfc2507a9b0f3524326615feeee
Abhilash <abhilash.sivan@ibm.com>
2024-10-08 20:09:32 +0530

    chore: update in install script

--- .tekton/install-node.sh
+++ .tekton/install-node.sh
@@ -3,6 +3,8 @@
 NODE_VERSION=$1
 PRERELEASE_NODE_VERSION=$2
 
+echo "Installing node prerelease version $PRERELEASE_NODE_VERSION.."
+
 if ! command -v nvm &> /dev/null; then
   echo "NVM not found. Installing NVM..."
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -14,15 +16,15 @@ else
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 fi
 
-NVM_NODEJS_ORG_MIRROR="https://nodejs.org/download/rc" nvm install "$NODE_VERSION" 
+NVM_NODEJS_ORG_MIRROR="https://nodejs.org/download/rc" nvm install "$PRERELEASE_NODE_VERSION" 
 
-if ! nvm ls "$NODE_VERSION" >/dev/null; then
-  echo "RC installation failed for Node.js version $NODE_VERSION."
+if ! nvm ls "$PRERELEASE_NODE_VERSION" >/dev/null; then
+  echo "RC installation failed for Node.js version $PRERELEASE_NODE_VERSION."
 
-  NVM_NODEJS_ORG_MIRROR="https://nodejs.org/download/nightly" nvm install "$NODE_VERSION" || \
+  NVM_NODEJS_ORG_MIRROR="https://nodejs.org/download/nightly" nvm install "$PRERELEASE_NODE_VERSION" || \
   echo "Installation failed with NIGHTLY mirror"
 fi
 
-nvm use "$NODE_VERSION"
+nvm use "$PRERELEASE_NODE_VERSION"
 
 echo "Currently using node: $(node --version)"
