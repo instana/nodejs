3a36b0b4160e18f14a6750d892c215d7b3ce9ae7
Abhilash <abhilash.sivan@ibm.com>
2024-10-10 21:01:41 +0530

    chore: update install script

--- bin/install-prerelease.sh
+++ bin/install-prerelease.sh
@@ -4,11 +4,10 @@ PRERELEASE_NODE_VERSION=$1
 
 echo "Installing node.js prerelease version $PRERELEASE_NODE_VERSION.."
 
-mirrors=("https://nodejs.org/download/rc" "https://nodejs.org/download/nightly")
-
-if ! command -v nvm &> /dev/null; then
+if ! command -v nvm &> /dev/null 2>&1; then
+  mirrors=("https://nodejs.org/download/rc" "https://nodejs.org/download/nightly")
   echo "NVM not found. Installing NVM..."
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh > /dev/null 2>&1| bash > /dev/null 2>&1
   # Load NVM
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" > /dev/null 2>&1
@@ -22,10 +21,9 @@ for mirror in "${mirrors[@]}"; do
   echo "Installation failed with mirror: $mirror"
 done
 
-if ! nvm ls "$PRERELEASE_NODE_VERSION" >/dev/null 2>&1; then
+if ! nvm ls "$PRERELEASE_NODE_VERSION" > /dev/null 2>&1; then
   echo "Both RC and Nightly installations failed for Node.js version $PRERELEASE_NODE_VERSION."
 fi
 
 nvm use "$PRERELEASE_NODE_VERSION" > /dev/null 2>&1
-echo "Currently using node: $(node --version)"
 
