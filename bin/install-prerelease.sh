6d2fe6859c175fbb48b2f03271730e5431815538
Abhilash <abhilash.sivan@ibm.com>
2024-10-10 21:26:34 +0530

    chore: optimized install script

--- bin/install-prerelease.sh
+++ bin/install-prerelease.sh
@@ -2,28 +2,26 @@
 
 PRERELEASE_NODE_VERSION=$1
 
-echo "Installing node.js prerelease version $PRERELEASE_NODE_VERSION.."
+echo "Installing Node.js prerelease version $PRERELEASE_NODE_VERSION..."
 
-if ! command -v nvm &> /dev/null 2>&1; then
-  mirrors=("https://nodejs.org/download/rc" "https://nodejs.org/download/nightly")
+if ! command -v nvm &> /dev/null; then
   echo "NVM not found. Installing NVM..."
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh > /dev/null 2>&1| bash > /dev/null 2>&1
-  # Load NVM
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" > /dev/null 2>&1
-else
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" > /dev/null 2>&1
+  if ! curl -sSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash &> /dev/null; then
+    echo "Failed to install NVM."
+    exit 1
+  fi
+  source "$HOME/.nvm/nvm.sh"
 fi
 
-for mirror in "${mirrors[@]}"; do
-  NVM_NODEJS_ORG_MIRROR="$mirror" nvm install "$PRERELEASE_NODE_VERSION" > /dev/null 2>&1 && break || \
-  echo "Installation failed with mirror: $mirror"
-done
+NVM_NODEJS_ORG_MIRROR="https://nodejs.org/download/rc"
+if ! nvm install "$PRERELEASE_NODE_VERSION" &> /dev/null; then
+  echo "RC installation failed. Trying Nightly mirror..."
 
-if ! nvm ls "$PRERELEASE_NODE_VERSION" > /dev/null 2>&1; then
-  echo "Both RC and Nightly installations failed for Node.js version $PRERELEASE_NODE_VERSION."
+  NVM_NODEJS_ORG_MIRROR="https://nodejs.org/download/nightly"
+  if ! nvm install "$PRERELEASE_NODE_VERSION" &> /dev/null; then
+    echo "Installation failed with both RC and Nightly mirrors."
+    exit 1
+  fi
 fi
 
-nvm use "$PRERELEASE_NODE_VERSION" > /dev/null 2>&1
-
+nvm use "$PRERELEASE_NODE_VERSION"
