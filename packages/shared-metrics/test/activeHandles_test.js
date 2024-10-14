b02b2c1d0067e3162a256ec570874170b6f1a813
Abhilash <70062455+abhilash-sivan@users.noreply.github.com>
2024-10-14 19:13:32 +0530

    test: skip test for activeHandles in shared metrics(#1387)

--- packages/shared-metrics/test/activeHandles_test.js
+++ packages/shared-metrics/test/activeHandles_test.js
@@ -6,7 +6,6 @@
 'use strict';
 
 const expect = require('chai').expect;
-const semver = require('semver');
 const net = require('net');
 
 const config = require('@instana/core/test/config');
@@ -21,15 +20,15 @@ describe('metrics.activeHandles', function () {
     expect(activeHandles.currentPayload).to.equal(process._getActiveHandles().length);
   });
 
-  it('should update handle count for a setTimeout', () => {
-    if (semver.satisfies(process.versions.node, '>=11')) {
-      // skip test beginning with Node.js 11, I suspect commit https://github.com/nodejs/node/commit/ccc3bb73db
-      // (PR https://github.com/nodejs/node/pull/24264) to have broken this test. Seems timeouts do no longer add to
-      // active handles. See 'with net client/server' for a test case that verifies this metric also for
-      // Node.js >= 11.x.
-      return;
-    }
+  // This test is only compatible with Node.js versions lower than 11.
+  // The failure is due to changes introduced in commit:
+  //  https://github.com/nodejs/node/commit/ccc3bb73db.
+  // We can reintroduce the test once we switch from the deprecated
+  // _getActiveHandles flag to getActiveResourcesInfo, which is more reliable.
+  // More details: https://nodejs.org/api/process.html#processgetactiveresourcesinfo
+  // For additional context, see: https://github.com/instana/nodejs/pull/1387
 
+  it.skip('should update handle count for a setTimeout', () => {
     const previousCount = activeHandles.currentPayload;
     const timeoutHandle = setTimeout(() => {}, 100);
 
