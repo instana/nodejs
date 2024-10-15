e0fe8f5c344f76f5dd39532489556a8aced8d0b4
kirrg001 <katharina.irrgang@instana.com>
2024-10-15 12:03:14 +0200

    test: fixed sdk/multiple_installations/cjs/test.js for Node v23

--- packages/collector/test/tracing/sdk/multiple_installations/cjs/src/load-instana.js
+++ packages/collector/test/tracing/sdk/multiple_installations/cjs/src/load-instana.js
@@ -4,4 +4,4 @@
 
 'use strict';
 
-require(process.env.INSTANA_COLLECTOR_PATH)({ agentPort: process.env.AGENT_PORT });
+require(process.env.INSTANA_COLLECTOR_PATH)({ agentPort: process.env.INSTANA_AGENT_PORT });
