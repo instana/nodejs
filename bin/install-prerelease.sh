90e07e15e48a167e3c91dba3ac76c0cd341b25cc
Abhilash <abhilash.sivan@ibm.com>
2024-10-25 13:22:00 +0530

    ci: updated logic for fetching latest prerelease version

--- bin/install-prerelease.sh
+++ bin/install-prerelease.sh
@@ -2,6 +2,23 @@
 
 PRERELEASE_NODE_VERSION=$1
 
+fetch_latest_prerelease_node_version() {
+  local latest_version
+  
+  latest_version=$(curl -sL https://nodejs.org/dist/index.json | jq -r '[.[]][0].version')
+  latest_version=${latest_version#v}
+  IFS='.' read -r major minor patch <<< "$latest_version"
+  
+  major=$((major + 1))
+  
+  echo "${major}"
+}
+
+# Check if PRERELEASE_NODE_VERSION is set; if not, set it to the latest prerelease
+if [ -z "$PRERELEASE_NODE_VERSION" ]; then
+  PRERELEASE_NODE_VERSION=$(fetch_latest_prerelease_node_version)
+fi
+
 echo "Installing Node.js prerelease version $PRERELEASE_NODE_VERSION..."
 
 if ! command -v nvm &> /dev/null; then
