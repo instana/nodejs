54914613bf40c6e81c652801899f2d3136308073
gurke <katharina.irrgang@ibm.com>
2024-12-03 09:42:26 +0100

    feat(serverless-collector): added service name detection (#1468)
    refs https://jsw.ibm.com/browse/INSTA-20571

--- packages/serverless-collector/src/activate.js
+++ packages/serverless-collector/src/activate.js
@@ -6,32 +6,43 @@
 
 const instanaCore = require('@instana/core');
 const { backendConnector, consoleLogger } = require('@instana/serverless');
-
 const identityProvider = require('./identity_provider');
 
 const { tracing, util: coreUtil } = instanaCore;
 const { normalizeConfig } = coreUtil;
+const customMetrics = require('./metrics');
 
 let logger = consoleLogger;
 
 const config = normalizeConfig({});
 config.logger = logger;
 
-function init() {
+async function init() {
+  // NOTE: We accept for `process.env.INSTANA_DEBUG` any string value - does not have to be "true".
   if (process.env.INSTANA_DEBUG || process.env.INSTANA_LOG_LEVEL) {
     logger.setLevel(process.env.INSTANA_DEBUG ? 'debug' : process.env.INSTANA_LOG_LEVEL);
   }
 
-  // NOTE: This package will not collect metrics.
   // NOTE: This package does not support autotracing.
+  // NOTE: This package does not support metrics.
 
   try {
+    // NOTE: We have to call pre-init because we are running an async call to get the service name.
+    //       The goal is to register our instrumentations as early as possible.
+    //       Otherwise we can easily run into errors e.g. from `hasThePackageBeenInitializedTooLate`.
+    instanaCore.preInit();
+
+    const serviceName = await customMetrics.name(config, logger);
+    if (serviceName) {
+      config.serviceName = serviceName;
+    }
+
     identityProvider.init();
     backendConnector.init(identityProvider, logger, false, true, 950);
     instanaCore.init(config, backendConnector, identityProvider);
     tracing.activate();
 
-    logger.debug('@instana/serverless-collector initialized.');
+    logger.info('@instana/serverless-collector initialized.');
 
     // eslint-disable-next-line no-unused-expressions
     process.send && process.send('instana.serverless-collector.initialized');
---
+++ packages/serverless-collector/src/metrics/name.js
@@ -0,0 +1,30 @@
+/*
+ * (c) Copyright IBM Corp. 2024
+ */
+
+'use strict';
+
+const instanaCore = require('@instana/core');
+
+// NOTE:
+// The service name will be "localhost" because the serverless collector does not support
+// metrics. Metrics contain the package.json name. AFAIK there is currently
+// no way to connect metrics with spans for **agentless non serverless** environments.
+// https://jsw.ibm.com/browse/INSTA-3607
+module.exports = (config, logger) => {
+  if (process.env.INSTANA_SERVICE_NAME) {
+    return process.env.INSTANA_SERVICE_NAME;
+  }
+
+  return new Promise(resolve => {
+    instanaCore.util.applicationUnderMonitoring.getMainPackageJsonStartingAtMainModule(config, (err, packageJson) => {
+      if (err) {
+        logger.debug('Failed to determine main package.json.', err);
+        return resolve();
+      }
+
+      logger.debug(`Found main package.json: ${packageJson.name}`);
+      resolve(packageJson.name);
+    });
+  });
+};
