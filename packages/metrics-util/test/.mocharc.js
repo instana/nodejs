c3e56ebc0f371542c0b5bd9d9a29f91162767da6
kirrg001 <katharina.irrgang@instana.com>
2024-12-09 12:08:35 +0100

    chore: updated mocharc for mocha watch
    - when you saved a src file in another pkg, mocha watch did not re-run

--- packages/metrics-util/test/.mocharc.js
+++ packages/metrics-util/test/.mocharc.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const mochaOptions = {
-  ignore: ['node_modules/**/*', 'test/**/node_modules/**/*']
+  ignore: ['node_modules/**/*', 'test/**/node_modules/**/*'],
+  watchFiles: ['test/**/*', '../*/src/**/*']
 };
 
 module.exports = mochaOptions;
