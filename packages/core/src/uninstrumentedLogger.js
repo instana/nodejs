956d1d12010c130b1474a8aff18096df7bae885a
kirrg001 <katharina.irrgang@ibm.com>
2025-01-02 10:52:01 +0100

    chore: fixed bug

--- packages/core/src/uninstrumentedLogger.js
+++ packages/core/src/uninstrumentedLogger.js
@@ -7,6 +7,17 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, instana/no-unsafe-require
 const logger = require('pino');
 
+// NOTE: There is a bug in the pino instrumentation. As soon as you require pino twice,
+//       only the first require is instrumented. `onFileLoad` causes the behavior for that.
+//       See https://jsw.ibm.com/browse/INSTA-23066
+// NOTE: We need the removal of the cache here anyway, because we do not want to trigger the pino instrumentation.
+//       This is an uninstrumented pino logger.
+Object.keys(require.cache).forEach(key => {
+  if (key.includes('pino')) {
+    delete require.cache[key];
+  }
+});
+
 function createCustomLogger() {
   const customLogger = Object.assign(function (/** @type {any} */ ...args) {
     // @ts-ignore
