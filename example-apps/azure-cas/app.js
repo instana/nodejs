1142023b46c9f2da4428a27b9b216514c5ee7fb6
kirrg001 <katharina.irrgang@ibm.com>
2025-02-05 11:25:15 +0100

    chore: added azure container app service example app
    - docs missing

---
+++ example-apps/azure-cas/app.js
@@ -0,0 +1,30 @@
+/*
+ * (c) Copyright IBM Corp. 2024
+ */
+
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Azure App!');
+});
+
+app.get('/data', async (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+setInterval(async () => {
+  try {
+    await fetch('https://jsonplaceholder.typicode.com/posts/1');
+    console.log('Fetched data successfully.');
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+  }
+}, 10 * 1000);
