cc36b1ffb2589546917768022f9c072b44083098
kirrg001 <katharina.irrgang@ibm.com>
2025-02-12 14:34:54 +0100

    chore: fixes

--- packages/aws-lambda/src/metrics/index.js
+++ packages/aws-lambda/src/metrics/index.js
@@ -10,13 +10,13 @@ const npmPackageName = require('./npmPackageName');
 const npmPackageVersion = require('./npmPackageVersion');
 const npmPackageDescription = require('./npmPackageDescription');
 
-coreMetrics.registerAdditionalMetrics([npmPackageName, npmPackageVersion, npmPackageDescription]);
-
 exports.init = function init(config) {
   coreMetrics.init(config);
   npmPackageName.init(config);
   npmPackageVersion.init(config);
   npmPackageDescription.init(config);
+
+  coreMetrics.registerAdditionalMetrics([npmPackageName, npmPackageVersion, npmPackageDescription]);
 };
 
 exports.activate = function activate() {
