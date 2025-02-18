ee1a1cc1f2afce06bb4af9174c6f888c987cb857
kirrg001 <katharina.irrgang@ibm.com>
2025-02-18 12:52:03 +0100

    chore: added docker example app for collector v2

---
+++ example-apps/docker/collector-v2/app.js
@@ -0,0 +1,24 @@
+/*
+ * (c) Copyright IBM Corp. 2025
+ */
+
+/* eslint-disable no-console */
+
+'use strict';
+
+require('@instana/collector')({
+  agentHost: '172.22.0.2',
+  agentPort: 42699
+});
+
+const express = require('express');
+const app = express();
+const port = 3022;
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
