87810ad28d6b9e9babdd437f3144afc661d56ff2
kirrg001 <katharina.irrgang@ibm.com>
2025-02-27 18:19:38 +0100

    ci: enabled long running test in agentCommunication_test.js

--- packages/collector/test/agentCommunication_test.js
+++ packages/collector/test/agentCommunication_test.js
@@ -8,16 +8,27 @@
 const expect = require('chai').expect;
 
 const config = require('@instana/core/test/config');
-const { delay, retry } = require('@instana/core/test/test_util');
+const { delay, retry, isCI, isCILongRunning } = require('@instana/core/test/test_util');
 const globalAgent = require('./globalAgent');
 const { isNodeVersionEOL } = require('../src/util/eol');
+let expressControls;
+let agentControls;
+
+let mochaSuiteFn = describe.skip;
+if (isCI() && isCILongRunning()) {
+  mochaSuiteFn = describe;
+}
+
+mochaSuiteFn('agentCommunication: lots of retries with exponential backoff', function () {
+  runRetryTest.bind(this)(300000, 45 * 1000, 7);
+});
 
 describe('agentCommunication', function () {
   this.timeout(config.getTestTimeout());
 
   globalAgent.setUpCleanUpHooks();
-  const agentControls = globalAgent.instance;
-  const expressControls = require('./apps/expressControls');
+  agentControls = globalAgent.instance;
+  expressControls = require('./apps/expressControls');
 
   before(async () => {
     await expressControls.start({ useGlobalAgent: true });
@@ -29,6 +40,17 @@ describe('agentCommunication', function () {
 
   after(async () => {
     await expressControls.stop();
+  });
+
+  // TODO: do we need this test?
+  describe('announce retry', function () {
+    describe('retry once after 10s', function () {
+      runRetryTest.bind(this)(29000, 10 * 1000, 1);
+    });
+
+    describe('retry twice after 25s', function () {
+      runRetryTest.bind(this)(59000, 25 * 1000, 2);
+    });
   });
 
   it('must announce itself to the agent', () =>
@@ -103,57 +125,41 @@ describe('agentCommunication', function () {
       expect(eolEvents, `Received one or more unexpected EOL events: ${JSON.stringify(eolEvents)}`).to.be.empty;
     }
   });
-
-  // TODO: do we need this test?
-  describe('announce retry', function () {
-    describe('retry once after 10s', function () {
-      runRetryTest.bind(this)(29000, 10 * 1000, 1);
-    });
-
-    describe('retry twice after 25s', function () {
-      runRetryTest.bind(this)(59000, 25 * 1000, 2);
-    });
-
-    // takes too long to include it in the general test suite
-    describe.skip('lots of retries with exponential backoff', function () {
-      runRetryTest.bind(this)(300000, 45 * 1000, 7);
-    });
-
-    function runRetryTest(timeout, retryTime, rejectedAttempts) {
-      this.timeout(timeout);
-
-      beforeEach(async () => {
-        expressControls.stop();
-
-        await agentControls.rejectAnnounceAttempts(rejectedAttempts);
-        await expressControls.start({
-          useGlobalAgent: true,
-          collectorUninitialized: true,
-          env: {
-            INSTANA_LOG_LEVEL: 'info'
-          }
-        });
-      });
-
-      afterEach(() => expressControls.stop());
-
-      it('must retry announce ', async () => {
-        await retry(
-          () =>
-            agentControls.getDiscoveries().then(discoveries => {
-              const discoveriesForPid = discoveries[expressControls.getPid()];
-              expect(discoveriesForPid.length).to.be.at.least(1);
-              const discovery = discoveriesForPid[0];
-              expect(discovery.pid).to.be.a('number');
-              expect(discovery.fd).to.be.a('string');
-              if (/linux/i.test(process.platform)) {
-                expect(discovery.inode).to.be.a('string');
-              }
-            }),
-          retryTime,
-          Date.now() + timeout
-        );
-      });
-    }
-  });
 });
+
+function runRetryTest(timeout, retryTime, rejectedAttempts) {
+  this.timeout(timeout);
+
+  beforeEach(async () => {
+    expressControls.stop();
+
+    await agentControls.rejectAnnounceAttempts(rejectedAttempts);
+    await expressControls.start({
+      useGlobalAgent: true,
+      collectorUninitialized: true,
+      env: {
+        INSTANA_LOG_LEVEL: 'info'
+      }
+    });
+  });
+
+  afterEach(() => expressControls.stop());
+
+  it('must retry announce ', async () => {
+    await retry(
+      () =>
+        agentControls.getDiscoveries().then(discoveries => {
+          const discoveriesForPid = discoveries[expressControls.getPid()];
+          expect(discoveriesForPid.length).to.be.at.least(1);
+          const discovery = discoveriesForPid[0];
+          expect(discovery.pid).to.be.a('number');
+          expect(discovery.fd).to.be.a('string');
+          if (/linux/i.test(process.platform)) {
+            expect(discovery.inode).to.be.a('string');
+          }
+        }),
+      retryTime,
+      Date.now() + timeout
+    );
+  });
+}
