instana/nodejs

commit 96bc4c6fbcc888b73cf655d541842e8587e8cfea

refactor: removed "getLogger" usage (#1556)
refs https://jsw.ibm.com/browse/INSTA-13498

packages/collector/src/announceCycle/agentready.js
@@ -13,12 +13,10 @@ try {
 }
 
 const { tracing } = require('@instana/core');
-
 const agentConnection = require('../agentConnection');
 const agentOpts = require('../agent/opts');
 const initializedTooLate = require('../util/initializedTooLate');
 const metrics = require('../metrics');
-const pidStore = require('../pidStore');
 const requestHandler = require('../agent/requestHandler');
 const transmissionCycle = require('../metrics/transmissionCycle');
 const uncaught = require('../uncaught');
@@ -39,9 +37,6 @@ let profiler;
 
 /** @type {import('@instana/core/src/core').GenericLogger} */
 let logger;
-logger = require('../logger').getLogger('announceCycle/agentready', newLogger => {
-  logger = newLogger;
-});
 
 if (agentOpts.autoProfile) {
   try {
@@ -77,10 +72,19 @@ if (typeof process.env.INSTANA_TRACER_METRICS_INTERVAL === 'string') {
 /** @type {NodeJS.Timeout} */
 let tracingMetricsTimeout = null;
 
-module.exports = exports = {
-  enter,
-  leave
-};
+/** @type {{ pid: number, getEntityId: Function }} */
+let pidStore;
+
+/**
+ * @param {import('@instana/core/src/util/normalizeConfig').InstanaConfig} config
+ * @param {any} _pidStore
+ */
+function init(config, _pidStore) {
+  logger = config.logger;
+  pidStore = _pidStore;
+
+  initializedTooLate.init(config);
+}
 
 /**
  * @param {import('./').AnnounceCycleContext} _ctx
@@ -166,6 +170,7 @@ function leave() {
 
 function sendTracingMetrics() {
   const payload = tracing._getAndResetTracingMetrics();
+
   agentConnection.sendTracingMetricsToAgent(payload, error => {
     if (error) {
       logger.info(
@@ -248,3 +253,9 @@ function detectEOLNodeVersion() {
     }, 2000);
   }
 }
+
+module.exports = exports = {
+  init,
+  enter,
+  leave
+};

packages/collector/src/index.js
@@ -44,23 +44,22 @@ try {
 const path = require('path');
 const instanaNodeJsCore = require('@instana/core');
 const instanaSharedMetrics = require('@instana/shared-metrics');
-
 require('./tracing'); // load additional instrumentations
 const log = require('./logger');
-
-const normalizeConfig = require('./util/normalizeConfig');
+const normalizeCollectorConfig = require('./util/normalizeConfig');
 const experimental = require('./experimental');
 
+// NOTE: Default collector logger && config for cases like `preinit`.
+const logger = log.init();
+/** @type {import('./types/collector').CollectorConfig} */
+let config = instanaNodeJsCore.util.normalizeConfig({}, logger);
 /** @type {import('./agentConnection')} */
 let agentConnection;
-
-/** @type {import('./types/collector').CollectorConfig} */
-let config;
 
 /**
  * @param {import('./types/collector').CollectorConfig} [_config]
  */
-function init(_config) {
+function init(_config = {}) {
   // @ts-ignore: Property '__INSTANA_INITIALIZED' does not exist on type global
   if (global.__INSTANA_INITIALIZED) {
     // Prevent initializing @instana/collector multiple times for the same process: @instana/collector has already been
@@ -94,32 +93,37 @@ function init(_config) {
   }
   // @ts-ignore: Property '__INSTANA_INITIALIZED' does not exist on type global
   global.__INSTANA_INITIALIZED = true;
-  config = normalizeConfig(_config);
+
+  // CASE: reinit logger if custom logger or log level is provided.
+  if (_config.logger || _config.level) {
+    log.init(_config);
+  }
+
+  // TODO: The idea of having a config per parent module probably makes, sense
+  //       but as far as I can see, the code in the collector config module is a duplicate.
+  config = normalizeCollectorConfig(_config);
+  config = instanaNodeJsCore.util.normalizeConfig(config, logger);
 
   agentConnection = require('./agentConnection');
   const agentOpts = require('./agent/opts');
   const pidStore = require('./pidStore');
   const uncaught = require('./uncaught');
+  const announceCycle = require('./announceCycle');
+  const metrics = require('./metrics');
 
-  let logger;
-  logger = log.getLogger('index', newLogger => {
-    logger = newLogger;
-  });
-
-  // NOTE: By default we set our instana internal logger
-  config.logger = logger;
-
+  pidStore.init(config);
+  announceCycle.init(config, pidStore);
   agentOpts.init(config);
+  agentConnection.init(config, pidStore);
   instanaNodeJsCore.init(config, agentConnection, pidStore);
-  instanaSharedMetrics.setLogger(logger);
 
   if (isMainThread) {
     uncaught.init(config, agentConnection, pidStore);
-    require('./metrics').init(config);
+    metrics.init(config, pidStore);
   }
 
   logger.info(`@instana/collector module version: ${require(path.join(__dirname, '..', 'package.json')).version}`);
-  require('./announceCycle').start();
+  announceCycle.start();
 
   return init;
 }
@@ -137,12 +141,11 @@ init.isConnected = function isConnected() {
 };
 
 /**
- * @param {import('@instana/core/src/core').GenericLogger} logger
+ * @param {import('@instana/core/src/core').GenericLogger} _logger
  */
-init.setLogger = function setLogger(logger) {
+init.setLogger = function setLogger(_logger) {
   // NOTE: Override our default logger with customer's logger
-  config.logger = logger;
-  log.init(config, true);
+  log.init({ logger: _logger });
 };
 
 init.core = instanaNodeJsCore;
@@ -157,7 +160,7 @@ if (process.env.INSTANA_IMMEDIATE_INIT != null && process.env.INSTANA_IMMEDIATE_
   process.env.INSTANA_EARLY_INSTRUMENTATION != null &&
   process.env.INSTANA_EARLY_INSTRUMENTATION.toLowerCase() === 'true'
 ) {
-  instanaNodeJsCore.preInit();
+  instanaNodeJsCore.preInit(config);
 }
 
 module.exports = init;

packages/aws-fargate/src/metrics/index.js
@@ -5,14 +5,14 @@
 
 'use strict';
 
-const { consoleLogger } = require('@instana/serverless');
 const transmissionCycle = require('./transmissionCycle');
 
-let logger = consoleLogger;
-
+let logger;
 const metadataUriKey = 'ECS_CONTAINER_METADATA_URI';
 
 exports.init = function init(config, onReady) {
+  logger = config.logger;
+
   const metadataUri = process.env.ECS_CONTAINER_METADATA_URI;
   if (!metadataUri) {
     logger.error(`${metadataUriKey} is not set. This fargate task will not be monitored.`);
@@ -28,9 +28,4 @@ exports.activate = function activate() {
 
 exports.deactivate = function deactivate() {
   transmissionCycle.deactivate();
-};
-
-exports.setLogger = function setLogger(_logger) {
-  logger = _logger;
-  transmissionCycle.setLogger(logger);
 };

packages/aws-lambda/src/metrics/index.js
@@ -6,15 +6,17 @@
 'use strict';
 
 const { metrics: coreMetrics } = require('@instana/core');
-
-coreMetrics.registerAdditionalMetrics([
-  require('./npmPackageName'),
-  require('./npmPackageVersion'),
-  require('./npmPackageDescription')
-]);
+const npmPackageName = require('./npmPackageName');
+const npmPackageVersion = require('./npmPackageVersion');
+const npmPackageDescription = require('./npmPackageDescription');
 
 exports.init = function init(config) {
   coreMetrics.init(config);
+  npmPackageName.init(config);
+  npmPackageVersion.init(config);
+  npmPackageDescription.init(config);
+
+  coreMetrics.registerAdditionalMetrics([npmPackageName, npmPackageVersion, npmPackageDescription]);
 };
 
 exports.activate = function activate() {

packages/collector/src/announceCycle/index.js
@@ -16,21 +16,23 @@
  * @property {(ctx?: AnnounceCycleContext) => void} leave
  */
 
+const agentHostLookup = require('./agentHostLookup');
+const unannounced = require('./unannounced');
+const announced = require('./announced');
+const agentready = require('./agentready');
+
 /** @type {import('@instana/core/src/core').GenericLogger} */
 let logger;
-logger = require('../logger').getLogger('announceCycle', newLogger => {
-  logger = newLogger;
-});
 
 /** @type {string} */
 let currentState = null;
 
 /** @type {Object.<string, AgentState>} */
 const states = {
-  agentHostLookup: require('./agentHostLookup'),
-  unannounced: require('./unannounced'),
-  announced: require('./announced'),
-  agentready: require('./agentready')
+  agentHostLookup,
+  unannounced,
+  announced,
+  agentready
 };
 
 /** @type {AnnounceCycleContext} */
@@ -47,6 +49,19 @@ const ctx = {
     currentState = newStateName;
     states[newStateName].enter(ctx);
   }
+};
+
+/**
+ * @param {import('@instana/core/src/util/normalizeConfig').InstanaConfig} config
+ * @param {any} pidStore
+ */
+exports.init = function init(config, pidStore) {
+  logger = config.logger;
+
+  agentHostLookup.init(config);
+  announced.init(config);
+  agentready.init(config, pidStore);
+  unannounced.init(config, pidStore);
 };
 
 exports.start = function start() {

packages/collector/src/metrics/index.js
@@ -8,23 +8,26 @@
 const coreMetrics = require('@instana/core').metrics;
 const sharedMetrics = require('@instana/shared-metrics');
 const transmissionCycle = require('./transmissionCycle');
+const pid = require('./pid');
 
-coreMetrics.registerAdditionalMetrics(sharedMetrics.allMetrics);
-const additionalCollectorMetrics = coreMetrics.findAndRequire(__dirname);
-coreMetrics.registerAdditionalMetrics(additionalCollectorMetrics);
-
-/** @type {import('@instana/core/src/core').GenericLogger} */
-const logger = require('../logger').getLogger('metrics', newLogger => {
-  coreMetrics.setLogger(newLogger);
-});
-coreMetrics.setLogger(logger);
+const additionalMetrics = [pid];
 
 /**
  * @param {import('@instana/core/src/metrics').InstanaConfig} config
+ * @param {any} pidStore
  */
-exports.init = function init(config) {
+exports.init = function init(config, pidStore) {
   coreMetrics.init(config);
+  sharedMetrics.init(config);
   transmissionCycle.init(config);
+
+  additionalMetrics.forEach(metric => {
+    metric.init(config, pidStore);
+  });
+
+  coreMetrics.registerAdditionalMetrics(sharedMetrics.allMetrics);
+  const additionalCollectorMetrics = coreMetrics.findAndRequire(__dirname);
+  coreMetrics.registerAdditionalMetrics(additionalCollectorMetrics);
 };
 
 exports.activate = function activate() {

packages/collector/src/pidStore/index.js
@@ -13,15 +13,39 @@ const agentOpts = require('../agent/opts');
 
 /** @type {import('@instana/core/src/core').GenericLogger} */
 let logger;
-logger = require('../logger').getLogger('pidStore', newLogger => {
-  logger = newLogger;
-});
 
 const eventName = 'pidChanged';
-const eventEmitter = new EventEmitter();
-exports.onPidChange = eventEmitter.on.bind(eventEmitter, eventName);
 
-logger.info(`PID Store starting with pid ${internalPidStore.pid}`);
+/** @type {import('events').EventEmitter} */
+let eventEmitter;
+
+/**
+ * @param {import('@instana/core/src/util/normalizeConfig').InstanaConfig} config
+ */
+exports.init = function init(config) {
+  logger = config.logger;
+  eventEmitter = new EventEmitter();
+
+  logger.info(`PID Store starting with pid ${internalPidStore.pid}`);
+
+  if (!process.env.CONTINUOUS_INTEGRATION) {
+    const pidInParentNamespace = getPidFromParentNamespace();
+
+    if (pidInParentNamespace) {
+      internalPidStore.pid = pidInParentNamespace;
+      logger.info(
+        `Changing pid to ${pidInParentNamespace} due to successful identification of PID in parent namespace`
+      );
+    }
+  }
+};
+
+/**
+ * @param {*} cb
+ */
+exports.onPidChange = cb => {
+  return eventEmitter.on(eventName, cb);
+};
 
 Object.defineProperty(exports, 'pid', {
   get: function getPid() {
@@ -47,14 +71,6 @@ exports.getFrom = function getFrom() {
     h: agentOpts.agentUuid
   };
 };
-
-if (!process.env.CONTINUOUS_INTEGRATION) {
-  const pidInParentNamespace = getPidFromParentNamespace();
-  if (pidInParentNamespace) {
-    internalPidStore.pid = pidInParentNamespace;
-    logger.info(`Changing pid to ${pidInParentNamespace} due to successful identification of PID in parent namespace`);
-  }
-}
 
 function getPidFromParentNamespace() {
   try {

packages/collector/src/uncaught/index.js
@@ -13,9 +13,6 @@ const serializeError = require('serialize-error');
 
 /** @type {import('@instana/core/src/core').GenericLogger} */
 let logger;
-logger = require('../logger').getLogger('util/uncaughtExceptionHandler', newLogger => {
-  logger = newLogger;
-});
 
 /** @type {import('../agentConnection')} */
 let downstreamConnection = null;
@@ -45,6 +42,7 @@ for (let i = 0; i < process.execArgv.length; i++) {
  * @param {import('../pidStore')} _processIdentityProvider
  */
 exports.init = function (_config, _downstreamConnection, _processIdentityProvider) {
+  logger = _config.logger;
   config = _config;
   downstreamConnection = _downstreamConnection;
   processIdentityProvider = _processIdentityProvider;

commit 28382bdc8cb5ee0bcf78f71f17d419b536e22b81

chore: fixed auto profiler
- we broke it in https://github.com/instana/nodejs/commit/96bc4c6fbcc888b73cf655d541842e8587e8cfea
- no user facing change, because we have not released yet

packages/collector/src/agent/opts.js
@@ -27,6 +27,8 @@ exports.init = function init(config) {
   exports.host = config.agentHost;
   // @ts-ignore - Cannot redeclare exported variable
   exports.port = config.agentPort;
+
+  // TODO: Why is autoProfile part of agentOpts? O_o
   // @ts-ignore - Cannot redeclare exported variable
   exports.autoProfile = config.autoProfile;
 };

packages/collector/src/announceCycle/agentready.js
@@ -38,26 +38,6 @@ let profiler;
 /** @type {import('@instana/core/src/core').GenericLogger} */
 let logger;
 
-if (agentOpts.autoProfile) {
-  try {
-    // @ts-ignore - TS cannot find @instana/profile. TODO: @instana/autoprofile is not linted or typed
-    // eslint-disable-next-line import/no-extraneous-dependencies
-    autoprofile = require('@instana/autoprofile');
-  } catch (e) {
-    logger.info(
-      'Could not load @instana/autoprofile. You will not get profiling information for this Node.js app in Instana, ' +
-        'although autoprofiling has been enabled. This typically occurs when native addons could not be built ' +
-        'during module installation (npm install/yarn) or when npm install --no-optional or yarn --ignore-optional ' +
-        'have been used to install dependencies. See the instructions to learn more about the requirements of the ' +
-        'collector: ' +
-        'https://www.ibm.com/docs/en/instana-observability/current?topic=nodejs-collector-installation#native-add-ons'
-    );
-
-    fireMonitoringEvent();
-    setInterval(fireMonitoringEvent, FIRE_MONITORING_EVENT_DURATION_IN_MS).unref();
-  }
-}
-
 /** @type {import('./').AnnounceCycleContext} */
 let ctx;
 
@@ -82,8 +62,29 @@ let pidStore;
 function init(config, _pidStore) {
   logger = config.logger;
   pidStore = _pidStore;
-
   initializedTooLate.init(config);
+
+  // TODO: Why is autoProfile part of agentOpts? O_o Please refactor this away in next major release.
+  if (agentOpts.autoProfile) {
+    try {
+      // @ts-ignore - TS cannot find @instana/profile.
+      // TODO: @instana/autoprofile is not linted or typed
+      // eslint-disable-next-line import/no-extraneous-dependencies
+      autoprofile = require('@instana/autoprofile');
+    } catch (e) {
+      logger.info(
+        'Could not load @instana/autoprofile. You will not get profiling information for this Node.js app in Instana,' +
+          'although autoprofiling has been enabled. This typically occurs when native addons could not be built ' +
+          'during module installation (npm install/yarn) or when npm install --no-optional or yarn --ignore-optional ' +
+          'have been used to install dependencies. See the instructions to learn more about the requirements of the ' +
+          'collector: ' +
+          'https://www.ibm.com/docs/en/instana-observability/current?topic=nodejs-collector-installation#native-add-ons'
+      );
+
+      fireMonitoringEvent();
+      setInterval(fireMonitoringEvent, FIRE_MONITORING_EVENT_DURATION_IN_MS).unref();
+    }
+  }
 }
 
 /**

packages/collector/src/index.js
@@ -112,8 +112,8 @@ function init(_config = {}) {
   const metrics = require('./metrics');
 
   pidStore.init(config);
-  announceCycle.init(config, pidStore);
   agentOpts.init(config);
+  announceCycle.init(config, pidStore);
   agentConnection.init(config, pidStore);
   instanaNodeJsCore.init(config, agentConnection, pidStore);
 

packages/collector/src/util/normalizeConfig.js
@@ -24,8 +24,8 @@ const defaults = {
 module.exports = function normalizeConfig(config = {}) {
   config.agentHost = config.agentHost || process.env.INSTANA_AGENT_HOST || defaults.agentHost;
   config.agentPort = config.agentPort || parseToPositiveInteger(process.env.INSTANA_AGENT_PORT, defaults.agentPort);
-  config.autoProfile = config.autoProfile || process.env.INSTANA_AUTO_PROFILE || defaults.autoProfile;
 
+  config.autoProfile = config.autoProfile || process.env.INSTANA_AUTO_PROFILE || defaults.autoProfile;
   config.tracing = config.tracing || {};
 
   if (config.tracing.stackTraceLength == null) {

packages/collector/test/profiling/test.js
@@ -21,7 +21,7 @@ if (isCI() && !isCILongRunning()) {
 mochaSuiteFn('profiling', function () {
   // profiles are send every two minutes. We wait a bit more than twice that time.
   const testTimeout = 6 * 60 * 1000;
-  const retryTimeout = 5 * 60 * 1000;
+  const retryTimeout = 60 * 1000;
 
   this.timeout(testTimeout);
 
@@ -61,21 +61,25 @@ mochaSuiteFn('profiling', function () {
       // eslint-disable-next-line no-console
       console.log('Waiting for profiles...');
 
-      return retry(() => {
-        // eslint-disable-next-line no-console
-        console.log('...still waiting for profiles...');
-        return (
-          agentControls
-            .getProfiles()
-            .then(profiles => {
-              expect(profiles.length).to.be.at.least(1);
-              keepTriggeringHttpRequests = false;
-              return agentControls.getSpans();
-            })
-            // check that the app produces spans continuously
-            .then(spans => expect(spans.length).to.be.at.least(100))
-        );
-      }, retryTimeout);
+      return retry(
+        () => {
+          // eslint-disable-next-line no-console
+          console.log('...still waiting for profiles...');
+          return (
+            agentControls
+              .getProfiles()
+              .then(profiles => {
+                expect(profiles.length).to.be.at.least(1);
+                keepTriggeringHttpRequests = false;
+                return agentControls.getSpans();
+              })
+              // check that the app produces spans continuously
+              .then(spans => expect(spans.length).to.be.at.least(100))
+          );
+        },
+        retryTimeout,
+        Date.now() + testTimeout
+      );
     });
   });
 