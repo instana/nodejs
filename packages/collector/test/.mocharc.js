da5bc430d7e8caf5649c0cfe9657f5b499e568ac
kirrg001 <katharina.irrgang@ibm.com>
2025-03-04 16:53:55 +0100

    test: separated js and mjs watch files in collector/test/.mocharc.js
    - it seems *.{js,mjs} is not supported

--- packages/collector/test/.mocharc.js
+++ packages/collector/test/.mocharc.js
@@ -3,7 +3,7 @@
 const mochaOptions = {
   file: ['test/initEnv.js'],
   ignore: ['node_modules/**/*', 'test/**/node_modules/**/*'],
-  watchFiles: ['test/**/*.{js,mjs}', '../*/src/**/*.{js,mjs}']
+  watchFiles: ['test/**/*.js', '../*/src/**/*.js', 'test/**/*.mjs', '../*/src/**/*.mjs']
 };
 
 // To address the certificate authorization issue with node-fetch, process.env.NODE_TLS_REJECT_UNAUTHORIZED
