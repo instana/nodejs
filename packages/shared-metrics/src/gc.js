3ba63dc0e9db138011f7ec833812703b85a5d3b4
Abhilash <abhilash.sivan@ibm.com>
2025-03-14 21:28:43 +0530

    chore: update

--- packages/shared-metrics/src/gc.js
+++ packages/shared-metrics/src/gc.js
@@ -68,7 +68,7 @@ exports.init = function init() {
 
 exports.activate = function activate() {
   activateHasBeenCalled = true;
-  if (gcStats) {
+  if (gcStats && typeof gcStats.on === 'function') {
     actuallyActivate();
   }
 };
@@ -81,30 +81,28 @@ function actuallyActivate() {
     return;
   }
   hasBeenActivated = true;
-  if (gcStats && typeof gcStats.on === 'function') {
-    gcStats.on('stats', (/** @type {*} */ stats) => {
-      // gcstats exposes start and end in nanoseconds
-      const pause = (stats.end - stats.start) / 1000000;
-      gcPauseWindow.addPoint(pause);
-      const type = stats.gctype;
-      if (type === 1) {
-        minorGcWindow.addPoint(1);
-      } else if (type === 2) {
-        majorGcWindow.addPoint(1);
-      } else if (type === 4) {
-        incrementalMarkingsWindow.addPoint(1);
-      } else if (type === 8) {
-        processWeakCallbacksWindow.addPoint(1);
-      } else if (type === 15) {
-        minorGcWindow.addPoint(1);
-        majorGcWindow.addPoint(1);
-        incrementalMarkingsWindow.addPoint(1);
-        processWeakCallbacksWindow.addPoint(1);
-      }
-      exports.currentPayload.usedHeapSizeAfterGc = stats.after.usedHeapSize;
-    });
-    startSensing();
-  }
+  gcStats.on('stats', (/** @type {*} */ stats) => {
+    // gcstats exposes start and end in nanoseconds
+    const pause = (stats.end - stats.start) / 1000000;
+    gcPauseWindow.addPoint(pause);
+    const type = stats.gctype;
+    if (type === 1) {
+      minorGcWindow.addPoint(1);
+    } else if (type === 2) {
+      majorGcWindow.addPoint(1);
+    } else if (type === 4) {
+      incrementalMarkingsWindow.addPoint(1);
+    } else if (type === 8) {
+      processWeakCallbacksWindow.addPoint(1);
+    } else if (type === 15) {
+      minorGcWindow.addPoint(1);
+      majorGcWindow.addPoint(1);
+      incrementalMarkingsWindow.addPoint(1);
+      processWeakCallbacksWindow.addPoint(1);
+    }
+    exports.currentPayload.usedHeapSizeAfterGc = stats.after.usedHeapSize;
+  });
+  startSensing();
 }
 
 function startSensing() {
