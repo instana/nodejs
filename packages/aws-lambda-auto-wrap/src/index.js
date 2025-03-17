a3e182422119682ab820e535fdf45945cda5579b
Abhilash <abhilash.sivan@ibm.com>
2025-03-17 12:49:43 +0530

    chore: update

--- packages/aws-lambda-auto-wrap/src/index.js
+++ packages/aws-lambda-auto-wrap/src/index.js
@@ -10,25 +10,21 @@ const localUtils = require('./utils');
 
 let wrappedHandler;
 
-// case 1: return directly from the root
-// result: there is a delay of 4 seconds in ap-northeast-1 region
-// return;
-
 if (!wrappedHandler) {
-  const targetHandler = loadTargetHandlerFunction();
-   // case 2: check for the targethandler and then return immediately
-   // result: there is 4 sec delay and no logs
-    if (!targetHandler) {
-      console.log('================= no targetHandler found');
-      return;
-    } else {
-      console.log('================== found', targetHandler);
-    }
-  wrappedHandler = instana.wrap(targetHandler);
+  try {
+    const targetHandler = loadTargetHandlerFunction();
+    wrappedHandler = instana.wrap(targetHandler);
+  } catch (err) {
+    console.error('Not traced', err);
+  }
 }
 
 exports.handler = function instanaAutowrapHandler(event, context, callback) {
-  return wrappedHandler(event, context, callback);
+  if (!wrappedHandler) {
+    callback.apply(this, arguments);
+  } else {
+    return wrappedHandler(event, context, callback);
+  }
 };
 
 function loadTargetHandlerFunction() {
@@ -49,13 +45,6 @@ function loadTargetHandlerFunction() {
 
 function requireTargetHandlerModule(lambdaTaskRoot, targetHandlerModuleFolder, targetHandlerModuleName) {
   const requirePath = localUtils.getImportPath(lambdaTaskRoot, targetHandlerModuleFolder, targetHandlerModuleName);
-
-//   if (requirePath.endsWith('.mjs')) {
-//     throw new localUtils.errors.lambda.ImportModuleError(
-//       'Your Lambda function is using an ES module. ' +
-//         "Please use the 'instana-aws-lambda-auto-wrap-esm.handler' as runtime handler."
-//     );
-// }
 
   try {
     return require(requirePath);
