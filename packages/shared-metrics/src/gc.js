df04199e7e13b16c3f897831dc023000166503bf
Arya <arya.mohanan@ibm.com>
2025-03-19 20:09:56 +0530

    chore: review
    Co-authored-by: kirrg001 <katharina.irrgang@ibm.com>

--- packages/shared-metrics/src/gc.js
+++ packages/shared-metrics/src/gc.js
@@ -46,7 +46,7 @@ exports.init = function init() {
     nativeModuleName: 'gcstats.js',
     moduleRoot: path.join(__dirname, '..'),
     message:
-      'Could not load gcstats.js. Garbage collection information will not be available in Instana for this ' +
+      'Could not load gcstats.js. Garbage collection information will not be available for this ' +
       'application. This typically occurs when native add-ons fail to build during module installation. ' +
       'However, general tracing functionality is unaffected. Enable debug logging for more details. ' +
       'For more information on native add-on, visit: ' +
