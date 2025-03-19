10060ae1a8437f0aa6eb28ee705f811cb570f8e1
Arya <arya.mohanan@ibm.com>
2025-03-19 19:59:17 +0530

    docs: improved the logs

--- packages/shared-metrics/src/libuv.js
+++ packages/shared-metrics/src/libuv.js
@@ -19,8 +19,8 @@ exports.init = function init() {
     nativeModuleName: 'event-loop-stats',
     moduleRoot: path.join(__dirname, '..'),
     message:
-      'Could not load event-loop-stats. Event loop information will be limited in Instana for this application. ' +
-      'This typically occurs when native add-ons fail to build during module installation. ' +
+      'Could not load event-loop-stats. Event loop information will not be available in Instana for this ' +
+      'application. This typically occurs when native add-ons fail to build during module installation. ' +
       'However, general tracing functionality remains unaffected. Enable debug logging for more details. ' +
       'For more information on native add-ons, visit: ' +
       'https://www.ibm.com/docs/en/instana-observability/current?topic=nodejs-collector-installation#native-add-ons'
