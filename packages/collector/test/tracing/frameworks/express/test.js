88cc6dc0274e781dbc3dca971b061bc6f025f53e
Abhilash <abhilash.sivan@ibm.com>
2025-03-27 19:22:29 +0530

    test: express test refactor

--- packages/collector/test/tracing/frameworks/express/test.js
+++ packages/collector/test/tracing/frameworks/express/test.js
@@ -15,9 +15,9 @@ const testUtils = require('../../../../../core/test/test_util');
 const ProcessControls = require('../../../test_util/ProcessControls');
 const globalAgent = require('../../../globalAgent');
 
-const mochaSuiteFn = supportedVersion(process.versions.node) ? describe : describe.skip;
+const mochaSuiteFn = supportedVersion(process.versions.node) ? describe.only : describe.skip;
 
-['latest', 'v5'].forEach(version => {
+['latest', 'v4'].forEach(version => {
   mochaSuiteFn(`tracing/express@${version}`, function () {
     this.timeout(config.getTestTimeout());
 
