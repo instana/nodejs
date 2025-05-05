4ac147e5fac22d215c51a00fa03cffffb7e9e348
Arya <arya.mohanan@ibm.com>
2025-05-05 19:24:10 +0530

    test: fix redis client in tests v4

--- packages/collector/test/tracing/database/redis/mockVersion.js
+++ packages/collector/test/tracing/database/redis/mockVersion.js
@@ -6,9 +6,20 @@
 
 const mock = require('@instana/core/test/test_util/mockRequire');
 
+const REDIS_PKG = process.env.REDIS_PKG;
 const REDIS_VERSION = process.env.REDIS_VERSION;
-const REDIS_REQUIRE = process.env.REDIS_VERSION === 'latest' ? 'redis' : `redis-${REDIS_VERSION}`;
 
-if (REDIS_REQUIRE !== 'redis') {
-  mock('redis', REDIS_REQUIRE);
+let mockedModuleName;
+
+if (REDIS_PKG === 'redis') {
+  mockedModuleName = REDIS_VERSION === 'latest' ? 'redis' : `redis-${REDIS_VERSION}`;
+} else if (REDIS_PKG === '@redis/client') {
+  // NOTE: The @redis/client package was introduced with Redis v4 (client v1).
+  // In Redis v5, the versioning of @redis/client was aligned with Redis itself,
+  // so @redis/client v5 corresponds to Redis v5.
+  mockedModuleName = REDIS_VERSION === 'latest' ? '@redis/client' : `@redis/client-${REDIS_VERSION}`;
+}
+
+if (mockedModuleName !== REDIS_PKG) {
+  mock(REDIS_PKG, mockedModuleName);
 }
