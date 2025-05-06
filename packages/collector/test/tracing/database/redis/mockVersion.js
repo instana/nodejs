90e98db8378e176af10f1b3c9345b0ef2b904a00
Arya <arya.mohanan@ibm.com>
2025-05-06 10:21:08 +0530

    test: mocking

--- packages/collector/test/tracing/database/redis/mockVersion.js
+++ packages/collector/test/tracing/database/redis/mockVersion.js
@@ -5,6 +5,7 @@
 'use strict';
 
 const mock = require('@instana/core/test/test_util/mockRequire');
+const hook = require('../../../../../core/src/util/hook');
 
 const REDIS_PKG = process.env.REDIS_PKG;
 const REDIS_VERSION = process.env.REDIS_VERSION;
@@ -23,3 +24,24 @@ if (REDIS_PKG === 'redis') {
 if (mockedModuleName !== REDIS_PKG) {
   mock(REDIS_PKG, mockedModuleName);
 }
+
+const originalOnFileLoad = hook.onFileLoad;
+
+hook.onFileLoad = function onFileLoad() {
+  const source = arguments[0]?.source;
+
+  if (
+    mockedModuleName.startsWith('@redis/client') &&
+    (source === '\\/@redis\\/client\\/dist\\/lib\\/commands\\/index.js' ||
+      source === '\\/@redis\\/client\\/dist\\/lib\\/cluster\\/commands.js')
+  ) {
+    const updatedSource = source.replace('@redis\\/client', mockedModuleName);
+
+    const regex = new RegExp(updatedSource);
+    arguments[0] = regex;
+
+    return originalOnFileLoad.apply(this, arguments);
+  }
+
+  return originalOnFileLoad.apply(this, arguments);
+};
