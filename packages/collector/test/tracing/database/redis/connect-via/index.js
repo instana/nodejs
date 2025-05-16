4674f6f13dc7989bc5bd9815e097f602700123b4
Arya <arya.mohanan@ibm.com>
2025-05-16 12:03:57 +0530

    feat: added redis sentinel support

--- packages/collector/test/tracing/database/redis/connect-via/index.js
+++ packages/collector/test/tracing/database/redis/connect-via/index.js
@@ -5,10 +5,18 @@
 'use strict';
 
 module.exports = function (redis, log) {
-  if (process.env.REDIS_CLUSTER === 'true') {
+  const setupType = process.env.REDIS_SETUP_TYPE;
+  const usePool = process.env.REDIS_POOL === 'true';
+
+  if (setupType === 'cluster') {
     return require('./cluster')(redis, log);
   }
-  if (process.env.REDIS_POOL === 'true') {
+
+  if (setupType === 'sentinel') {
+    return require('./sentinel')(redis, log);
+  }
+
+  if (usePool) {
     return require('./pool')(redis, log);
   }
 
