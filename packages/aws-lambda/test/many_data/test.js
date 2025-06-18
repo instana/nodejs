f1f142d430ad06e0da69a203c8e77d2cc51fd76e
kirrg001 <katharina.irrgang@ibm.com>
2025-06-18 23:18:12 +0200

    test(aws-lambda): clarified many spans tests (#1767)
    - splitted detached and many span tests for clarity and easier handling of span expecations
    - replaced instana.com host
    - replaced https usage
    - clarified batching and buffering cases

--- packages/aws-lambda/test/many_data/test.js
+++ packages/aws-lambda/test/many_data/test.js
@@ -25,9 +25,10 @@ const qualifiedArn = `${unqualifiedArn}:${version}`;
 
 const instanaAgentKey = 'aws-lambda-dummy-key';
 
-describe('multiple data lambda handler', function () {
-  describe('with 3 iterations', function () {
-    this.timeout(config.getTestTimeout() * 2);
+describe('aws-lambda: many data', function () {
+  this.timeout(config.getTestTimeout() * 2);
+
+  describe('[batching disabled] with 3 iterations', function () {
     let control;
 
     before(async () => {
@@ -71,31 +72,95 @@ describe('multiple data lambda handler', function () {
           verifyResponse(control, 1);
         })
         .then(() => {
-          return retry(
-            () => {
-              return Promise.all([
-                //
-                control.getSpans(),
-                control.getRawBundles(),
-                control.getRawSpanArrays()
-              ]).then(([spans, rawBundles, rawSpanArrays]) => {
-                verifySpans(spans);
-                expect(rawSpanArrays).to.be.an('array');
-                expect(rawSpanArrays).to.have.lengthOf(2);
-                expect(rawBundles).to.be.an('array');
-                expect(rawBundles).to.have.lengthOf.at.least(1);
-                expect(rawBundles).to.have.lengthOf.at.most(4);
-              });
-            },
-            null,
-            Date.now() + config.getRetryTimeout() * 2
-          );
+          return retry(() => {
+            return Promise.all([
+              //
+              control.getSpans(),
+              control.getRawBundles(),
+              control.getRawSpanArrays()
+            ]).then(([spans, rawBundles, rawSpanArrays]) => {
+              verifySpans(spans);
+              expect(rawSpanArrays).to.be.an('array');
+              // We only send one bundle request with 1 entry and 3 exits.
+              expect(rawSpanArrays.length).to.equal(0);
+              expect(rawBundles).to.be.an('array');
+              expect(rawBundles.length).to.equal(1);
+              expect(rawBundles[0].spans.length).to.equal(4);
+            });
+          }, 500);
         });
     });
   });
 
-  describe('[batching disabled] with 100 iterations', function () {
-    this.timeout(config.getTestTimeout() * 2);
+  describe('[batching enabled] with 100 iterations', function () {
+    let control;
+
+    before(async () => {
+      control = new Control({
+        faasRuntimePath: path.join(__dirname, '../runtime_mock'),
+        handlerDefinitionPath: path.join(__dirname, './lambda'),
+        startBackend: true,
+        env: {
+          INSTANA_AGENT_KEY: instanaAgentKey,
+          WITH_CONFIG: 'true',
+          INSTANA_NUMBER_OF_ITERATIONS: 100,
+          INSTANA_SPANBATCHING_ENABLED: 'true',
+          // Batching spans together is duration 10ms by default.
+          // aws-lambda tests do not use any containers,
+          // thats why we just force batching for http requests.
+          // An HTTP request to our local dummy is <1s
+          // We don't care about this configuration for this test.
+          INSTANA_DEV_BATCH_THRESHOLD: 500
+        }
+      });
+
+      await control.start();
+    });
+
+    beforeEach(async () => {
+      // wait a little to ensure no more spans are sent from the handler
+      // the handler sends some async request in the background
+      await delay(2000);
+
+      await control.reset();
+      await control.resetBackendSpansAndMetrics();
+    });
+
+    after(async () => {
+      await control.stop();
+    });
+
+    it('must capture metrics and all spans', () => {
+      return control
+        .runHandler()
+        .then(() => {
+          verifyResponse(control, 1);
+        })
+        .then(() => {
+          return retry(() => {
+            return Promise.all([
+              //
+              control.getSpans(),
+              control.getRawBundles(),
+              control.getRawSpanArrays()
+            ]).then(([spans, rawBundles, rawSpanArrays]) => {
+              // 1 x bundle request at the end of the lambda fn (entry span)
+              expect(rawBundles.length).to.equal(1);
+
+              // 1 x entry and 1 exit (100 batched http requests) span in the bundle.
+              expect(rawBundles[0].spans.length).to.equal(2);
+
+              // No additional /traces requests, only the one bundle request.
+              expect(rawSpanArrays.length).to.equal(0);
+
+              expect(spans.length).to.equal(2);
+            });
+          }, 500);
+        });
+    });
+  });
+
+  describe('[batching disabled] with 100 iterations, all at once', function () {
     let control;
 
     before(async () => {
@@ -133,35 +198,30 @@ describe('multiple data lambda handler', function () {
           verifyResponse(control, 1);
         })
         .then(() => {
-          return retry(
-            () => {
-              return Promise.all([
-                //
-                control.getSpans(),
-                control.getRawBundles(),
-                control.getRawSpanArrays()
-              ]).then(([spans, rawBundles, rawSpanArrays]) => {
-                // 1 X bundle request at the end of the lambda fn
-                expect(rawBundles.length).to.equal(1);
+          return retry(() => {
+            return Promise.all([
+              //
+              control.getSpans(),
+              control.getRawBundles(),
+              control.getRawSpanArrays()
+            ]).then(([spans, rawBundles, rawSpanArrays]) => {
+              // 1 X bundle request at the end of the lambda fn
+              expect(rawBundles.length).to.equal(1);
 
-                // All spans are sent at the end of the lambda fn
-                expect(rawBundles[0].spans.length).to.equal(101);
+              // all spans are sent at the end of the lambda fn
+              expect(rawBundles[0].spans.length).to.equal(101);
 
-                // 0 requests from span buffer.
-                expect(rawSpanArrays.length).to.equal(0);
+              // 0 requests from span buffer.
+              expect(rawSpanArrays.length).to.equal(0);
 
-                expect(spans.length).to.equal(101);
-              });
-            },
-            null,
-            Date.now() + config.getRetryTimeout() * 2
-          );
+              expect(spans.length).to.equal(101);
+            });
+          }, 500);
         });
     });
   });
 
-  describe('[batching enabled] with 100 iterations', function () {
-    this.timeout(config.getTestTimeout() * 2);
+  describe('[batching disabled] with 100 iterations, 10 exits per request', function () {
     let control;
 
     before(async () => {
@@ -173,7 +233,7 @@ describe('multiple data lambda handler', function () {
           INSTANA_AGENT_KEY: instanaAgentKey,
           WITH_CONFIG: 'true',
           INSTANA_NUMBER_OF_ITERATIONS: 100,
-          INSTANA_SPANBATCHING_ENABLED: 'true',
+          // TODO: This is currently off by default.
           INSTANA_FORCE_TRANSMISSION_STARTING_AT: 10,
           INSTANA_DEV_MIN_DELAY_BEFORE_SENDING_SPANS: 100
         }
@@ -202,41 +262,35 @@ describe('multiple data lambda handler', function () {
           verifyResponse(control, 1);
         })
         .then(() => {
-          return retry(
-            () => {
-              return Promise.all([
-                //
-                control.getSpans(),
-                control.getRawBundles(),
-                control.getRawSpanArrays()
-              ]).then(([spans, rawBundles, rawSpanArrays]) => {
-                // 1 X bundle requestat the end of the lambda fn
-                expect(rawBundles.length).to.equal(1);
+          return retry(() => {
+            return Promise.all([
+              //
+              control.getSpans(),
+              control.getRawBundles(),
+              control.getRawSpanArrays()
+            ]).then(([spans, rawBundles, rawSpanArrays]) => {
+              // 1 X bundle request at the end of the lambda fn
+              // contains the entry span!
+              expect(rawBundles.length).to.equal(1);
 
-                // This is the delayed request.
-                expect(rawBundles[0].spans.length).to.equal(1);
+              // 1 x entry
+              expect(rawBundles[0].spans.length).to.equal(1);
 
-                // 10 requests from span buffer.
-                expect(rawSpanArrays.length).to.equal(12);
-                expect(rawSpanArrays[0].length).to.equal(10);
-                expect(rawSpanArrays[1].length).to.equal(10);
-                expect(rawSpanArrays[2].length).to.equal(10);
-                expect(rawSpanArrays[3].length).to.equal(10);
-                expect(rawSpanArrays[4].length).to.equal(10);
-                expect(rawSpanArrays[5].length).to.equal(10);
-                expect(rawSpanArrays[6].length).to.equal(10);
-                expect(rawSpanArrays[7].length).to.equal(10);
-                expect(rawSpanArrays[8].length).to.equal(10);
-                expect(rawSpanArrays[9].length).to.equal(10);
-                expect(rawSpanArrays[10].length).to.equal(1);
-                expect(rawSpanArrays[11].length).to.equal(1);
+              // x requests buffered / splitted
+              expect(rawSpanArrays.length).to.equal(10);
+              expect(rawSpanArrays[0].length).to.equal(10);
+              expect(rawSpanArrays[1].length).to.equal(10);
+              expect(rawSpanArrays[2].length).to.equal(10);
+              expect(rawSpanArrays[3].length).to.equal(10);
+              expect(rawSpanArrays[4].length).to.equal(10);
+              expect(rawSpanArrays[5].length).to.equal(10);
+              expect(rawSpanArrays[6].length).to.equal(10);
+              expect(rawSpanArrays[7].length).to.equal(10);
+              expect(rawSpanArrays[8].length).to.equal(10);
 
-                expect(spans.length).to.equal(103);
-              });
-            },
-            null,
-            Date.now() + config.getRetryTimeout() * 2
-          );
+              expect(spans.length).to.equal(101);
+            });
+          }, 500);
         });
     });
   });
@@ -264,7 +318,7 @@ describe('multiple data lambda handler', function () {
   }
 
   function verifyHttpExit(spans) {
-    return expectExactlyNMatching(spans, 5, span => {
+    return expectExactlyNMatching(spans, 3, span => {
       expect(span.s).to.exist;
       expect(span.n).to.equal('node.http.client');
       expect(span.k).to.equal(constants.EXIT);
@@ -274,7 +328,7 @@ describe('multiple data lambda handler', function () {
       expect(span.f.cp).to.equal('aws');
       expect(span.data.http).to.be.an('object');
       expect(span.data.http.method).to.equal('GET');
-      expect(span.data.http.url).to.equal('https://www.instana.com/');
+      expect(span.data.http.url).to.contain('localhost');
     });
   }
 
