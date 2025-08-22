dd6954703b496de1770aa2bf91384c438d29b753
kirrg001 <katharina.irrgang@ibm.com>
2025-08-22 16:30:03 +0200

    chore: fixed mocha watch
    - with https://github.com/instana/nodejs/commit/73f789070f86a8de502fd6f6dbb9b83dd3c97051
      the node_modules changes got ignored, but the watch command did not work at all anymore
    - I feel like watchFiles and watchIgnore are not working well together
    - This should do the trick for now.

--- packages/collector/test/.mocharc.js
+++ packages/collector/test/.mocharc.js
@@ -3,8 +3,7 @@
 const mochaOptions = {
   file: ['test/initEnv.js'],
   ignore: ['node_modules/**/*', 'test/**/node_modules/**/*'],
-  watchFiles: ['test/**/*.{js,mjs}', '../*/src/**/*.{js,mjs}'],
-  'watch-ignore': ['test/**/node_modules']
+  'watch-ignore': ['test/**/node_modules/**/*']
 };
 
 process.env.NODE_ENV = 'test';
--- packages/metrics-util/test/.mocharc.js
+++ packages/metrics-util/test/.mocharc.js
@@ -2,8 +2,7 @@
 
 const mochaOptions = {
   ignore: ['node_modules/**/*', 'test/**/node_modules/**/*'],
-  watchFiles: ['test/**/*.{js,mjs}', '../*/src/**/*.{js,mjs}'],
-  'watch-ignore': ['test/**/node_modules']
+  'watch-ignore': ['test/**/node_modules/**/*']
 };
 
 module.exports = mochaOptions;
