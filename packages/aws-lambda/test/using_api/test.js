05f5cec204658d77604bc487a03b5473941367a5
Arya <arya.mohanan@ibm.com>
2025-08-25 12:20:50 +0530

    test: updated the log assertion in serverless tests
    Updated the backendConnector log in https://github.com/instana/nodejs/pull/1963

--- packages/aws-lambda/test/using_api/test.js
+++ packages/aws-lambda/test/using_api/test.js
@@ -474,11 +474,11 @@ describe('Using the API', function () {
 
         if (isDebug) {
           expect(body.logs.debug).to.satisfy(logs => {
-            return logs.some(log => /\[instana_\w+\] Sending trace data to Instana \.*/.test(log));
+            return logs.some(log => /\[instana_\w+\] Sending data to Instana \.*/.test(log));
           });
 
           expect(body.logs.debug).to.satisfy(logs => {
-            return logs.some(log => /\[instana_\w+\] The trace data have been successfully sent to Instana/.test(log));
+            return logs.some(log => /\[instana_\w+\] The data have been successfully sent to Instana/.test(log));
           });
 
           // We run http by default in the tests. No need to set INSTANA_DISABLE_CA_CHECK
@@ -499,11 +499,11 @@ describe('Using the API', function () {
           }
         } else {
           expect(body.logs.debug).to.satisfy(logs => {
-            return logs.some(log => /\[instana] Sending trace data to Instana \.*/.test(log));
+            return logs.some(log => /\[instana] Sending data to Instana \.*/.test(log));
           });
 
           expect(body.logs.debug).to.satisfy(logs => {
-            return logs.some(log => /\[instana] The trace data have been successfully sent to Instana/.test(log));
+            return logs.some(log => /\[instana] The data have been successfully sent to Instana/.test(log));
           });
 
           if (backendUsesHttps) {
