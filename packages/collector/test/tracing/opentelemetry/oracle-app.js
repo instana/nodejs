ff6fd3ea02e76c3aa3c8b2167e18cbaaad4c6c9e
kirrg001 <katharina.irrgang@ibm.com>
2025-09-08 13:59:41 +0200

    chore: fixes

--- packages/collector/test/tracing/opentelemetry/oracle-app.js
+++ packages/collector/test/tracing/opentelemetry/oracle-app.js
@@ -14,7 +14,7 @@ process.on('SIGTERM', () => {
 
 const opentelemetryDisabled = process.env.INSTANA_DISABLE_USE_OPENTELEMETRY === 'true';
 
-require('./node_modules/@instana/collector')({
+require('@instana/collector')({
   tracing: {
     useOpentelemetry: !opentelemetryDisabled
   }
@@ -22,6 +22,7 @@ require('./node_modules/@instana/collector')({
 
 const oracledb = require('oracledb');
 const port = require('../../test_util/app-port')();
+const delay = require('@instana/core/test/test_util/delay');
 const logPrefix = `Oracle App (${process.pid}):\t`;
 const express = require('express');
 const app = express();
@@ -29,18 +30,21 @@ const app = express();
 let connected = false;
 let connection;
 
-(async () => {
+(async function connect() {
   try {
     connection = await oracledb.getConnection({
       user: 'teamnodejs',
       password: 'teamnodejspassword',
-      connectString: `${process.env.ORACLEDB}/XEPDB1`
+      connectString: `${process.env.ORACLEDB}/FREEPDB1`
     });
 
     connected = true;
     log('Connected to OracleDB');
   } catch (err) {
     log('Failed to connect to OracleDB:', err);
+    log('Retrying in a few seconds...');
+    await delay(1000 * 5);
+    await connect();
   }
 })();
 
