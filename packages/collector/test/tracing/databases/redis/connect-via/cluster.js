38c4a331c6212b293f9d010236113b72f5cb762b
Arya <arya.mohanan@ibm.com>
2025-09-16 15:45:33 +0530

    test: updated comments in the redis cluster test (#1999)
    This was missed while updating #1979

--- packages/collector/test/tracing/databases/redis/connect-via/cluster.js
+++ packages/collector/test/tracing/databases/redis/connect-via/cluster.js
@@ -10,10 +10,13 @@ const { delay } = require('../../../../../../core/test/test_util');
 // NOTE: We run the tests locally and on CI against azure redis cluster.
 // NOTE: We cannot run redis cluster on Tekton https://github.com/bitnami/charts/issues/28894
 // NOTE: We cannot use a docker based redis cluster at the moment!
-//       See https://github.com/redis/node-redis/issues/2815.
-//       These commands are useful as soon as we switch to a docker based cluster.
-//       node bin/start-test-containers.js --redis-node-0 --redis-node-1 --redis-node-2
-//       docker exec -it 2aaaac7b9112 redis-cli -p 6379 cluster info
+//       See https://github.com/redis/node-redis/issues/2815
+// NOTE: The Docker-based Redis cluster(image:bitnami/redis-cluster) was removed from Docker Compose,
+//       as it was no longer used locally and will require a paid subscription after Aug 28, 2025:
+//       https://bitnami.com/announcements/bitnami-docker-image-changes
+// NOTE: If a local Docker setup is needed in the future, we can explore
+//       alternative images or solutions.
+
 module.exports = async function connect(redis, log, tries = 0) {
   if (!process.env.AZURE_REDIS_CLUSTER || !process.env.AZURE_REDIS_CLUSTER_PWD) {
     log(
