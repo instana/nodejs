f40761ba8cc4ae9a0ef4e34d89be29f4bd76f9ce
kirrg001 <katharina.irrgang@ibm.com>
2025-10-28 10:35:08 +0100

    chore: clarified comment

--- packages/collector/src/uninstrumentedLogger.js
+++ packages/collector/src/uninstrumentedLogger.js
@@ -8,11 +8,15 @@ const pinoWasRequiredBeforeUs = Object.keys(require.cache).some(key => key.inclu
 
 // NOTE: The pino instrumentation is not yet loaded. The logger is loaded very early in our codebase.
 //       Therefore our internal pino instance will not be instrumented.
-//       BUT this require still loads pino into the internal node.js cache!
-//       That means if the customer is using the SAME npm version (our version), it won't get traced!
-//       Thats why we have to remove the cache entry manually after the require.
+//       BUT our pino require still loads pino into the internal node.js cache. Especially the files of the pino library
+//       such as lib/tools.js. These sub requires are not triggered anymore when pino is required the second time.
+//       That means if the customer is using the SAME npm version (our version), it won't get traced without our manual
+//       cache deletion below.
 //       If the customer has its own pino version, it will be traced correctly, because it will trigger "onFileLoad".
-// TODO: Consider migrating to "onModuleLoad". This will remove the need of having to manually delete the cache entry.
+// TODO: Consider migrating to "onModuleLoad". This will remove the need of having to manually delete the cache entry,
+//       because any later pino require will trigger the require hook again with "pino" as module name.
+//       Furthermore, using "onModuleLoad" is much more clean because we don't have to rely on the precense of
+//       lib/tools.js.
 // SEE:  https://jsw.ibm.com/browse/INSTA-23066
 
 // eslint-disable-next-line import/no-extraneous-dependencies, instana/no-unsafe-require
