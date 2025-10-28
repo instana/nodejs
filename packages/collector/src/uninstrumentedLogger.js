ff5847d5c000e9eac67efc81e215f11c4fb01b83
kirrg001 <katharina.irrgang@ibm.com>
2025-10-28 09:46:43 +0100

    chore: wording

--- packages/collector/src/uninstrumentedLogger.js
+++ packages/collector/src/uninstrumentedLogger.js
@@ -12,7 +12,7 @@ const pinoWasRequiredBeforeUs = Object.keys(require.cache).some(key => key.inclu
 //       That means if the customer is using the SAME npm version (our version), it won't get traced!
 //       Thats why we have to remove the cache entry manually after the require.
 //       If the customer has its own pino version, it will be traced correctly, because it will trigger "onFileLoad".
-// TODO: Migrate pino to "onModuleLoad". This will remove the need of having to manually delete the cache entry.
+// TODO: Consider migrating to "onModuleLoad". This will remove the need of having to manually delete the cache entry.
 // SEE:  https://jsw.ibm.com/browse/INSTA-23066
 
 // eslint-disable-next-line import/no-extraneous-dependencies, instana/no-unsafe-require
