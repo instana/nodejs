9740f26bb67ea23cd3af7bd32e6a48fd6655bfbb
Arya <arya.mohanan@ibm.com>
2025-11-06 16:42:10 +0530

    chore(example-apps): added nextjs example app

---
+++ example-apps/nextjs-app/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div
+      style={{ fontFamily: 'Arial', textAlign: 'center', marginTop: '50px' }}
+    >
+      <h1>Instana Next.js API Test</h1>
+      <p>
+        API available at <a href='/api/hello'>/api/hello</a>
+      </p>
+    </div>
+  );
+}
