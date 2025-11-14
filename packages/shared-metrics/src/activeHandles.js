e0bb6f8c9b95a530909a32d0a1baa976d1399b18
kirrg001 <katharina.irrgang@ibm.com>
2025-11-14 11:42:14 +0100

    chore: extended docs for activeHandles and activeRequests
    refs https://jsw.ibm.com/browse/INSTA-64277

--- packages/shared-metrics/src/activeHandles.js
+++ packages/shared-metrics/src/activeHandles.js
@@ -11,7 +11,8 @@ Object.defineProperty(exports, 'currentPayload', {
   get: function () {
     // TODO: _getActiveHandles is deprecated. Replace with getActiveResourcesInfo.
     //       https://nodejs.org/api/deprecations.html#dep0161-process_getactiverequests-and-process_getactivehandles
-    //       Added in v16. Replace in major release.
+    //       Added in v16.
+    // refs https://jsw.ibm.com/browse/INSTA-64277
     // @ts-ignore
     return process._getActiveHandles().length;
   }
--- packages/shared-metrics/src/activeRequests.js
+++ packages/shared-metrics/src/activeRequests.js
@@ -9,6 +9,10 @@ exports.payloadPrefix = 'activeRequests';
 
 Object.defineProperty(exports, 'currentPayload', {
   get: function () {
+    // TODO: _getActiveRequests is deprecated. Replace with getActiveResourcesInfo.
+    //       https://nodejs.org/api/deprecations.html#dep0161-process_getactiverequests-and-process_getactivehandles
+    //       Added in v16.
+    // refs https://jsw.ibm.com/browse/INSTA-64277
     // @ts-ignore
     return process._getActiveRequests().length;
   }
