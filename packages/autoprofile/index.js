1a30364a47f0526d910c07dab40af77885ad39c2
Abhilash <abhilash.sivan@ibm.com>
2025-11-20 18:11:57 +0530

    chore: update

--- packages/autoprofile/index.js
+++ packages/autoprofile/index.js
@@ -3,14 +3,6 @@
  * (c) Copyright Instana Inc. and contributors 2020
  */
 
-/*
- * NOTE: Even though the 'autoprofile' directory is excluded in tsconfig, the TypeScript
- * compiler (with allowJs/checkJs) still type-checks its source files when they are imported
- * see: https://www.typescriptlang.org/tsconfig/#exclude
- * To skip checking, we must either refactor to a
- * package-name import or use an explicit // @ts-nocheck directive
- */
-
 // @ts-nocheck
 
 const AutoProfiler = require('./lib/auto_profiler').AutoProfiler;
