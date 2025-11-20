2e2703034f24017519ef796f5eb0488a9458b483
Abhilash <abhilash.sivan@ibm.com>
2025-11-20 18:28:01 +0530

    chore: update the comments

--- packages/autoprofile/index.js
+++ packages/autoprofile/index.js
@@ -3,6 +3,9 @@
  * (c) Copyright Instana Inc. and contributors 2020
  */
 
+// TODO: remove @ts-nocheck and add proper typing and
+// linting support for @instana/autoprofile (INSTA-65031)
+
 // @ts-nocheck
 
 const AutoProfiler = require('./lib/auto_profiler').AutoProfiler;
