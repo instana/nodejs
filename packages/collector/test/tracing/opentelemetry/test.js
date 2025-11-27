4388d65bc95f58ae9b5c9349140c71647efb58b1
kirrg001 <katharina.irrgang@ibm.com>
2025-11-27 22:18:19 +0100

    chore: fixes

--- packages/collector/test/tracing/opentelemetry/test.js
+++ packages/collector/test/tracing/opentelemetry/test.js
@@ -1025,7 +1025,7 @@ function checkTelemetryResourceAttrs(span) {
   expect(span.data.resource['service.name']).to.not.exist;
   expect(span.data.resource['telemetry.sdk.language']).to.eql('nodejs');
   expect(span.data.resource['telemetry.sdk.name']).to.eql('opentelemetry');
-  expect(span.data.resource['telemetry.sdk.version']).to.match(/1\.\d+\.\d/);
+  expect(span.data.resource['telemetry.sdk.version']).to.match(/2\.\d+\.\d/);
 }
 
 function verifyHttpExit(spans, parentSpan) {
