d4a66f35243b120b8f8909b1653ed42355badda7
kirrg001 <katharina.irrgang@ibm.com>
2025-11-28 14:33:58 +0100

    chore: fixed tedious

--- packages/collector/test/tracing/opentelemetry/test.js
+++ packages/collector/test/tracing/opentelemetry/test.js
@@ -556,7 +556,7 @@ mochaSuiteFn('opentelemetry tests', function () {
             ));
       });
 
-      describe.skip('tedious', function () {
+      describe('tedious', function () {
         describe('opentelemetry is enabled', function () {
           globalAgent.setUpCleanUpHooks();
           const agentControls = globalAgent.instance;
