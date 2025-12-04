31c44350483367ea633049f10ebd6e57cbd03fae
kirrg001 <katharina.irrgang@ibm.com>
2025-12-04 08:46:08 +0100

    fix: bumped @opentelemetry/sdk-trace-base to v2 (#2196)
    refs https://jsw.ibm.com/browse/INSTA-65941

--- packages/core/src/tracing/opentelemetry-instrumentations/tedious.js
+++ packages/core/src/tracing/opentelemetry-instrumentations/tedious.js
@@ -18,14 +18,6 @@ module.exports.init = () => {
     instrumentation.enable();
   }
 };
-exports.transform = otelSpan => {
-  // NOTE: This assignment is necessary to display the database name in the UI.
-  // In the backend, for OpenTelemetry, the service name is based on the OpenTelemetry span attribute service.name.
-  if (otelSpan.attributes && 'db.name' in otelSpan.attributes) {
-    otelSpan.resource._attributes['service.name'] = otelSpan.attributes['db.name'];
-  }
-  return otelSpan;
-};
 
 module.exports.getKind = () => {
   const kind = constants.EXIT;
