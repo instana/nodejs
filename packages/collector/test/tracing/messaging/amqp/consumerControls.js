ab65a7c877452c2321788e92aa8deff0d2f1d212
kirrg001 <katharina.irrgang@ibm.com>
2025-12-15 12:24:30 +0100

    test: fixes

--- packages/collector/test/tracing/messaging/amqp/consumerControls.js
+++ packages/collector/test/tracing/messaging/amqp/consumerControls.js
@@ -22,6 +22,7 @@ exports.registerTestHooks = opts => {
     env.TRACING_ENABLED = opts.enableTracing !== false;
     env.AMQPLIB_VERSION = opts.version;
     env.INSTANA_RETRY_AGENT_CONNECTION_IN_MS = 100;
+    env.INSTANA_IPC_ENABLED = 'true';
 
     app = spawn('node', [path.join(__dirname, `consumer${opts.apiType}.js`)], {
       stdio: config.getAppStdio(),
