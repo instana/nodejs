0ca714490edd3ccce71381e0aa0f4e6c40bcb519
Arya <arya.mohanan@ibm.com>
2026-01-09 15:55:36 +0530

    test: make node-fetch version handling explicit in tests (#2264)

--- packages/collector/test/tracing/misc/node-fetch/test.js
+++ packages/collector/test/tracing/misc/node-fetch/test.js
@@ -14,68 +14,79 @@ const globalAgent = require('../../../globalAgent');
 
 const mochaSuiteFn = supportedVersion(process.versions.node) ? describe : describe.skip;
 
-mochaSuiteFn('tracing/node-fetch', function () {
-  this.timeout(config.getTestTimeout());
+['latest', 'v2'].forEach(version => {
+  mochaSuiteFn(`tracing/node-fetch @${version}`, function () {
+    // Skip testing the latest version in CommonJS mode, as it is ESM-only.
+    // From v3 onwards, node-fetch is a pure ESM module and does not support require().
+    if (!process.env.RUN_ESM && version === 'latest') return;
 
-  globalAgent.setUpCleanUpHooks();
-  const agentControls = globalAgent.instance;
+    // Curently we do not run ESM tests for all versions, so skip the ESM app for non-latest versions.
+    // TODO: Support for mocking `import` in ESM apps is planned under INSTA-788.
+    if (process.env.RUN_ESM && version !== 'latest') return;
 
-  let controls;
+    this.timeout(config.getTestTimeout());
 
-  before(async () => {
-    controls = new ProcessControls({
-      dirname: __dirname,
-      useGlobalAgent: true
+    globalAgent.setUpCleanUpHooks();
+    const agentControls = globalAgent.instance;
+
+    let controls;
+
+    before(async () => {
+      controls = new ProcessControls({
+        dirname: __dirname,
+        useGlobalAgent: true,
+        env: { NODE_FETCH_VERSION: version }
+      });
+
+      await controls.startAndWaitForAgentConnection();
     });
 
-    await controls.startAndWaitForAgentConnection();
+    beforeEach(async () => {
+      await agentControls.clearReceivedTraceData();
+    });
+
+    after(async () => {
+      await controls.stop();
+    });
+
+    afterEach(async () => {
+      await controls.clearIpcMessages();
+    });
+
+    it('GET request', () =>
+      controls
+        .sendRequest({
+          method: 'GET',
+          path: '/request'
+        })
+        .then(() =>
+          retry(() =>
+            agentControls.getSpans().then(spans => {
+              expect(spans.length).to.equal(2);
+
+              const httpEntry = verifyHttpRootEntry({
+                spans,
+                apiPath: '/request',
+                pid: String(controls.getPid())
+              });
+
+              verifyExitSpan({
+                spanName: 'node.http.client',
+                spans,
+                parent: httpEntry,
+                withError: false,
+                pid: String(controls.getPid()),
+                dataProperty: 'http',
+                extraTests: [
+                  span => {
+                    expect(span.data.http.method).to.equal('GET');
+                    expect(span.data.http.url).to.equal(`http://127.0.0.1:${agentControls.agentPort}/ping`);
+                    expect(span.data.http.status).to.equal(200);
+                  }
+                ]
+              });
+            })
+          )
+        ));
   });
-
-  beforeEach(async () => {
-    await agentControls.clearReceivedTraceData();
-  });
-
-  after(async () => {
-    await controls.stop();
-  });
-
-  afterEach(async () => {
-    await controls.clearIpcMessages();
-  });
-
-  it('GET request', () =>
-    controls
-      .sendRequest({
-        method: 'GET',
-        path: '/request'
-      })
-      .then(() =>
-        retry(() =>
-          agentControls.getSpans().then(spans => {
-            expect(spans.length).to.equal(2);
-
-            const httpEntry = verifyHttpRootEntry({
-              spans,
-              apiPath: '/request',
-              pid: String(controls.getPid())
-            });
-
-            verifyExitSpan({
-              spanName: 'node.http.client',
-              spans,
-              parent: httpEntry,
-              withError: false,
-              pid: String(controls.getPid()),
-              dataProperty: 'http',
-              extraTests: [
-                span => {
-                  expect(span.data.http.method).to.equal('GET');
-                  expect(span.data.http.url).to.equal(`http://127.0.0.1:${agentControls.agentPort}/ping`);
-                  expect(span.data.http.status).to.equal(200);
-                }
-              ]
-            });
-          })
-        )
-      ));
 });
