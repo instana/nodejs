c681c4c56f0141788a70ebe0b89bb747e4d1d5a3
Abhilash <abhilash.sivan@ibm.com>
2026-01-15 14:34:47 +0530

    chore: updated more libs

--- packages/core/src/tracing/instrumentation/cloud/aws-sdk/v3/sqs-consumer.js
+++ packages/core/src/tracing/instrumentation/cloud/aws-sdk/v3/sqs-consumer.js
@@ -24,17 +24,22 @@ function instrument(SQSConsumer) {
           span.disableAutoEnd();
           const res = orig.apply(this, arguments);
 
-          res
-            .then(() => {
-              span.d = Date.now() - span.ts;
-              span.transmitManual();
-            })
-            .catch(err => {
-              span.ec = 1;
-              tracingUtil.setErrorDetails(span, err, 'sqs');
-              span.d = Date.now() - span.ts;
-              span.transmitManual();
-            });
+          if (res && typeof res.then === 'function') {
+            res
+              .then(() => {
+                span.d = Date.now() - span.ts;
+                span.transmitManual();
+              })
+              .catch(err => {
+                span.ec = 1;
+                tracingUtil.setErrorDetails(span, err, 'sqs');
+                span.d = Date.now() - span.ts;
+                span.transmitManual();
+              });
+          } else {
+            span.d = Date.now() - span.ts;
+            span.transmitManual();
+          }
 
           return res;
         });
@@ -56,17 +61,22 @@ function instrument(SQSConsumer) {
           span.disableAutoEnd();
           const res = orig.apply(this, arguments);
 
-          res
-            .then(() => {
-              span.d = Date.now() - span.ts;
-              span.transmitManual();
-            })
-            .catch(err => {
-              span.ec = 1;
-              tracingUtil.setErrorDetails(span, err, 'sqs');
-              span.d = Date.now() - span.ts;
-              span.transmitManual();
-            });
+          if (res && typeof res.then === 'function') {
+            res
+              .then(() => {
+                span.d = Date.now() - span.ts;
+                span.transmitManual();
+              })
+              .catch(err => {
+                span.ec = 1;
+                tracingUtil.setErrorDetails(span, err, 'sqs');
+                span.d = Date.now() - span.ts;
+                span.transmitManual();
+              });
+          } else {
+            span.d = Date.now() - span.ts;
+            span.transmitManual();
+          }
 
           return res;
         });
