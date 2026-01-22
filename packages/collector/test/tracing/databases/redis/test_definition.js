84127613ee669b5bcf678f23f52bc48e2fde0212
Arya <arya.mohanan@ibm.com>
2026-01-22 09:54:43 +0530

    test(redis): applied legacymode only for redis v4

--- packages/collector/test/tracing/databases/redis/test_definition.js
+++ packages/collector/test/tracing/databases/redis/test_definition.js
@@ -77,6 +77,11 @@ module.exports = function (setupType, legacyMode = false) {
           mochaSuiteFn = describe.skip;
         }
 
+        // Legacy mode is only supported in Redis v4, skip tests for other versions
+        if (legacyMode === true && redisVersion !== 'v4') {
+          mochaSuiteFn = describe.skip;
+        }
+
         // The allowRootExitSpanApp is compatable with Redis v4 and v5 (latest).
         if (redisVersion !== legacyVersion && legacyMode === false) {
           mochaSuiteFn('When allowRootExitSpan: true is set', function () {
