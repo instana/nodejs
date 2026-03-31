69b1551bad46bf643d7b93e3d851482229e26485
Abhilash <70062455+abhilash-sivan@users.noreply.github.com>
2026-03-31 20:46:46 +0530

    test: added comment in socket.io test (#2452)
    refs https://jsw.ibm.com/browse/INSTA-70626

--- packages/collector/test/integration/currencies/messaging/socket.io/test_base.js
+++ packages/collector/test/integration/currencies/messaging/socket.io/test_base.js
@@ -92,25 +92,7 @@ module.exports = function (name, version, isLatest) {
                     pid: String(serverControls.getPid())
                   });
 
-                  verifyEntrySpan({
-                    spanName: 'otel',
-                    spans,
-                    withError: false,
-                    pid: String(serverControls.getPid()),
-                    dataProperty: 'tags',
-                    extraTests: span => {
-                      expect(span.data.operation).to.equal('socket.io');
-                      expect(span.data.tags.name).to.contain('receive');
-                      expect(span.data.tags['messaging.system']).to.eql('socket.io');
-                      expect(span.data.tags['messaging.destination']).to.eql('ON test_reply');
-                      expect(span.data.tags['messaging.operation']).to.eql('receive');
-                      expect(span.data.tags['messaging.socket.io.event_name']).to.eql('test_reply');
-
-                      checkTelemetryResourceAttrs(span);
-                    }
-                  });
-
-                  verifyExitSpan({
+                  const serverSocketExit = verifyExitSpan({
                     spanName: 'otel',
                     spans,
                     parent: httpEntry,
@@ -118,12 +100,37 @@ module.exports = function (name, version, isLatest) {
                     pid: String(serverControls.getPid()),
                     dataProperty: 'tags',
                     extraTests: span => {
+                      expect(span.t).to.equal(httpEntry.t);
+                      expect(span.p).to.equal(httpEntry.s);
                       expect(span.data.tags.name).to.contain('send');
                       expect(span.data.tags['messaging.system']).to.eql('socket.io');
                       expect(span.data.tags['messaging.destination_kind']).to.eql('topic');
                       expect(span.data.tags['messaging.socket.io.event_name']).to.eql('test');
                       expect(span.data.tags['messaging.socket.io.namespace']).to.eql('/');
                       expect(span.data.tags['messaging.destination']).to.eql('EMIT test');
+
+                      checkTelemetryResourceAttrs(span);
+                    }
+                  });
+
+                  verifyEntrySpan({
+                    spanName: 'otel',
+                    spans,
+                    parent: serverSocketExit,
+                    withError: false,
+                    pid: String(serverControls.getPid()),
+                    dataProperty: 'tags',
+                    extraTests: span => {
+                      // TODO: Uncomment these assertions once ctx propagation is complete
+                      // (https://jsw.ibm.com/browse/INSTA-83330)
+                      // expect(span.t).to.equal(httpEntry.t);
+                      // expect(span.p).to.equal(serverSocketExit.s);
+                      expect(span.data.operation).to.equal('socket.io');
+                      expect(span.data.tags.name).to.contain('receive');
+                      expect(span.data.tags['messaging.system']).to.eql('socket.io');
+                      expect(span.data.tags['messaging.destination']).to.eql('ON test_reply');
+                      expect(span.data.tags['messaging.operation']).to.eql('receive');
+                      expect(span.data.tags['messaging.socket.io.event_name']).to.eql('test_reply');
 
                       checkTelemetryResourceAttrs(span);
                     }
