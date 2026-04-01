4abf3fe83e4869718c53e9af2361ea454c8a6be8
Arya <arya.mohanan@ibm.com>
2026-04-01 11:31:53 +0530

    refactor(collector): refcatored config module

--- packages/collector/src/util/normalizeConfig.js
+++ packages/collector/src/util/normalizeConfig.js
@@ -16,38 +16,129 @@ const defaults = {
 
 /**
  * Merges the config that was passed to the init function with environment variables and default values.
- * @param {import('../types/collector').CollectorConfig} config
+ * @param {import('../types/collector').CollectorConfig} userConfig
  * @returns {import('../types/collector').CollectorConfig}
  */
-module.exports = function normalizeConfig(config = {}) {
-  config.agentHost = config.agentHost || process.env.INSTANA_AGENT_HOST || defaults.agentHost;
-  config.agentPort = config.agentPort || parseToPositiveInteger(process.env.INSTANA_AGENT_PORT, defaults.agentPort);
-  config.agentRequestTimeout =
-    config.agentRequestTimeout ||
-    parseToPositiveInteger(process.env.INSTANA_AGENT_REQUEST_TIMEOUT, defaults.agentRequestTimeout);
+module.exports = function normalizeConfig(userConfig = {}) {
+  const finalConfig = {};
 
-  config.autoProfile = config.autoProfile || process.env.INSTANA_AUTO_PROFILE || defaults.autoProfile;
-  config.tracing = config.tracing || {};
+  finalConfig.agentHost = normalizeAgentHost(userConfig, defaults);
+  finalConfig.agentPort = normalizeAgentPort(userConfig, defaults);
+  finalConfig.agentRequestTimeout = normalizeAgentRequestTimeout(userConfig, defaults);
+  finalConfig.autoProfile = normalizeAutoProfile(userConfig, defaults);
+  finalConfig.tracing = normalizeTracing(userConfig);
+  finalConfig.reportUnhandledPromiseRejections = normalizeUnhandledRejections(userConfig);
 
-  if (config.reportUnhandledPromiseRejections == null) {
-    config.reportUnhandledPromiseRejections = false;
-  }
-
-  return config;
+  return finalConfig;
 };
 
 /**
- * @param {string | number} value
+ * @param {import('../types/collector').CollectorConfig} userConfig
+ * @param {{ agentHost: string }} defaultConfig
+ * @returns {string}
+ */
+function normalizeAgentHost(userConfig, defaultConfig) {
+  return resolveConfig(process.env.INSTANA_AGENT_HOST, userConfig.agentHost, defaultConfig.agentHost);
+}
+
+/**
+ * @param {import('../types/collector').CollectorConfig} userConfig
+ * @param {{ agentPort: number }} defaultConfig
+ * @returns {number}
+ */
+function normalizeAgentPort(userConfig, defaultConfig) {
+  return resolveNumericConfig(process.env.INSTANA_AGENT_PORT, userConfig.agentPort, defaultConfig.agentPort);
+}
+
+/**
+ * @param {import('../types/collector').CollectorConfig} userConfig
+ * @param {{ agentRequestTimeout: number }} defaultConfig
+ * @returns {number}
+ */
+function normalizeAgentRequestTimeout(userConfig, defaultConfig) {
+  return resolveNumericConfig(
+    process.env.INSTANA_AGENT_REQUEST_TIMEOUT,
+    userConfig.agentRequestTimeout,
+    defaultConfig.agentRequestTimeout
+  );
+}
+
+/**
+ * @param {import('../types/collector').CollectorConfig} userConfig
+ * @param {{ autoProfile: string | boolean }} defaultConfig
+ * @returns {string | boolean}
+ */
+function normalizeAutoProfile(userConfig, defaultConfig) {
+  return resolveConfig(process.env.INSTANA_AUTO_PROFILE, userConfig.autoProfile, defaultConfig.autoProfile);
+}
+
+/**
+ * @param {import('../types/collector').CollectorConfig} userConfig
+ * @returns {object}
+ */
+function normalizeTracing(userConfig) {
+  return userConfig.tracing ?? {};
+}
+
+/**
+ * @param {import('../types/collector').CollectorConfig} userConfig
+ * @returns {boolean}
+ */
+function normalizeUnhandledRejections(userConfig) {
+  return userConfig.reportUnhandledPromiseRejections ?? false;
+}
+
+/**
+ * @template T
+ * @param {T | undefined} envValue
+ * @param {T | undefined} configValue
+ * @param {T} defaultValue
+ * @returns {T}
+ */
+function resolveConfig(envValue, configValue, defaultValue) {
+  if (configValue != null) {
+    return configValue;
+  }
+
+  if (envValue != null) {
+    return envValue;
+  }
+
+  return defaultValue;
+}
+
+/**
+ * @param {string | undefined} envValue
+ * @param {number | undefined} configValue
  * @param {number} defaultValue
  * @returns {number}
  */
-function parseToPositiveInteger(value, defaultValue) {
-  if (typeof value !== 'string') {
-    return defaultValue;
+function resolveNumericConfig(envValue, configValue, defaultValue) {
+  if (configValue != null) {
+    return configValue;
   }
-  value = parseInt(value, 10);
-  if (!isNaN(value)) {
-    return Math.abs(Math.round(value));
+
+  const parsedEnv = parseToPositiveInteger(envValue);
+  if (parsedEnv != null) {
+    return parsedEnv;
   }
   return defaultValue;
+}
+
+/**
+ * @param {string | number | undefined} value
+ * @returns {number | null}
+ */
+function parseToPositiveInteger(value) {
+  if (typeof value !== 'string') {
+    return null;
+  }
+
+  const parsed = Number.parseInt(value, 10);
+
+  if (!isNaN(parsed)) {
+    return Math.abs(Math.round(parsed));
+  }
+
+  return null;
 }
