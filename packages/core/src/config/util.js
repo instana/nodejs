808655418a67153b72af9f96d16b41f1050c2aec
Arya <arya.mohanan@ibm.com>
2026-04-23 20:12:59 +0530

    chore: updated

--- packages/core/src/config/util.js
+++ packages/core/src/config/util.js
@@ -29,7 +29,7 @@ const CONFIG_PRIORITY = Object.entries(CONFIG_SOURCES)
  * @param {any} [params.inCodeValue]
  * @param {any} [params.agentValue]
  * @param {any} params.defaultValue
- * @param {string} [params.configPath] - The config path (e.g., 'config.serviceName')
+ * @param {string} [params.configPath]
  * @param {Function|Function[]} validators - validator(s) returning value | undefined
  * @returns {{ value: any, source: number }}
  */
