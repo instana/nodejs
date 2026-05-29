71fbb5d1101776c9bef364fe6bfcca8ba4298a95
kirrg001 <katharina.irrgang@ibm.com>
2026-05-29 12:49:26 +0200

    chore: agent basedhttp

---
+++ example-apps/otel/agent-based-http/README.md
@@ -0,0 +1,22 @@
+# Agent-based-http
+
+## Requirements
+
+- Instana Agent is running locally
+- Get the x-instana-key from the agent installation docs of the target SaaS instance.
+
+## Installation
+
+```sh
+npm i
+```
+
+## Start
+
+```sh
+INSTANA_AGENT_KEY=xxx OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318" OTEL_LOG_LEVEL=debug OTEL_SERVICE_NAME=node-agent-based-otel-sdk npm start
+node kafka-consumer.js
+
+curl http://localhost:6215/http
+curl -X POST http://localhost:6215/kafka-msg
+```
---
+++ example-apps/otel/agent-based-http/tracing.js
@@ -0,0 +1,50 @@
+/*
+ * (c) Copyright IBM Corp. 2026
+ */
+
+'use strict';
+
+const { metrics } = require('@opentelemetry/api');
+const opentelemetry = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+const { KafkaJsInstrumentation } = require('@opentelemetry/instrumentation-kafkajs');
+const { HostMetrics } = require('@opentelemetry/host-metrics');
+
+const otlpEndpoint = process.env.OTEL_EXPORTER_OTLP_ENDPOINT || 'http://127.0.0.1:4318';
+const headers = {
+  'x-instana-key': process.env.INSTANA_AGENT_KEY
+};
+
+const instanaReader = new PeriodicExportingMetricReader({
+  exportIntervalMillis: 1000 * 10,
+  exporter: new OTLPMetricExporter({
+    url: `${otlpEndpoint}/v1/metrics`,
+    headers
+  })
+});
+
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    url: `${otlpEndpoint}/v1/traces`,
+    headers
+  }),
+  metricReader: instanaReader,
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-fs': { enabled: false }
+    }),
+    new KafkaJsInstrumentation()
+  ]
+});
+
+sdk.start();
+
+const hostMetrics = new HostMetrics({
+  meterProvider: metrics.getMeterProvider(),
+  name: 'host-stats'
+});
+
+hostMetrics.start();
