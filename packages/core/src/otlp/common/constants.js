59fda363edb17f3278a554be2951e301c85ea2f1
Arya <arya.mohanan@ibm.com>
2026-06-12 19:41:34 +0530

    chore: resource

---
+++ packages/core/src/otlp/common/constants.js
@@ -0,0 +1,14 @@
+/*
+ * (c) Copyright IBM Corp. 2026
+ */
+
+'use strict';
+
+exports.RESOURCE_ATTRIBUTES = {
+  SERVICE_NAME: 'service.name',
+  SDK_LANGUAGE: 'telemetry.sdk.language',
+  SDK_NAME: 'telemetry.sdk.name',
+  SDK_VERSION: 'telemetry.sdk.version',
+  HOST_NAME: 'host.name',
+  PROCESS_PID: 'process.pid'
+};
