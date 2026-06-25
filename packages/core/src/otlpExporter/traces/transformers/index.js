ca714638b7f89fd77a4fa8ee323d7de702366451
Arya <arya.mohanan@ibm.com>
2026-06-25 11:40:02 +0530

    chore: updated integretion test

--- packages/core/src/otlpExporter/traces/transformers/index.js
+++ packages/core/src/otlpExporter/traces/transformers/index.js
@@ -4,16 +4,6 @@
 
 'use strict';
 
-/**
- * This module acts as the centralized aggregation point for trace-specific
- * transformation routines converting Instana span footprints to OTLP compliance format.
- *
- * Sub-Components:
- * - resource: Extracts and transforms resource attributes (from common/transformers)
- * - spanMetaData: Parses base context envelopes (IDs, nano time-scales, kinds, statuses)
- * - spanAttributes: Processes protocol specific block definitions (HTTP, RPC, messaging, DBs)
- */
-
 const resource = require('../../common/transformers/resource');
 const spanMetadata = require('./spanMetadata');
 const spanAttributes = require('./spanAttributes');
