e03d90c486c2abfa5e33c43cb76cd26a89b3db7e
Abhilash <70062455+abhilash-sivan@users.noreply.github.com>
2026-07-30 19:13:22 +0530

    chore: apply suggestions from code review
    Co-authored-by: kirrg001 <katharina.irrgang@googlemail.com>

--- packages/core/src/tracing/instrumentation/protocols/graphql.js
+++ packages/core/src/tracing/instrumentation/protocols/graphql.js
@@ -24,10 +24,10 @@ const subscriptionUpdate = 'subscription-update';
 exports.init = function init(config) {
   logger = config.logger;
 
-  // For graphql < v17: hook into the execution file as it is loaded (CJS).
+  // For graphql < v17: require('graphql') triggers the internal file load 'execution/execute'
   hook.onFileLoad(/\/graphql\/execution\/execute.js/, instrumentExecute);
 
-  // For graphql >= v17: it is shipped as ESM first
+  // For graphql >= v17: They shipped ESM support including a larger refactoring loading the execute file from an internal harness module. CJS apps load the index.mjs entrypoint because of the `module-sync` feature which was added in Node v22 (https://nodejs.org/api/packages.html#conditional-exports). Thats why CJS apps with GraphQL v17 do not trigger `onFileLoad` for `execution/execute`.
   hook.onModuleLoad('graphql', instrumentGraphQL, { nativeEsm: true });
 
   hook.onFileLoad(/\/@apollo\/gateway\/dist\/executeQueryPlan.js/, instrumentApolloGatewayExecuteQueryPlan);
