52b560c6bfabf8600ce8939f2d3d0ddbe31a6182
Arya <arya.mohanan@ibm.com>
2026-09-24 18:57:32 +0530

    test: updated

--- packages/shared-metrics/test/util/nativeModuleRetry_test.js
+++ packages/shared-metrics/test/util/nativeModuleRetry_test.js
@@ -11,15 +11,6 @@ const path = require('path');
 const os = require('os');
 
 const config = require('@_local/core/test/config');
-function makeSandboxedBufferError() {
-  const obj = {};
-  Object.defineProperty(obj, 'concat', { value: function Buffer() {}, writable: false });
-  try {
-    obj.concat = function () {};
-  } catch (e) {
-    return e;
-  }
-}
 
 describe('shared-metrics/util/nativeModuleRetry', function () {
   this.timeout(config.getTestTimeout());
@@ -28,6 +19,8 @@ describe('shared-metrics/util/nativeModuleRetry', function () {
   let logger;
   /** @type {typeof import('../../src/util/nativeModuleRetry')} */
   let nativeModuleRetry;
+  /** @type {sinon.SinonStub} */
+  let tarXStub;
 
   const NON_EXISTENT_MODULE = 'instana-non-existent-native-addon-for-test';
 
@@ -41,9 +34,7 @@ describe('shared-metrics/util/nativeModuleRetry', function () {
 
   const makeFsStub = () => ({
     stat: sinon.stub().callsFake((_p, cb) => cb(null)),
-    promises: {
-      cp: sinon.stub().resolves()
-    }
+    promises: { cp: sinon.stub().resolves() }
   });
 
   beforeEach(() => {
@@ -52,19 +43,25 @@ describe('shared-metrics/util/nativeModuleRetry', function () {
       info: sinon.stub(),
       warn: sinon.stub()
     };
+    tarXStub = sinon.stub().resolves();
   });
 
   afterEach(() => {
     sinon.restore();
   });
 
-  it('should emit "failed" without crashing when tar.x() throws synchronously due to non-writable Buffer.concat', done => {
-    const sandboxedBufferError = makeSandboxedBufferError();
-    expect(sandboxedBufferError).to.be.instanceof(TypeError);
-    expect(sandboxedBufferError.message).to.include('read only property');
+  it('should emit "failed" and never call tar.x() when Buffer.concat is non-writable', done => {
+    // Make Buffer.concat non-writable to simulate the sandboxed environment
+    // (e.g. n8n Task Runner freezes globals after its own init).
+    const originalDescriptor = Object.getOwnPropertyDescriptor(Buffer, 'concat');
+    Object.defineProperty(Buffer, 'concat', {
+      value: Buffer.concat,
+      writable: false,
+      configurable: true
+    });
 
     nativeModuleRetry = proxyquire('../../src/util/nativeModuleRetry', {
-      tar: { x: sinon.stub().throws(sandboxedBufferError) },
+      tar: { x: tarXStub },
       '@instana/core': { uninstrumentedFs: makeFsStub() }
     });
     nativeModuleRetry.init({ logger });
@@ -72,13 +69,16 @@ describe('shared-metrics/util/nativeModuleRetry', function () {
     const emitter = nativeModuleRetry.loadNativeAddOn(makeOpts());
 
     emitter.once('failed', () => {
+      // Restore before asserting so a failure doesn't leave Buffer frozen.
+      Object.defineProperty(Buffer, 'concat', originalDescriptor);
+
+      expect(tarXStub.called).to.be.false;
       expect(logger.warn.called).to.be.true;
-      const warnMsg = logger.warn.args.flat().join(' ');
-      expect(warnMsg).to.include('read only property');
       done();
     });
 
     emitter.once('loaded', () => {
+      Object.defineProperty(Buffer, 'concat', originalDescriptor);
       done(new Error('Expected "failed" event, got "loaded"'));
     });
   });
